195e4172ff6b59dbe2df29d9be726226b861610a
Alex <qt-info@nokia.com>
2009-05-22 16:23:00 +1000

    windows wasn't actualy broken. For some reason the service database unit test was just renamed and subsequently the old test was executed which relied on old (now removed) library symbols

--- bin/rununittests.bat
+++ bin/rununittests.bat
@@ -5,7 +5,7 @@ set PATH=%BATCH_PATH%..\build\Debug\bin;%PATH%
 
 cd %BATCH_PATH%..\build\tests\Debug\bin
 tst_servicemetadata.exe
-tst_servicedatabasetest.exe
+tst_servicedatabase.exe
 tst_qservicemanager.exe
 tst_qservicefilter.exe
 tst_qserviceinterfacedescriptor.exe
