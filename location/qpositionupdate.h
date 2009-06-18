e83759264ab0e0c9ecbcbced1e8f39e1ac9f0c8b
Bea Lam <bea.lam@nokia.com>
2009-06-18 11:06:18 +1000

    Make compile on Windows

--- location/qpositionupdate.h
+++ location/qpositionupdate.h
@@ -91,9 +91,9 @@ public:
     bool hasProperty(Property property) const;
 
 private:
-    friend QDebug operator<<(QDebug dbg, const QPositionUpdate &update);
-    friend QDataStream &operator<<(QDataStream &stream, const QPositionUpdate &update);
-    friend QDataStream &operator>>(QDataStream &stream, QPositionUpdate &update);
+    friend Q_LOCATION_EXPORT QDebug operator<<(QDebug dbg, const QPositionUpdate &update);
+    friend Q_LOCATION_EXPORT QDataStream &operator<<(QDataStream &stream, const QPositionUpdate &update);
+    friend Q_LOCATION_EXPORT QDataStream &operator>>(QDataStream &stream, QPositionUpdate &update);
     QPositionUpdatePrivate *d;
 };
 
