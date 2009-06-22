82b06b7753e3e1c8f3bfbdcbc5b70fc7dbd70855
Dmytro Poplavskiy <dmytro.poplavskiy@nokia.com>
2009-06-22 14:07:35 +1000

    changed shared playlist to use (not implemented yet) playlist source

--- multimedia/qsharedmediaplaylist.h
+++ multimedia/qsharedmediaplaylist.h
@@ -1,10 +1,10 @@
 #ifndef QSHAREDMEDIAPLAYLIST_H
 #define QSHAREDMEDIAPLAYLIST_H
 
-#include "qabstractmediaplaylist.h"
+#include "qmediaplaylist.h"
 
 class QSharedMediaPlaylistPrivate;
-class QSharedMediaPlaylist : public QAbstractMediaPlaylist
+class QSharedMediaPlaylist : public QMediaPlaylist
 {
     Q_OBJECT
 public:
@@ -12,26 +12,7 @@ public:
     virtual ~QSharedMediaPlaylist();
 
     QString name() const;
-
     static QStringList sharedPlaylists();
-
-    virtual int currentItemPos() const;
-    virtual int size() const;
-
-    virtual QMediaSource operator [](int pos) const;
-
-    virtual void append(const QMediaSource &source);
-    virtual void append(const QList<QMediaSource> &sources);
-    virtual void insert(int pos, const QMediaSource &source);
-    virtual void remove(int fromPos, int toPos);
-
-    virtual bool isShuffled() const;
-
-public Q_SLOTS:
-    virtual void jump(int);
-
-    virtual void shuffle();
-    virtual void unshuffle();
 
 private:
     Q_DECLARE_PRIVATE(QSharedMediaPlaylist)
