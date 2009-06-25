860cfec74bda30d541954e7e6bb8ef353626039c
Andrew den Exter <andrew.den-exter@nokia.com>
2009-06-25 17:43:09 +1000

    Get meta data from windows media player plug-in.

---
+++ plugins/wmp/qwmpmetadata.h
@@ -0,0 +1,52 @@
+#ifndef QWMPMETADATA_H
+#define QWMPMETADATA_H
+
+#include <QtCore/qobject.h>
+#include <QtCore/qvariant.h>
+
+#include <wmp.h>
+
+#include "qwmpglobal.h"
+
+class Q_WMP_EXPORT QMediaMetaData : public QObject
+{
+    Q_OBJECT
+public:
+    QMediaMetaData(QObject *parent = 0) : QObject(parent) {}
+    ~QMediaMetaData() {}
+
+    virtual QStringList keys() const = 0;
+
+    virtual int count(const QString &key) const = 0;
+
+    virtual QVariant value(const QString &key, int index = 0) const = 0;
+    virtual QVariantList values(const QString &key) const = 0;
+
+Q_SIGNALS:
+    void changed();
+};
+
+
+class QWmpMetaData : public QMediaMetaData
+{
+    Q_OBJECT
+public:
+    QWmpMetaData(QObject *parent = 0);
+    ~QWmpMetaData();
+
+    QStringList keys() const;
+
+    int count(const QString &key) const;
+
+    QVariant value(const QString &key, int index = 0) const;
+    QVariantList values(const QString &key) const;
+
+    IWMPMedia *media() const;
+    void setMedia(IWMPMedia *media);
+
+private:
+    IWMPMedia *m_media;
+    IWMPMedia3 *m_media3;
+};
+
+#endif
