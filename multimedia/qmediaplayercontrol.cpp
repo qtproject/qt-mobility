7aab80e0787aa1d590bcfbe74819de63562d16d7
Justin McPherson <justin.mcpherson@nokia.com>
2009-07-01 14:10:36 +1000

    Defer all properties to implementor.

--- multimedia/qmediaplayercontrol.cpp
+++ multimedia/qmediaplayercontrol.cpp
@@ -33,107 +33,17 @@
 ****************************************************************************/
 
 #include "qmediaplayercontrol.h"
+#include  "qabstractmediacontrol_p.h"
 #include "qmediasource.h"
 
-Q_DECLARE_METATYPE(QMediaPlaylist*)
+
 
 QMediaPlayerControl::~QMediaPlayerControl()
 {
 }
 
-int QMediaPlayerControl::state() const
-{
-    return property("state").toInt();
-}
-
-void QMediaPlayerControl::setState(int state)
-{
-    changePropertyValue("state", state);
-}
-
-QMediaPlaylist* QMediaPlayerControl::mediaPlaylist() const
-{
-    return property("mediaPlaylist").value<QMediaPlaylist*>();
-}
-
-void QMediaPlayerControl::setMediaPlaylist(QMediaPlaylist *mediaPlaylist)
-{
-    changePropertyValue("mediaPlaylist", qVariantFromValue(mediaPlaylist));
-}
-
-qint64 QMediaPlayerControl::duration() const
-{
-    return property("duration").toLongLong();
-}
-
-void QMediaPlayerControl::setDuration(qint64 duration)
-{
-    changePropertyValue("duration", duration);
-}
-
-qint64 QMediaPlayerControl::position() const
-{
-    return property("position").toLongLong();
-}
-
-void QMediaPlayerControl::setPosition(qint64 position)
-{
-    changePropertyValue("position", position);
-}
-
-int QMediaPlayerControl::volume() const
-{
-    return property("volume").toInt();
-}
-
-void QMediaPlayerControl::setVolume(int volume)
-{
-    changePropertyValue("volume", volume);
-}
-
-bool QMediaPlayerControl::isMuted() const
-{
-    return property("muted").toBool();
-}
-
-void QMediaPlayerControl::setMuted(bool muted)
-{
-    changePropertyValue("muted", muted);
-}
-
-bool QMediaPlayerControl::isBuffering() const
-{
-    return property("buffering").toBool();
-}
-
-void QMediaPlayerControl::setBuffering(bool buffering)
-{
-    changePropertyValue("buffering", buffering);
-}
-
-int QMediaPlayerControl::bufferStatus() const
-{
-    return property("bufferStatus").toInt();
-}
-
-void QMediaPlayerControl::setBufferState(int bufferStatus)
-{
-    changePropertyValue("bufferStatus", bufferStatus);
-}
-
-bool QMediaPlayerControl::isVideoAvailable() const
-{
-    return property("videoAvailable").toBool();
-}
-
-void QMediaPlayerControl::setVideoAvailable(bool videoAvailable)
-{
-    changePropertyValue("videoAvailable", videoAvailable);
-}
-
-
 QMediaPlayerControl::QMediaPlayerControl(QObject *parent):
-    QAbstractMediaControl(parent)
+    QAbstractMediaControl(*new QAbstractMediaControlPrivate, parent)
 {
     addPropertyWatch("position");
 }
