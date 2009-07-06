3e043a30ae7513a66ade23f60206e84140a352b0
Dmytro Poplavskiy <dmytro.poplavskiy@nokia.com>
2009-07-06 14:20:29 +1000

    Added QMediaStreams/QMediaStreamsControl
    It allows to inspect available video, audio, subpicture
    and data streams and switch between them.

---
+++ multimedia/qmediastreamscontrol.h
@@ -0,0 +1,23 @@
+#ifndef QMEDIASTREAMSCONTROL_H
+#define QMEDIASTREAMSCONTROL_H
+
+#include "qabstractmediacontrol.h"
+
+class QMediaStreamsControl : public QAbstractMediaControl
+{
+Q_OBJECT
+public:
+    virtual ~QMediaStreamsControl();
+
+    virtual int streamCount() = 0;
+    virtual int streamType(int streamNumber) = 0;
+    virtual QMap<QString,QVariant> streamProperties(int streamNumber) = 0;
+    virtual bool isActive(int streamNumber) = 0;
+    virtual void setActive(int streamNumber, bool state) = 0;
+
+signals:
+    void streamsChanged();
+    void activeStreamsChanged();
+};
+
+#endif // QMEDIASTREAMSCONTROL_H
