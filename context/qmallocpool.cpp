2a7dc7cb6271d51e07c93457219a98240bdba5f8
Alex <qt-info@nokia.com>
2009-08-18 13:41:14 +1000

    Update license header

--- context/qmallocpool.cpp
+++ context/qmallocpool.cpp
@@ -26,7 +26,7 @@
 ** package.  
 **
 ** If you have questions regarding the use of this file, please
-** contact Nokia at http://www.qtsoftware.com/contact.
+** contact Nokia at http://qt.nokia.com/contact.
 ** $QT_END_LICENSE$
 **
 ****************************************************************************/
