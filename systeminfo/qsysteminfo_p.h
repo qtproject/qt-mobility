d54ee6255caad0d7b4fb86f3f7849ee76568d727
Lorn Potter <lorn.potter@nokia.com>
2009-08-26 15:14:00 +1000

    add private header warning

--- systeminfo/qsysteminfo_p.h
+++ systeminfo/qsysteminfo_p.h
@@ -33,6 +33,19 @@
 #ifndef QSYSTEMINFO_P_H
 #define QSYSTEMINFO_P_H
 
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+
 #include <QObject>
 #include <QSize>
 #include <QHash>
