9bb9189c02bec877c112b607efd68bc11a2144a8
Dmytro Poplavskiy <dmytro.poplavskiy@nokia.com>
2009-09-18 12:27:59 +1000

    Added camera device selection in the camera capture example.

--- examples/cameracapture/cameracapture.cpp
+++ examples/cameracapture/cameracapture.cpp
@@ -62,11 +62,7 @@ CameraCapture::CameraCapture(QWidget *parent) :
     ui->setupUi(this);
 
     camera = new QCamera;
-
-    if(camera->isValid())
-        mediaRecorder = new QMediaRecorder(camera);
-    else
-        mediaRecorder = new QMediaRecorder();
+    mediaRecorder = new QMediaRecorder(camera);
 
     mediaRecorder->setSink(QUrl("test.mkv"));
     audioDevice = mediaRecorder->service()->control<QAudioDeviceControl*>();
@@ -122,6 +118,11 @@ CameraCapture::CameraCapture(QWidget *parent) :
             ui->containerFormatBox->setCurrentIndex(ui->containerFormatBox->count()-1);
     }
 
+    //camera devices
+    foreach(const QString deviceName, camera->devices()) {
+        ui->cameraDeviceBox->addItem(deviceName+" "+camera->deviceDescription(deviceName), QVariant(deviceName));
+    }
+
     camera->setMetaData(QAbstractMediaObject::Title, QVariant(QLatin1String("Test Title")));
 
     QWidget *videoWidget = new QVideoWidget(mediaRecorder);
@@ -146,7 +147,7 @@ void CameraCapture::setAudioInputDevice(int idx)
 
 void CameraCapture::setCameraDevice(int idx)
 {
-    Q_UNUSED(idx);
+    camera->setDevice(ui->cameraDeviceBox->itemData(idx).toString());
 }
 
 void CameraCapture::setAudioCodec(int idx)
