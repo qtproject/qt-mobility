3b379b0385dbc44b5ccbc07f6db00b76a7620cfc
Lorn Potter <lorn.potter@nokia.com>
2009-09-23 14:27:51 +1000

    fix build

--- systeminfo/qsysteminfo_linux.cpp
+++ systeminfo/qsysteminfo_linux.cpp
@@ -1690,7 +1690,7 @@ bool QSystemDeviceInfoPrivate::isDeviceLocked()
                          qWarning() << reply.error();
                      }
 //                 }
-             }
+//             }
          }
 #endif
      } else {
