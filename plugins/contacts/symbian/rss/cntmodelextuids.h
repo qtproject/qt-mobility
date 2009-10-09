e82c0c3ac2e5753a14d7c2fcf0d3c2e8915668f2
njansson <niklas.r.jansson@nokia.com>
2009-10-09 09:15:17 +0300

    fixed a compilation error on symbian 10.1

--- plugins/contacts/symbian/rss/cntmodelextuids.h
+++ plugins/contacts/symbian/rss/cntmodelextuids.h
@@ -61,6 +61,12 @@ const TUid KUidContactFieldSyncTarget={KUidContactFieldSyncTargetValue};
  */
 const TUid KUidContactFieldGender={KUidContactFieldGenderValue};
 
+// !! In case this is undefined on earlier platforms
+#ifndef KUidContactFieldGEOValue
+#define KUidContactFieldGEOValue            0x10274DB2
+const TUid KUidContactFieldGEO = {KUidContactFieldGEOValue};
+#endif
+
 #endif // CNTMODELEXTUIDS_HRH
 
 // End of File
