22e5f8b69cb2b4d56d76f1a963aac8c0fff9566f
Keith Isdale <keith.isdale@nokia.com>
2009-10-09 14:31:31 +1000

    If the QA deploy tool is not present for WindowsCE do use it
    Skip deploy if not available ( hoping the user has already deployed
    QT to the device )

--- bin/rununittests_wince.bat
+++ bin/rununittests_wince.bat
@@ -3,8 +3,11 @@ set DEPOT="%CD%"
 IF NOT EXIST sfw.pro GOTO NOSOURCE
 set CETESTCOMMAND=cetest -cache %QTDIR%\.qmake.cache -libpath \Windows 
 
+IF NOT EXIST %DEPOT%\..\..\qa-dungeon\mainline\tests\wince\deploy\ GOTO SKIPDEPLOY
 cd %DEPOT%\..\..\qa-dungeon\mainline\tests\wince\deploy\
 call %CETESTCOMMAND%  
+
+:SKIPDEPLOY
 cd %DEPOT%\tests\auto\servicemetadata
 call %CETESTCOMMAND%  
 cd %DEPOT%\tests\auto\servicedatabase
