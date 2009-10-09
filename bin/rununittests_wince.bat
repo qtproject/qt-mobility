13031b052896a8be140a53d69c9fce695212018c
Keith Isdale <keith.isdale@nokia.com>
2009-10-09 14:05:14 +1000

    Add a easy rununitests script for WinCE
    WinCE unittest must be run from the source directory

---
+++ bin/rununittests_wince.bat
@@ -0,0 +1,30 @@
+set DEPOT="%CD%"
+
+IF NOT EXIST sfw.pro GOTO NOSOURCE
+set CETESTCOMMAND=cetest -cache %QTDIR%\.qmake.cache -libpath \Windows 
+
+cd %DEPOT%\..\..\qa-dungeon\mainline\tests\wince\deploy\
+call %CETESTCOMMAND%  
+cd %DEPOT%\tests\auto\servicemetadata
+call %CETESTCOMMAND%  
+cd %DEPOT%\tests\auto\servicedatabase
+call %CETESTCOMMAND%  
+cd %DEPOT%\tests\auto\databasemanager
+call %CETESTCOMMAND%  
+cd %DEPOT%\tests\auto\qservicemanager"
+call %CETESTCOMMAND%
+cd %DEPOT%\tests\auto\qservicefilter
+call %CETESTCOMMAND%  
+cd %DEPOT%\tests\auto\qserviceinterfacedescriptor
+call %CETESTCOMMAND%  
+cd %DEPOT%\tests\auto\qabstractsecuritysession
+call %CETESTCOMMAND%  
+GOTO EOF
+
+:NOSOURCE
+echo "Unable to find sfw.pro check current directory is serviceframework source" 
+GOTO EOF
+
+:EOF
+cd %DEPOT%
+
