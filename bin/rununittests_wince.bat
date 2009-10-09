26c0904ebe60fc2396f6640c0cc5d6285b96fb22
Keith Isdale <keith.isdale@nokia.com>
2009-10-09 14:28:28 +1000

    Add a rununittests script for WinCE
    Must be source directory to run test

---
+++ bin/rununittests_wince.bat
@@ -0,0 +1,26 @@
+set DEPOT="%CD%"
+
+IF NOT EXIST bearermanagement.pro GOTO NOSOURCE
+set CETESTCOMMAND=cetest -cache %QTDIR%\.qmake.cache -libpath \Windows 
+
+IF NOT EXIST %DEPOT%\..\..\qa-dungeon\mainline\tests\wince\deploy\ GOTO SKIPDEPLOY
+cd %DEPOT%\..\..\qa-dungeon\mainline\tests\wince\deploy\
+call %CETESTCOMMAND%  
+
+:SKIPDEPLOY
+cd %DEPOT%\..\..\qa-dungeon\mainline\tests\wince\deploy\
+call %CETESTCOMMAND%  
+cd %DEPOT%\tests\auto\qnetworkconfiguration
+call %CETESTCOMMAND%  
+cd %DEPOT%\tests\auto\qnetworkconfigmanager
+call %CETESTCOMMAND%  
+cd %DEPOT%\tests\auto\qnetworksession
+GOTO EOF
+
+:NOSOURCE
+echo "Unable to find bearermanagement.pro check current directory is bearermanagment source" 
+GOTO EOF
+
+:EOF
+cd %DEPOT%
+
