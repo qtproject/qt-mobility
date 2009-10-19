b12a23b023c73bb3b3128854dd62bc0e5f1e6ebe
Alex <qt-info@nokia.com>
2009-10-19 13:34:10 +1000

    add compile tests for Unix configure (Windows will follow)

---
+++ config.tests/networkmanager/main.cpp
@@ -0,0 +1,6 @@
+#include <NetworkManager/NetworkManager.h>
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
---
+++ config.tests/qmf/main.cpp
@@ -0,0 +1,8 @@
+#include <qmailmessage.h>
+#include <qmailglobal.h>
+
+int main(int argc, char** argv)
+{
+    QMailMessage m;
+    return 0;
+}
