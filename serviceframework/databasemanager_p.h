435fab66a5ddc948e2ed1ac3da8b0a2e16a94da8
Stian Sandvik Thomassen <stian.thomassen@nokia.com>
2009-10-19 12:19:29 +1000

    Added "we mean it" comment to private header files
    Reviewed-by: Trust Me

--- serviceframework/databasemanager_p.h
+++ serviceframework/databasemanager_p.h
@@ -41,6 +41,17 @@
 #ifndef DATABASEMANAGER_H_
 #define DATABASEMANAGER_H_
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API. It exists purely as an
+// implementation detail. This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include "servicedatabase_p.h"
 #include <QObject>
 #include "qserviceglobal.h"
--- serviceframework/servicedatabase_p.h
+++ serviceframework/servicedatabase_p.h
@@ -41,6 +41,17 @@
 #ifndef QSERVICEDATABASE_H_
 #define QSERVICEDATABASE_H_
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API. It exists purely as an
+// implementation detail. This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <servicemetadata_p.h>
 #include <qservicefilter.h>
 #include <QtSql>
