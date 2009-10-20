4f818b712ecf98a7c60906f1232fb5656b18e53e
Antti Klemetti <antti.klemetti@nokia.com>
2009-10-20 10:36:45 +0300

    Added the implementations for QVersitReaderPrivate and QVersitWriterPrivate

---
+++ versit/qversitreader_p.cpp
@@ -0,0 +1,128 @@
+/****************************************************************************
+**
+** Copyright (c) 2008-2009 Nokia Corporation and/or its subsidiary(-ies).
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the Qt Mobility Components.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** No Commercial Usage
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the Technology Preview License Agreement accompanying
+** this package.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain
+** additional rights. These rights are described in the Nokia Qt LGPL
+** Exception version 1.0, included in the file LGPL_EXCEPTION.txt in this
+** package.
+**
+** If you have questions regarding the use of this file, please
+** contact Nokia at http://qt.nokia.com/contact.
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "qversitreader_p.h"
+#include "versitutils.h"
+
+// Some big enough value for nested versit documents to prevent infite recursion
+#define MAX_VERSIT_DOCUMENT_NESTING_DEPTH 20
+
+/*! Construct a reader. */
+QVersitReaderPrivate::QVersitReaderPrivate() : 
+    mIoDevice(0), 
+    mDocumentNestingLevel(0)
+{
+}
+    
+/*! Destroy a reader. */    
+QVersitReaderPrivate::~QVersitReaderPrivate()
+{
+}
+
+/*!
+ * Parses a versit document and returns the resulting document object
+ */
+QVersitDocument QVersitReaderPrivate::parseVersitDocument(QByteArray& text)
+{
+    mDocumentNestingLevel++;
+    QVersitDocument document;
+    text = text.mid(VersitUtils::countLeadingWhiteSpaces(text));
+    QVersitProperty property = parseNextVersitProperty(text);
+    if (property.name() == QString::fromAscii("BEGIN") && 
+        property.value().trimmed().toUpper() == "VCARD") {
+        while (property.name().length() > 0 && 
+               property.name() != QString::fromAscii("END")) {
+            property = parseNextVersitProperty(text);   
+            if (!containsSupportedVersion(property)) {
+                mDocumentNestingLevel--;
+                return QVersitDocument(); // return an empty document
+            }
+            if (property.name() != QString::fromAscii("VERSION") && 
+                property.name() != QString::fromAscii("END"))
+                document.addProperty(property);
+        }
+    }
+    mDocumentNestingLevel--;
+    return document;
+}
+
+/*!
+ * Parses a versit document and returns whether parsing succeeded.
+ */
+QVersitProperty QVersitReaderPrivate::parseNextVersitProperty(QByteArray& text)
+{
+    QVersitProperty property;
+    property.setName(VersitUtils::extractPropertyName(text));
+    property.setParameters(VersitUtils::extractPropertyParams(text));
+    text = VersitUtils::extractPropertyValue(text); 
+    if (property.name() == QString::fromAscii("AGENT")) {
+        if (mDocumentNestingLevel >= MAX_VERSIT_DOCUMENT_NESTING_DEPTH)
+            return property; // To prevent infinite recursion
+        property.setEmbeddedDocument(parseVersitDocument(text));
+    }
+    else {
+        int crlfPos = -1;
+        QString encoding(QString::fromAscii("ENCODING"));
+        QString quotedPrintable(QString::fromAscii("QUOTED-PRINTABLE"));
+        if (property.parameters().contains(encoding,quotedPrintable)) {
+            crlfPos = VersitUtils::findHardLineBreakInQuotedPrintable(text);
+            QByteArray value = text.left(crlfPos);
+            VersitUtils::decodeQuotedPrintable(value);
+            // Remove the encoding parameter as the value is now decoded
+            property.removeParameter(encoding,quotedPrintable);
+            property.setValue(value);
+        }
+        else {
+            crlfPos = text.indexOf("\r\n");
+            property.setValue(text.left(crlfPos));
+        }
+        text = text.mid(crlfPos+2); // +2 is for skipping the CRLF
+    }
+    return property;
+}
+
+/*!
+ * Checks whether the VERSION property contains a supported version.
+ */
+bool QVersitReaderPrivate::containsSupportedVersion(const QVersitProperty& property) const
+{
+    bool valid = true;
+    if (property.name() == QString::fromAscii("VERSION")) {
+        QByteArray value = property.value().trimmed();
+        if (property.parameters().contains(
+                QString::fromAscii("ENCODING"),QString::fromAscii("BASE64")))
+            value = QByteArray::fromBase64(value);
+        valid = (value == "2.1");
+    } 
+    return valid;
+}
---
+++ versit/qversitwriter_p.cpp
@@ -0,0 +1,139 @@
+/****************************************************************************
+**
+** Copyright (c) 2008-2009 Nokia Corporation and/or its subsidiary(-ies).
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the Qt Mobility Components.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** No Commercial Usage
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the Technology Preview License Agreement accompanying
+** this package.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain
+** additional rights. These rights are described in the Nokia Qt LGPL
+** Exception version 1.0, included in the file LGPL_EXCEPTION.txt in this
+** package.
+**
+** If you have questions regarding the use of this file, please
+** contact Nokia at http://qt.nokia.com/contact.
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "qversitwriter_p.h"
+#include "versitutils.h"
+#include <QStringList>
+
+
+/*! Constructs a writer. */
+QVersitWriterPrivate::QVersitWriterPrivate() : mIoDevice(0)
+{
+}
+
+/*! Destroys a writer. */
+QVersitWriterPrivate::~QVersitWriterPrivate()
+{
+}
+
+/*!
+ * Encodes the \a document to text. 
+ */
+QByteArray QVersitWriterPrivate::encodeVersitDocument(const QVersitDocument& document)
+{
+    QList<QVersitProperty> properties = document.properties();
+    QByteArray encodedDocument;
+    
+    encodedDocument.append("BEGIN:VCARD\r\n");
+    encodedDocument.append("VERSION:2.1\r\n");
+    foreach (QVersitProperty property, properties) {
+        encodedDocument.append(encodeVersitProperty(property));
+    }
+    encodedDocument.append("END:VCARD\r\n");
+    
+    return encodedDocument;
+}
+
+/*!
+ * Encodes the \a property to text. 
+ */
+QByteArray QVersitWriterPrivate::encodeVersitProperty(const QVersitProperty& property)
+{
+    QByteArray encodedProperty;
+    QString name = property.name();    
+    encodedProperty.append(name.toAscii());
+
+    QByteArray value(property.value());
+    bool valueQuotedPrintableEncoded = quotedPrintableEncode(property,value);
+    QByteArray encodedParameters = 
+        encodeParameters(property.parameters(),valueQuotedPrintableEncoded);
+    encodedProperty.append(encodedParameters);
+
+    encodedProperty.append(":");
+    if (name == QString::fromAscii("AGENT")) {
+        encodedProperty.append("\r\n");
+        QVersitDocument embeddedDocument = property.embeddedDocument();
+        encodedProperty.append(encodeVersitDocument(embeddedDocument));
+    } else {
+        encodedProperty.append(value);
+    }
+    encodedProperty.append("\r\n");
+
+    return encodedProperty;
+}
+
+/*!
+ * Encodes the \a parameters to text. 
+ */
+QByteArray QVersitWriterPrivate::encodeParameters(
+    const QMultiHash<QString,QString>& parameters,
+    bool addQuotedPrintable)
+{
+    QByteArray encodedParameters;
+    QString typeParameterName(QString::fromAscii("TYPE"));
+    QList<QString> names = parameters.uniqueKeys();
+    foreach (QString name, names) {
+        QStringList values = parameters.values(name);
+        foreach (QString value, values) {
+            encodedParameters.append(";");
+            if (name != typeParameterName) {
+                encodedParameters.append(name.toAscii());
+                encodedParameters.append("=");
+            }
+            encodedParameters.append(value.toAscii());
+        } 
+    }
+    QString encodingParameterName(QString::fromAscii("ENCODING"));
+    QString quotedPrintableValue(QString::fromAscii("QUOTED-PRINTABLE"));
+    if (addQuotedPrintable && 
+        !parameters.contains(encodingParameterName,quotedPrintableValue))
+        encodedParameters.append(";ENCODING=QUOTED-PRINTABLE");
+    return encodedParameters;
+}
+
+/*!
+ * Encodes the \a value with Quoted-Printable encoding
+ * if it needs to be encoded and the parameters 
+ * of the \a property do not yet indicate encoding.  
+ */
+bool QVersitWriterPrivate::quotedPrintableEncode(
+    const QVersitProperty& property,
+    QByteArray& value) const
+{
+    bool encoded = false;
+    value = property.value();
+    if (!property.parameters().contains(QString::fromAscii("ENCODING"))) {
+        encoded = VersitUtils::quotedPrintableEncode(value);
+    }
+    return encoded;
+}
