d416c04b3b12fe511075b198baa56cadf198d100
njansson <niklas.r.jansson@nokia.com>
2009-11-02 13:13:16 +0200

    symbian: removed old file

--- plugins/contacts/symbian/src/cntsymbiansorterdbms.cpp
+++ plugins/contacts/symbian/src/cntsymbiansorterdbms.cpp
@@ -123,6 +123,15 @@ bool CntSymbianSorterDbms::sortOrderSupported(const QList<QContactSortOrder>& so
         // Always case sensitive
         if( s.caseSensitivity() != Qt::CaseSensitive )
             return false;
+
+#ifndef __SYMBIAN_CNTMODEL_USE_SQLITE__
+        // NOTE:
+        // Seems that there is a bug in cntmodel which causes that sorting
+        // is working correctly only if the direction is the same for all
+        // sort orders.
+        if( s.direction() != sortOrders[0].direction() )
+            return false;
+#endif
     }
     return true;
 }
