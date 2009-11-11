6450b434943e8c9017899a24a21dcad63399e9af
Chris Adams <christopher.adams@nokia.com>
2009-11-11 14:48:58 +1000

    Deprecate contacts accessor taking contact-type parameter in QCM

--- contacts/qcontactmanager.h
+++ contacts/qcontactmanager.h
@@ -109,7 +109,7 @@ public:
     /* Contacts - Accessors and Mutators */
     QList<QContactLocalId> contacts(const QList<QContactSortOrder>& sortOrders = QList<QContactSortOrder>()) const;    // retrieve contact ids
     QList<QContactLocalId> contacts(const QContactFilter& filter, const QList<QContactSortOrder>& sortOrders = QList<QContactSortOrder>()) const; // retrieve ids of contacts matching the filter
-    QList<QContactLocalId> contacts(const QString& contactType, const QList<QContactSortOrder>& sortOrders = QList<QContactSortOrder>()) const; // retrieve contacts of the given type
+    QList<QContactLocalId> Q_DECL_DEPRECATED contacts(const QString& contactType, const QList<QContactSortOrder>& sortOrders = QList<QContactSortOrder>()) const; // retrieve contacts of the given type
 
     QContact contact(const QContactLocalId& contactId) const;  // retrieve a contact
 
