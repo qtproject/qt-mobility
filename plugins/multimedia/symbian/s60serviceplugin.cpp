qtproject/qt-mobility

commit fe6adabad8d18af04f0e16d014ea9c38c790572a

Test for depedancy on mpengine.lib Pull in mediplayer code for S60 only if mpengine.lib is available in the SDK

plugins/multimedia/symbian/symbian.pro
@@ -24,7 +24,9 @@ SOURCES += s60serviceplugin.cpp \
 
 include(radio/radio.pri)
 # include(camera/camera_s60.pri)
-include(mediaplayer/mediaplayer_s60.pri)
+exists($${EPOCROOT}epoc32/release/winscw/udeb/mpengine.lib){
+    include(mediaplayer/mediaplayer_s60.pri)
+}
 include(audiosource/audiosource_s60.pri)
 
 load(data_caging_paths)

commit 4cbd14db83dc206fcb1d77ecd4a3e8be54644014

build still fails after fe6adabad8d18af04f0e16d014ea9c38c790572a put in ifdef's to test for missing functionality

plugins/multimedia/symbian/s60serviceplugin.cpp
@@ -46,7 +46,9 @@
 #include "s60serviceplugin.h"
 #include "s60radiotunerservice.h"
 //#include "s60cameraservice.h" //Camera impl is on hold
+#ifdef HAS_MEDIA_PLAYER
 #include "s60mediaplayerservice.h"
+#endif
 #include "s60audiocaptureservice.h"
 
 QStringList S60ServicePlugin::keys() const
@@ -54,19 +56,23 @@ QStringList S60ServicePlugin::keys() const
     QStringList list;
     list << QLatin1String(Q_MEDIASERVICE_RADIO);
   //  list << QLatin1String(Q_MEDIASERVICE_CAMERA); //Camera impl is on hold
+#ifdef HAS_MEDIA_PLAYER  
     list << QLatin1String(Q_MEDIASERVICE_MEDIAPLAYER);
+#endif    
     list << QLatin1String(Q_MEDIASERVICE_AUDIOSOURCE);
     return list;
 }
 
 QMediaService* S60ServicePlugin::create(QString const& key)
 {
-    if (key == QLatin1String(Q_MEDIASERVICE_RADIO)) 
+    if (key == QLatin1String(Q_MEDIASERVICE_RADIO))
         return new S60RadioTunerService;
  /*   else if (key == QLatin1String(Q_MEDIASERVICE_CAMERA))
         return new S60CameraService;*/ //Camera impl is on hold
+#ifdef HAS_MEDIA_PLAYER
     else if (key == QLatin1String(Q_MEDIASERVICE_MEDIAPLAYER))
         return new S60MediaPlayerService;
+#endif
     else if (key == QLatin1String(Q_MEDIASERVICE_AUDIOSOURCE))
         return new S60AudioCaptureService;
 
@@ -106,13 +112,13 @@ QString S60ServicePlugin::deviceDescription(const QByteArray &service, const QBy
 
 void S60ServicePlugin::updateDevices() const
 {
-    
+
     m_cameraDevices.clear();
     m_cameraDescriptions.clear();
     for (int i=0; i < S60CameraService::deviceCount(); i ++) {
         QString deviceName = QString().number(i);
         m_cameraDevices.append(deviceName.toUtf8());
-        QString deviceDesc = S60CameraService::deviceDescription(i); 
+        QString deviceDesc = S60CameraService::deviceDescription(i);
         m_cameraDescriptions.append(deviceDesc);
     }
 

plugins/multimedia/symbian/symbian.pro
@@ -24,8 +24,11 @@ SOURCES += s60serviceplugin.cpp \
 
 include(radio/radio.pri)
 # include(camera/camera_s60.pri)
+
+# depends on mpengine.lib, found in S60 SDK greater than 3.1
 exists($${EPOCROOT}epoc32/release/winscw/udeb/mpengine.lib){
     include(mediaplayer/mediaplayer_s60.pri)
+    DEFINES += HAS_MEDIA_PLAYER
 }
 include(audiosource/audiosource_s60.pri)
 