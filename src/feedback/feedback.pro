30b103b4d1afb38a575f129a38db6afc1ca441c0
Michael Goddard <michael.goddard@nokia.com>
2010-01-18 16:51:40 +1000

    Some initial concepting.
    Also renamed to feedback instead of vibra.

--- src/feedback/feedback.pro
+++ src/feedback/feedback.pro
@@ -1,48 +1,43 @@
 # #####################################################################
-# Vibra Mobility API
+# Feedback Mobility API
 # #####################################################################
 TEMPLATE = lib
 
 # Target gets fixed up in common.pri
-TARGET = QtVibra
-DEFINES += QT_BUILD_VIBRA_LIB QT_MAKEDLL\
-    QT_ASCII_CAST_WARNINGS
-
+TARGET = QtFeedback
+DEFINES += QT_BUILD_FEEDBACK_LIB QT_MAKEDLL QT_ASCII_CAST_WARNINGS
 include(../../common.pri)
 
 # Input
 PUBLIC_HEADERS += \
+            qfeedbackcontroller.h \
+            qfeedbackeffect.h
 
 # Private Headers
-PRIVATE_HEADERS += \
+PRIVATE_HEADERS += 
 
-SOURCES += \
+SOURCES +=  qfeedbackcontroller.cpp \
+            qfeedbackeffect.cpp
 
-HEADERS += \
-    $$PUBLIC_HEADERS \
-    $$PRIVATE_HEADERS
+HEADERS +=  $$PUBLIC_HEADERS \
+            $$PRIVATE_HEADERS \
 
-symbian {
+symbian { 
     TARGET.EPOCALLOWDLLDATA = 1
     TARGET.CAPABILITY = ALL -TCB
-    ## Need a new UID
-    ## TARGET.UID3 = 0x2002AC7A
-
-    ### Vibra
+    
+    # # Need a new UID
+    # # TARGET.UID3 = 0x2002AC7A
+    # ## Feedback
     # Main library
-    VIBRA_DEPLOYMENT.sources = QtVibra.dll
-    VIBRA_DEPLOYMENT.path = \sys\bin
-    DEPLOYMENT += VIBRA_DEPLOYMENT
-
+    FEEDBACK_DEPLOYMENT.sources = QtFeedback.dll
+    FEEDBACK_DEPLOYMENT.path = \sys\bin
+    DEPLOYMENT += FEEDBACK_DEPLOYMENT
     deploy.path = $$EPOCROOT
     exportheaders.sources = $$PUBLIC_HEADERS
     exportheaders.path = epoc32/include
-
-    #export headers into EPOCROOT
-    for(header, exportheaders.sources) {
-        BLD_INF_RULES.prj_exports += "$$header $$deploy.path$$exportheaders.path/$$basename(header)"
-    }
+    
+    # export headers into EPOCROOT
+    for(header, exportheaders.sources):BLD_INF_RULES.prj_exports += "$$header $$deploy.path$$exportheaders.path/$$basename(header)"
 }
-
 include(../../features/deploy.pri)
-
---
+++ src/feedback/qfeedbackcontroller.h
@@ -0,0 +1,102 @@
+/****************************************************************************
+**
+** Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the Qt Mobility Components.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** No Commercial Usage
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the Technology Preview License Agreement accompanying
+** this package.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights.  These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at qt-info@nokia.com.
+**
+**
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+
+#ifndef QFEEDBACKCONTROLLER_H
+#define QFEEDBACKCONTROLLER_H
+
+#include <QObject>
+
+#include <qmobilityglobal.h>
+#include <qfeedbackeffect.h>
+
+QTM_BEGIN_NAMESPACE
+
+class QFeedbackEffect;
+class Q_FEEDBACK_EXPORT QFeedbackController : public QObject
+{
+    Q_OBJECT
+public:
+    explicit QFeedbackController(QObject *parent = 0);
+
+    // Default factory (smart system controller)
+    static QFeedbackController* defaultController();
+
+    // All controllers (e.g. attached gamepads, bluetooth/usb devices, piezo/eccentric mass engines)
+    static QList<QFeedbackController*> allControllers();
+
+    // Metadata
+    QList<QFeedbackEffect::EffectType> supportedEffectTypes(); // might need emulatedTypes
+
+    // Number of channels?? Or some way of describing links to another controller?
+    // (e.g. gamepad with two independent eccentric mass engines.. although
+    // that can just be handled with an effect that specifies the source location)
+
+
+    // Type of feedback, for those that care
+    enum FeedbackType {
+        VibrationTactile,
+        PiezoTactile,
+        Auditory,
+        Visual,
+        LinearForce,
+        RotatingForce,
+        Temperature // Not common yet :)
+    };
+
+    QList<FeedbackType> supportedFeedbackTypes() const;
+
+    enum Error {
+        NoError,
+        PermissionsError,
+        DeviceBusyError,
+    };
+    Error error() const;
+
+signals:
+
+public slots:
+    bool performEffect(const QFeedbackEffect& effect) const;
+};
+
+QTM_END_NAMESPACE
+
+#endif // QFEEDBACKCONTROLLER_H
