qtproject/qt-mobility

commit e6f674d849c8724e094f7a23642b3487f524d0c6

Fixed a filtering bug in samplephonebook and in symbian backend.

examples/samplephonebook/filterpage.cpp
@@ -79,12 +79,10 @@ FilterPage::FilterPage(QWidget* parent)
     filterableFields.append("Email");
     m_fieldCriteriaCombo->addItems(filterableFields);
 
-    QStringList criteriaTypes;
-    criteriaTypes.append("Exact Match");
-    criteriaTypes.append("Starts With");
-    criteriaTypes.append("Ends With");
-    criteriaTypes.append("Contains");
-    m_criteriaTypeCombo->addItems(criteriaTypes);
+    m_criteriaTypeCombo->addItem("Equals", QContactFilter::MatchExactly);
+    m_criteriaTypeCombo->addItem("Contains", QContactFilter::MatchContains);
+    m_criteriaTypeCombo->addItem("Starts with", QContactFilter::MatchStartsWith);
+    m_criteriaTypeCombo->addItem("Ends with", QContactFilter::MatchEndsWith);
 
     QStringList joinTypes;
     joinTypes.append("AND");
@@ -178,37 +176,9 @@ void FilterPage::addClicked()
     fil.setDetailDefinitionName(defName, fieldName);
     fil.setValue(m_valueCriteriaEdit->text());
 
-    QContactFilter::MatchFlags matchFlags;
-    switch (m_criteriaTypeCombo->currentIndex()) {
-        case 0:
-        {
-            matchFlags |= QContactFilter::MatchExactly;
-            exprMatch = "equals";
-        }
-        break;
-
-        case 1:
-        {
-            matchFlags |= QContactFilter::MatchStartsWith;
-            exprMatch = "starts with";
-        }
-        break;
-
-        case 2:
-        {
-            matchFlags |= QContactFilter::MatchEndsWith;
-            exprMatch = "ends with";
-        }
-        break;
-
-        default:
-        {
-            matchFlags |= QContactFilter::MatchContains;
-            exprMatch = "contains";
-        }
-        break;
-    }
-    fil.setMatchFlags(matchFlags);
+    QContactFilter::MatchFlags flag = m_criteriaTypeCombo->itemData(m_criteriaTypeCombo->currentIndex()).toInt();
+    fil.setMatchFlags(flag);
+    exprMatch = m_criteriaTypeCombo->currentText().toLower();
 
     // if OR then join with OR
     if (m_joinMethodCombo->currentIndex() == 1) {

plugins/contacts/symbian/src/cntsymbianfilterdbms.cpp
@@ -104,7 +104,6 @@ QList<QContactLocalId> CntSymbianFilter::contacts(
     QContactManager::Error &error)
 {
     QList<QContactLocalId> result;
-    filterSupportedFlag = true;
 
     // No need to proceed if some of the filters in the chain is not supported
     if(!filterSupportedFlag) return result;
@@ -211,55 +210,64 @@ bool CntSymbianFilter::filterSupported(const QContactFilter& filter)
  */
 CntAbstractContactFilter::FilterSupport CntSymbianFilter::filterSupportLevel(const QContactFilter& filter)
 {
-    FilterSupport filterSupported(NotSupported);
-
     if (filter.type() == QContactFilter::ContactDetailFilter) {
         const QContactDetailFilter &detailFilter = static_cast<const QContactDetailFilter &>(filter);
-        const QContactFilter::MatchFlags matchFlags = detailFilter.matchFlags();
+        QContactFilter::MatchFlags matchFlags = detailFilter.matchFlags();
         const QString defName = detailFilter.detailDefinitionName();
-        const int KMatchFlagsUnset(0);
-        QContactFilter::MatchFlags supportedFlags(KMatchFlagsUnset);
-        QContactFilter::MatchFlags preFilterFlags(KMatchFlagsUnset);
-        bool validDetailFilter(false);
 
         // Phone numbers
         if (defName == QContactPhoneNumber::DefinitionName) {
-            validDetailFilter = true;
-            supportedFlags = QContactFilter::MatchFlags(QContactFilter::MatchPhoneNumber);
-            preFilterFlags = QContactFilter::MatchFlags(QContactFilter::MatchEndsWith)
-                             | QContactFilter::MatchFlags(QContactFilter::MatchExactly);
+            
+            if (matchFlags == QContactFilter::MatchPhoneNumber) {
+                return Supported;
+            }
+            
+            if (matchFlags == QContactFilter::MatchExactly ||
+                matchFlags == QContactFilter::MatchEndsWith) {
+                return SupportedPreFilterOnly;
+            }
         // Names
         } else if (defName == QContactName::DefinitionName
                 || defName == QContactNickname::DefinitionName
                 || defName == QContactEmailAddress::DefinitionName) {
-            validDetailFilter = true;
-            supportedFlags = QContactFilter::MatchFlags(QContactFilter::MatchContains);
-            preFilterFlags = QContactFilter::MatchFlags(QContactFilter::MatchExactly)
-                             | QContactFilter::MatchFlags(QContactFilter::MatchStartsWith)
-                             | QContactFilter::MatchFlags(QContactFilter::MatchEndsWith)
-                             | QContactFilter::MatchFlags(QContactFilter::MatchCaseSensitive);
+
+            if (matchFlags == QContactFilter::MatchContains) {
+                return Supported;
+            }
+
+            // Don't care about case sensitivity flag because: 
+            // 1) We do not support it. 2) We are doing prefiltering only.
+            matchFlags &= ~QContactFilter::MatchFlags(QContactFilter::MatchCaseSensitive);
+            
+            if (matchFlags == QContactFilter::MatchExactly ||
+                matchFlags == QContactFilter::MatchContains ||
+                matchFlags == QContactFilter::MatchStartsWith ||
+                matchFlags == QContactFilter::MatchEndsWith) {
+                return SupportedPreFilterOnly;
+            }            
         // display label, this is a special case that contains several name
         // fields and company name
         //TODO: "unnamed" display label is not supported currently
         } else if (defName == QContactDisplayLabel::DefinitionName) {
-            validDetailFilter = true;
-            supportedFlags = QContactFilter::MatchFlags(QContactFilter::MatchStartsWith)
-                             | QContactFilter::MatchFlags(QContactFilter::MatchContains);
-            preFilterFlags = QContactFilter::MatchFlags(QContactFilter::MatchExactly)
-                             | QContactFilter::MatchFlags(QContactFilter::MatchEndsWith)
-                             | QContactFilter::MatchFlags(QContactFilter::MatchCaseSensitive);
-        }
-
-        if(validDetailFilter) {
-            if (matchFlags != QContactFilter::MatchFlags(QContactFilter::MatchExactly)
-                    && (matchFlags | supportedFlags) == supportedFlags ) {
-                filterSupported = Supported;
-            } else if ((matchFlags | preFilterFlags) == preFilterFlags ) {
-                filterSupported = SupportedPreFilterOnly;
+            
+            if (matchFlags == QContactFilter::MatchContains ||
+                matchFlags == QContactFilter::MatchStartsWith) {
+                return Supported;
             }
+            
+            // Don't care about case sensitivity flag because: 
+            // 1) We do not support it. 2) We are doing prefiltering only.
+            matchFlags &= ~QContactFilter::MatchFlags(QContactFilter::MatchCaseSensitive);
+            
+            if (matchFlags == QContactFilter::MatchExactly ||
+                matchFlags == QContactFilter::MatchContains ||
+                matchFlags == QContactFilter::MatchStartsWith ||
+                matchFlags == QContactFilter::MatchEndsWith) {
+                return SupportedPreFilterOnly;
+            }              
         }
     }
-    return filterSupported;
+    return NotSupported;
 }
 
 QList<QContactLocalId> CntSymbianFilter::filterContacts(

commit 5c5984d0c7793d923981eecf6cad92d08a83a5d5

Fix samplephonebook armv5 compile error.
I broke it with commit e6f674d849c8724e094f7a23642b3487f524d0c6.

examples/samplephonebook/filterpage.cpp
@@ -176,8 +176,8 @@ void FilterPage::addClicked()
     fil.setDetailDefinitionName(defName, fieldName);
     fil.setValue(m_valueCriteriaEdit->text());
 
-    QContactFilter::MatchFlags flag = m_criteriaTypeCombo->itemData(m_criteriaTypeCombo->currentIndex()).toInt();
-    fil.setMatchFlags(flag);
+    int flag = m_criteriaTypeCombo->itemData(m_criteriaTypeCombo->currentIndex()).toInt();
+    fil.setMatchFlags(QContactFilter::MatchFlags(flag));
     exprMatch = m_criteriaTypeCombo->currentText().toLower();
 
     // if OR then join with OR