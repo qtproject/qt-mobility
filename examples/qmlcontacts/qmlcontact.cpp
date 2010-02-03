d760bc67c62e27f6ef90cae92626531f9870f3ff
Chris Adams <christopher.adams@nokia.com>
2010-02-03 13:28:22 +1000

    Fix namespacing issues with qmlexample, add correct license info.

--- examples/qmlcontacts/qmlcontact.cpp
+++ examples/qmlcontacts/qmlcontact.cpp
@@ -1,3 +1,44 @@
+/****************************************************************************
+**
+** Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the Qt Mobility Components.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** No Commercial Usage
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the Technology Preview License Agreement accompanying
+** this package.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights.  These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at qt-info@nokia.com.
+**
+**
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
 #include "qmlcontact.h"
 #include <qcontactdetails.h>
 #include <QtDebug>
@@ -5,8 +46,6 @@
 
 QT_USE_NAMESPACE
 QTM_USE_NAMESPACE
-
-QTM_BEGIN_NAMESPACE
 
 QmlContact::QmlContact(QContact& contact, QObject *parent)
     : QObject(parent), m_contact(contact)
@@ -161,5 +200,5 @@ QVariantMap QmlContact::values(QString definitionId)
 }
 
 #include "moc_qmlcontact.cpp"
-QTM_END_NAMESPACE
+
 QML_DEFINE_TYPE(QmlContact, 1, 0, QmlContact, QmlContact)
