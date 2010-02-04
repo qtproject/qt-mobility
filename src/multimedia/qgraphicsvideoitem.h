0ba6302fb2764bda0d0440c082eb989ffcd7f143
Andrew den Exter <andrew.den-exter@nokia.com>
2010-02-04 16:48:18 +1000

    Remove the FillMode enum from QGraphicsVideoItem.

--- src/multimedia/qgraphicsvideoitem.h
+++ src/multimedia/qgraphicsvideoitem.h
@@ -62,13 +62,6 @@ class  Q_MEDIA_EXPORT QGraphicsVideoItem : public QGraphicsObject
     Q_PROPERTY(QSizeF size READ size WRITE setSize)
     Q_PROPERTY(QSizeF nativeSize READ nativeSize NOTIFY nativeSizeChanged)
 public:
-    enum FillMode
-    {
-        Stretch,
-        PreserveAspectFit,
-        PreserveAspectCrop
-    };
-
     QGraphicsVideoItem(QGraphicsItem *parent = 0);
     ~QGraphicsVideoItem();
 
