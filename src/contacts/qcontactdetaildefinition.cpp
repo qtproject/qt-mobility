qtproject/qt-mobility

commit 9f042428bfc3793ed45ac5c039c0336d1fc91180

Update documentation with deprecation flags.
Also removed asterisk borders from function documentation.
Linked to the versit module documentation correctly from contacts,
future and preview documentation pages.

src/contacts/qcontactdetaildefinition.cpp
@@ -45,30 +45,30 @@
 QTM_BEGIN_NAMESPACE
 
 /*!
- * \class QContactDetailDefinition
- *
- * The QContactDetailDefinition class provides the specification for
- * a detail that can be included in any particular QContact.
- * The definition does not include any data, but defines
- * the semantics of the representation and use of data
- * details that are stored in a QContact.
+  \class QContactDetailDefinition
+
+  The QContactDetailDefinition class provides the specification for
+  a detail that can be included in any particular QContact.
+  The definition does not include any data, but defines
+  the semantics of the representation and use of data
+  details that are stored in a QContact.
  */
 
 /*!
- * \fn QContactDetailDefinition::operator!=(const QContactDetailDefinition& other) const
- * Returns true if this detail definition has different allowable field types or uniqueness to the \a other definition
+  \fn QContactDetailDefinition::operator!=(const QContactDetailDefinition& other) const
+  Returns true if this detail definition has different allowable field types or uniqueness to the \a other definition
  */
 
 /*!
- * \enum QContactDetailDefinition::AccessConstraint
- *
- * \obsolete
- *
- * This enum defines the access constraints which may be set on all details of this definition in the store for which the definition is valid.
- *
- * \value NoConstraint Details of definitions with this access constraint set have no special access semantics associated with them.  Users can read, write, and otherwise modify such details in any manner.
- * \value ReadOnly Details of definitions with this access constraint set are dynamically modified by the backend.  Users cannot write values to details of definitions with this access constraint set.
- * \value CreateOnly Details of definitions with this access constraint set are static once created.  Their value cannot be changed dynamically, nor can they be written or read by users.
+  \enum QContactDetailDefinition::AccessConstraint
+
+  \obsolete
+
+  This enum defines the access constraints which may be set on all details of this definition in the store for which the definition is valid.
+
+  \value NoConstraint Details of definitions with this access constraint set have no special access semantics associated with them.  Users can read, write, and otherwise modify such details in any manner.
+  \value ReadOnly Details of definitions with this access constraint set are dynamically modified by the backend.  Users cannot write values to details of definitions with this access constraint set.
+  \value CreateOnly Details of definitions with this access constraint set are static once created.  Their value cannot be changed dynamically, nor can they be written or read by users.
  */
 
 /*! Construct a new, invalid QContactDetailDefinition */
@@ -124,9 +124,9 @@ void QContactDetailDefinition::setName(const QString& definitionName)
 }
 
 /*!
- * Sets whether a contact can have more than one detail of this type.
- * If \a unique is true, only one detail of this type can be added.
- * Otherwise, any number can be added.
+  Sets whether a contact can have more than one detail of this type.
+  If \a unique is true, only one detail of this type can be added.
+  Otherwise, any number can be added.
  */
 void QContactDetailDefinition::setUnique(bool unique)
 {
@@ -171,22 +171,23 @@ void QContactDetailDefinition::removeField(const QString& key)
 }
 
 /*!
- * \deprecated
- * Returns a reference to the map of keys to fields which are present in details of this definition.
- *
- * You can make changes to the returned map.
+  \fn Q_DECL_DEPRECATED QContactDetailDefinition::fields()
+  \deprecated
+  Returns a reference to the map of keys to fields which are present in details of this definition.
+
+  You can make changes to the returned map.
  */
-QMap<QString, QContactDetailFieldDefinition>& QContactDetailDefinition::fields()
+QMap<QString, QContactDetailFieldDefinition>& Q_DECL_DEPRECATED QContactDetailDefinition::fields()
 {
     return d->m_fields;
 }
 
 /*!
- * Returns the access constraint that is applied to details of this definition
- *
- * \obsolete
- *
- * This function is obsolete - use \l QContactDetail::accessConstraints()
+  Returns the access constraint that is applied to details of this definition
+
+  \obsolete
+
+  This function is obsolete - use \l QContactDetail::accessConstraints()
  */
 QContactDetailDefinition::AccessConstraint QContactDetailDefinition::accessConstraint() const
 {
@@ -194,11 +195,11 @@ QContactDetailDefinition::AccessConstraint QContactDetailDefinition::accessConst
 }
 
 /*!
- * Sets the access constraint that is applied to details of this definition to \a constraint
- *
- * \obsolete
- *
- * This function is obsolete.
+  Sets the access constraint that is applied to details of this definition to \a constraint
+
+  \obsolete
+
+  This function is obsolete.
  */
 void QContactDetailDefinition::setAccessConstraint(const QContactDetailDefinition::AccessConstraint& constraint)
 {

doc/src/contacts.qdoc
@@ -326,7 +326,7 @@ reported to clients through various functions provided by the \l{QContactManager
 
 \section2 Synchronization and Serialization
 
-The contacts API is used by another Qt Mobility module: the \l {Versit API} {Versit}* module.  It allows
+The contacts API is used by another Qt Mobility module: the \l {Versit API (technology preview)} {Versit}* module.  It allows
 serialization of a QContact into a vCard document, and vice versa.
 
 [*] Versit \reg is a trademark of the Internet Mail Consortium.

doc/src/future.qdoc
@@ -50,7 +50,7 @@
 \target {Management of Versit Documents}
 \section1 Management of Versit Documents
 
-\l{Versit API}{Versit API Documentation}
+\l{Versit API (technology preview)}{Versit API Documentation}
 
 Versit Classes.
 \annotatedlist versit

doc/src/preview.qdoc
@@ -50,7 +50,7 @@
 \target {Management of Versit Documents}
 \section1 Management of Versit Documents
 
-\l{Versit API}{Versit API Documentation}
+\l{Versit API (technology preview)}{Versit API Documentation}
 
 Versit Classes.
 \annotatedlist versit

src/contacts/filters/qcontactrelationshipfilter.cpp
@@ -80,20 +80,20 @@ QTM_BEGIN_NAMESPACE
 Q_IMPLEMENT_CONTACTFILTER_PRIVATE(QContactRelationshipFilter)
 
 /*!
- * \enum QContactRelationshipFilter::Role
- * Describes the roles that a contact may take in a relationship
- * \value First The contact is the first contact in the relationship
- * \value Second The contact is the second contact in the relationship
- * \value Either The contact is either the first or second contact in the relationship
+  \enum QContactRelationshipFilter::Role
+  Describes the roles that a contact may take in a relationship
+  \value First The contact is the first contact in the relationship
+  \value Second The contact is the second contact in the relationship
+  \value Either The contact is either the first or second contact in the relationship
  */
 
 /*!
- * \fn QContactRelationshipFilter::QContactRelationshipFilter(const QContactFilter& other)
- * Constructs a copy of \a other if possible, else constructs a new QContactRelationshipFilter.
+  \fn QContactRelationshipFilter::QContactRelationshipFilter(const QContactFilter& other)
+  Constructs a copy of \a other if possible, else constructs a new QContactRelationshipFilter.
  */
 
 /*!
- * Constructs a new relationship filter
+  Constructs a new relationship filter
  */
 QContactRelationshipFilter::QContactRelationshipFilter()
     : QContactFilter(new QContactRelationshipFilterPrivate)
@@ -101,7 +101,7 @@ QContactRelationshipFilter::QContactRelationshipFilter()
 }
 
 /*!
- * Sets the type of relationship which a contact must have in order to match this filter to \a relationshipType
+  Sets the type of relationship which a contact must have in order to match this filter to \a relationshipType
  */
 void QContactRelationshipFilter::setRelationshipType(const QString& relationshipType)
 {
@@ -110,7 +110,7 @@ void QContactRelationshipFilter::setRelationshipType(const QString& relationship
 }
 
 /*!
- * Returns the type of relationship that a contact must have in order to match the filter
+  Returns the type of relationship that a contact must have in order to match the filter
  */
 QString QContactRelationshipFilter::relationshipType() const
 {
@@ -119,7 +119,7 @@ QString QContactRelationshipFilter::relationshipType() const
 }
 
 /*!
- * Sets the id of the contact with whom the tested contact must have a relationship in order for the tested contact to match this filter to be \a relatedContactId
+  Sets the id of the contact with whom the tested contact must have a relationship in order for the tested contact to match this filter to be \a relatedContactId
  */
 void QContactRelationshipFilter::setRelatedContactId(const QContactId &relatedContactId)
 {
@@ -128,7 +128,7 @@ void QContactRelationshipFilter::setRelatedContactId(const QContactId &relatedCo
 }
 
 /*!
- * Returns the id of the contact with whom the tested contact must have a relationship in order for the tested contact to match this filter
+  Returns the id of the contact with whom the tested contact must have a relationship in order for the tested contact to match this filter
  */
 QContactId QContactRelationshipFilter::relatedContactId() const
 {
@@ -137,7 +137,7 @@ QContactId QContactRelationshipFilter::relatedContactId() const
 }
 
 /*!
- * Sets the role in the relationship with the tested contact that the related contact must play in order for the tested contact to match this filter to be \a relatedContactRole
+  Sets the role in the relationship with the tested contact that the related contact must play in order for the tested contact to match this filter to be \a relatedContactRole
  */
 void QContactRelationshipFilter::setRelatedContactRole(QContactRelationshipFilter::Role relatedContactRole)
 {
@@ -146,7 +146,7 @@ void QContactRelationshipFilter::setRelatedContactRole(QContactRelationshipFilte
 }
 
 /*!
- * Returns the role in the relationship with the tested contact that the related contact must play in order for the tested contact to match this filter
+  Returns the role in the relationship with the tested contact that the related contact must play in order for the tested contact to match this filter
  */
 QContactRelationshipFilter::Role QContactRelationshipFilter::relatedContactRole() const
 {
@@ -155,14 +155,15 @@ QContactRelationshipFilter::Role QContactRelationshipFilter::relatedContactRole(
 }
 
 /*!
- * \deprecated
- * Sets the role in the relationship that a contact must be in order to match this filter to \a roleInRelationship
- *
- * This function has been deprecated - you should pass the opposite value (e.g. First instead of Second, Second
- * instead of First) to \c setRelatedContactRole().
- * \sa setRelatedContactRole()
+  \fn Q_DECL_DEPRECATED QContactRelationshipFilter::setRole(QContactRelationshipFilter::Role roleInRelationship)
+  \deprecated
+  Sets the role in the relationship that a contact must be in order to match this filter to \a roleInRelationship
+
+  This function has been deprecated - you should pass the opposite value (e.g. First instead of Second, Second
+  instead of First) to \c setRelatedContactRole().
+  \sa setRelatedContactRole()
  */
-void QContactRelationshipFilter::setRole(QContactRelationshipFilter::Role roleInRelationship)
+void Q_DECL_DEPRECATED QContactRelationshipFilter::setRole(QContactRelationshipFilter::Role roleInRelationship)
 {
     Q_D(QContactRelationshipFilter);
     switch(roleInRelationship) {
@@ -179,21 +180,23 @@ void QContactRelationshipFilter::setRole(QContactRelationshipFilter::Role roleIn
 }
 
 /*!
- * \deprecated
- * Sets the contact id of the other participant which must be present in the relationship with the contact
- * in order for the contact to match the filter to be \a id
+  \fn Q_DECL_DEPRECATED QContactRelationshipFilter::setOtherParticipantId(const QContactId& id)
+  \deprecated
+  Sets the contact id of the other participant which must be present in the relationship with the contact
+  in order for the contact to match the filter to be \a id
  */
-void QContactRelationshipFilter::setOtherParticipantId(const QContactId& id)
+void Q_DECL_DEPRECATED QContactRelationshipFilter::setOtherParticipantId(const QContactId& id)
 {
     Q_D(QContactRelationshipFilter);
     d->m_relatedContactId = id;
 }
 
 /*!
- * \deprecated
- * Returns the role that a contact must have in a relationship in order to match the filter
+  \fn Q_DECL_DEPRECATED QContactRelationshipFilter::role() const
+  \deprecated
+  Returns the role that a contact must have in a relationship in order to match the filter
  */
-QContactRelationshipFilter::Role QContactRelationshipFilter::role() const
+QContactRelationshipFilter::Role Q_DECL_DEPRECATED QContactRelationshipFilter::role() const
 {
     Q_D(const QContactRelationshipFilter);
     switch(d->m_relatedContactRole) {
@@ -208,10 +211,11 @@ QContactRelationshipFilter::Role QContactRelationshipFilter::role() const
 }
 
 /*!
- * \deprecated
- * Returns the id of another contact with whom a contact must have a relationship in order to match the filter
+  \fn Q_DECL_DEPRECATED QContactRelationshipFilter::otherParticipantId() const
+  \deprecated
+  Returns the id of another contact with whom a contact must have a relationship in order to match the filter
  */
-QContactId QContactRelationshipFilter::otherParticipantId() const
+QContactId Q_DECL_DEPRECATED QContactRelationshipFilter::otherParticipantId() const
 {
     Q_D(const QContactRelationshipFilter);
     return d->m_relatedContactId;

src/contacts/qcontactabstractrequest.cpp
@@ -60,60 +60,60 @@ QTM_BEGIN_NAMESPACE
  */
 
 /*!
- * \fn QContactAbstractRequest::stateChanged(QContactAbstractRequest::State newState)
- * This signal is emitted when the state of the request is changed.  The new state of
- * the request will be contained in \a newState.
+  \fn QContactAbstractRequest::stateChanged(QContactAbstractRequest::State newState)
+  This signal is emitted when the state of the request is changed.  The new state of
+  the request will be contained in \a newState.
  */
 
 
 /*!
- * \fn QContactAbstractRequest::resultsAvailable()
- * This signal is emitted when new results are available.  Results can include
- * the operation error which may be accessed via error(), or derived-class-specific
- * results which are accessible through the derived class API.
- *
- * \sa error()
+  \fn QContactAbstractRequest::resultsAvailable()
+  This signal is emitted when new results are available.  Results can include
+  the operation error which may be accessed via error(), or derived-class-specific
+  results which are accessible through the derived class API.
+
+  \sa error()
  */
 
 /*!
- * \enum QContactAbstractRequest::RequestType
- * Enumerates the various possible types of asynchronous requests
- * \value InvalidRequest An invalid request
- * \value ContactFetchRequest A request to fetch a list of contacts
- * \value ContactLocalIdFetchRequest A request to fetch a list of local contact ids
- * \value ContactRemoveRequest A request to remove a list of contacts
- * \value ContactSaveRequest A request to save a list of contacts
- * \value DetailDefinitionFetchRequest A request to fetch a collection of detail definitions
- * \value DetailDefinitionRemoveRequest A request to remove a list of detail definitions
- * \value DetailDefinitionSaveRequest A request to save a list of detail definitions
- * \value RelationshipFetchRequest A request to fetch relationships between contacts
- * \value RelationshipRemoveRequest A request to remove any relationships which match the request criteria
- * \value RelationshipSaveRequest A request to save a list of relationships
+  \enum QContactAbstractRequest::RequestType
+  Enumerates the various possible types of asynchronous requests
+  \value InvalidRequest An invalid request
+  \value ContactFetchRequest A request to fetch a list of contacts
+  \value ContactLocalIdFetchRequest A request to fetch a list of local contact ids
+  \value ContactRemoveRequest A request to remove a list of contacts
+  \value ContactSaveRequest A request to save a list of contacts
+  \value DetailDefinitionFetchRequest A request to fetch a collection of detail definitions
+  \value DetailDefinitionRemoveRequest A request to remove a list of detail definitions
+  \value DetailDefinitionSaveRequest A request to save a list of detail definitions
+  \value RelationshipFetchRequest A request to fetch relationships between contacts
+  \value RelationshipRemoveRequest A request to remove any relationships which match the request criteria
+  \value RelationshipSaveRequest A request to save a list of relationships
  */
 
 /*!
- * \enum QContactAbstractRequest::Status
- * \deprecated
- * Enumerates the various states that a request may be in at any given time.  Deprecated - use QContactAbstractRequest::State instead!
- * \value Inactive Operation not yet started
- * \value Active Operation started, not yet finished
- * \value Cancelling Operation started then cancelled, not yet finished
- * \value Cancelled Operation is finished due to cancellation
- * \value Finished Operation successfully completed
+  \enum QContactAbstractRequest::Status
+  \deprecated
+  Enumerates the various states that a request may be in at any given time.  Deprecated - use QContactAbstractRequest::State instead!
+  \value Inactive Operation not yet started
+  \value Active Operation started, not yet finished
+  \value Cancelling Operation started then cancelled, not yet finished
+  \value Cancelled Operation is finished due to cancellation
+  \value Finished Operation successfully completed
  */
 
 /*!
- * \enum QContactAbstractRequest::State
- * Enumerates the various states that a request may be in at any given time
- * \value InactiveState Operation not yet started
- * \value ActiveState Operation started, not yet finished
- * \value CanceledState Operation is finished due to cancellation
- * \value FinishedState Operation successfully completed
+  \enum QContactAbstractRequest::State
+  Enumerates the various states that a request may be in at any given time
+  \value InactiveState Operation not yet started
+  \value ActiveState Operation started, not yet finished
+  \value CanceledState Operation is finished due to cancellation
+  \value FinishedState Operation successfully completed
  */
 
 /*!
- * \fn QContactAbstractRequest::QContactAbstractRequest()
- * Constructs a new, invalid asynchronous request
+  \fn QContactAbstractRequest::QContactAbstractRequest()
+  Constructs a new, invalid asynchronous request
  */
 
 /*! Constructs a new request from the given request data \a otherd */
@@ -136,9 +136,9 @@ QContactAbstractRequest::~QContactAbstractRequest()
 }
 
 /*!
- * Returns true if the request is in the \c QContactAbstractRequest::InactiveState state; otherwise, returns false
- *
- * \sa state()
+  Returns true if the request is in the \c QContactAbstractRequest::InactiveState state; otherwise, returns false
+
+  \sa state()
  */
 bool QContactAbstractRequest::isInactive() const
 {
@@ -146,9 +146,9 @@ bool QContactAbstractRequest::isInactive() const
 }
 
 /*!
- * Returns true if the request is in the \c QContactAbstractRequest::ActiveState state; otherwise, returns false
- *
- * \sa state()
+  Returns true if the request is in the \c QContactAbstractRequest::ActiveState state; otherwise, returns false
+
+  \sa state()
  */
 bool QContactAbstractRequest::isActive() const
 {
@@ -156,9 +156,9 @@ bool QContactAbstractRequest::isActive() const
 }
 
 /*!
- * Returns true if the request is in the \c QContactAbstractRequest::FinishedState; otherwise, returns false
- *
- * \sa state()
+  Returns true if the request is in the \c QContactAbstractRequest::FinishedState; otherwise, returns false
+
+  \sa state()
  */
 bool QContactAbstractRequest::isFinished() const
 {
@@ -166,9 +166,9 @@ bool QContactAbstractRequest::isFinished() const
 }
 
 /*!
- * Returns true if the request is in the \c QContactAbstractRequest::CanceledState; otherwise, returns false
- *
- * \sa state()
+  Returns true if the request is in the \c QContactAbstractRequest::CanceledState; otherwise, returns false
+
+  \sa state()
  */
 bool QContactAbstractRequest::isCanceled() const
 {
@@ -182,16 +182,17 @@ QContactManager::Error QContactAbstractRequest::error() const
 }
 
 /*!
- * \deprecated
- * Returns the list of errors which occurred during the most recent asynchronous operation.  Each individual error in the list corresponds to a result in the result list.
+  \fn Q_DECL_DEPRECATED QContactAbstractRequest::errors() const
+  \deprecated
+  Returns the list of errors which occurred during the most recent asynchronous operation.  Each individual error in the list corresponds to a result in the result list.
  */
-QList<QContactManager::Error> QContactAbstractRequest::errors() const
+QList<QContactManager::Error> Q_DECL_DEPRECATED QContactAbstractRequest::errors() const
 {
     return QList<QContactManager::Error>();
 }
 
 /*!
- * Returns the type of this asynchronous request
+  Returns the type of this asynchronous request
  */
 QContactAbstractRequest::RequestType QContactAbstractRequest::type() const
 {
@@ -199,9 +200,9 @@ QContactAbstractRequest::RequestType QContactAbstractRequest::type() const
 }
 
 /*!
- * \fn Q_DECL_DEPRECATED QContactAbstractRequest::status() const
- * \deprecated
- * Returns the current status of the request.
+  \fn Q_DECL_DEPRECATED QContactAbstractRequest::status() const
+  \deprecated
+  Returns the current status of the request.
  */
 QContactAbstractRequest::Status Q_DECL_DEPRECATED QContactAbstractRequest::status() const
 {
@@ -209,7 +210,7 @@ QContactAbstractRequest::Status Q_DECL_DEPRECATED QContactAbstractRequest::statu
 }
 
 /*!
- * Returns the current state of the request.
+  Returns the current state of the request.
  */
 QContactAbstractRequest::State QContactAbstractRequest::state() const
 {

commit 40c449f6ae52977da4efcb6f589065bd6d519e53

Fix compile error caused by 9f042428
The reference modifier must come after the deprecation tag in fn sign.

src/contacts/qcontactdetaildefinition.cpp
@@ -177,7 +177,7 @@ void QContactDetailDefinition::removeField(const QString& key)
 
   You can make changes to the returned map.
  */
-QMap<QString, QContactDetailFieldDefinition>& Q_DECL_DEPRECATED QContactDetailDefinition::fields()
+QMap<QString, QContactDetailFieldDefinition> Q_DECL_DEPRECATED & QContactDetailDefinition::fields()
 {
     return d->m_fields;
 }