qtproject/qt-mobility

commit 38afacc931e2acdd9a941ee5727354c4bfe0adaa

Fixed failing Contacts filtering test.

src/contacts/engines/qcontactmemorybackend.cpp
@@ -570,7 +570,13 @@ QList<QContactRelationship> QContactMemoryEngine::relationships(const QString& r
 bool QContactMemoryEngine::saveRelationship(QContactRelationship* relationship, QContactChangeSet& changeSet, QContactManager::Error& error)
 {
     // Attempt to validate the relationship.
-    // first, check that the source contact exists and is in this manager.
+    // first, check the relationship type is valid
+    if (!supportedRelationshipTypes(QString()).contains(relationship->relationshipType())) {
+        error = QContactManager::NotSupportedError;
+        return false;
+    }
+
+    // second, check that the source contact exists and is in this manager.
     QString myUri = managerUri();
     if ((!relationship->first().managerUri().isEmpty() && relationship->first().managerUri() != myUri)
             ||!d->m_contactIds.contains(relationship->first().localId())) {
@@ -578,7 +584,7 @@ bool QContactMemoryEngine::saveRelationship(QContactRelationship* relationship, 
         return false;
     }
 
-    // second, check that the second contact exists (if it's local); we cannot check other managers' contacts.
+    // third, check that the second contact exists (if it's local); we cannot check other managers' contacts.
     QContactId dest = relationship->second();
 
     if (dest.managerUri().isEmpty() || dest.managerUri() == myUri) {
@@ -1132,6 +1138,7 @@ QStringList QContactMemoryEngine::supportedRelationshipTypes(const QString& cont
         << QContactRelationship::Aggregates
         << QContactRelationship::IsSameAs
         << QContactRelationship::HasAssistant
+        << QContactRelationship::HasManager
         << QContactRelationship::HasSpouse;
 }
 

tests/auto/qcontactmanagerfiltering/tst_qcontactmanagerfiltering.cpp
@@ -1730,6 +1730,11 @@ void tst_QContactManagerFiltering::relationshipFiltering_data()
         QTest::newRow("RF-17") << manager << static_cast<int>(QContactRelationshipFilter::First) << QString(QLatin1String(QContactRelationship::HasSpouse)) << static_cast<unsigned int>(0) << QString() << "b";
         QTest::newRow("RF-18") << manager << static_cast<int>(QContactRelationshipFilter::Either) << QString(QLatin1String(QContactRelationship::HasSpouse)) << static_cast<unsigned int>(0) << QString() << "ab";
 
+        // Unknown relationship
+        QTest::newRow("RF-19") << manager << static_cast<int>(QContactRelationshipFilter::Second) << QString(QLatin1String("UnknownRelationship")) << static_cast<unsigned int>(0) << QString() << "";
+        QTest::newRow("RF-20") << manager << static_cast<int>(QContactRelationshipFilter::First) << QString(QLatin1String("UnknownRelationship")) << static_cast<unsigned int>(0) << QString() << "";
+        QTest::newRow("RF-21") << manager << static_cast<int>(QContactRelationshipFilter::Either) << QString(QLatin1String("UnknownRelationship")) << static_cast<unsigned int>(0) << QString() << "";
+
         // match any contact that is the related contact in a relationship with contact-A
         //QTest::newRow("RF-19") << manager << static_cast<int>(QContactRelationshipFilter::Second) << QString() << static_cast<unsigned int>(contactAId.value(manager).localId()) << contactAId.value(manager).managerUri() << "h";
         // match any contact has contact-A as the related contact

commit a945af127ab161e79f22d9defbb8730e29c17693

Fixed regression caused by 38afacc931e2acdd9a941ee5727354c4bfe0adaa

src/contacts/engines/qcontactmemorybackend.cpp
@@ -570,13 +570,7 @@ QList<QContactRelationship> QContactMemoryEngine::relationships(const QString& r
 bool QContactMemoryEngine::saveRelationship(QContactRelationship* relationship, QContactChangeSet& changeSet, QContactManager::Error& error)
 {
     // Attempt to validate the relationship.
-    // first, check the relationship type is valid
-    if (!supportedRelationshipTypes(QString()).contains(relationship->relationshipType())) {
-        error = QContactManager::NotSupportedError;
-        return false;
-    }
-
-    // second, check that the source contact exists and is in this manager.
+    // first, check that the source contact exists and is in this manager.
     QString myUri = managerUri();
     if ((!relationship->first().managerUri().isEmpty() && relationship->first().managerUri() != myUri)
             ||!d->m_contactIds.contains(relationship->first().localId())) {
@@ -584,7 +578,7 @@ bool QContactMemoryEngine::saveRelationship(QContactRelationship* relationship, 
         return false;
     }
 
-    // third, check that the second contact exists (if it's local); we cannot check other managers' contacts.
+    // second, check that the second contact exists (if it's local); we cannot check other managers' contacts.
     QContactId dest = relationship->second();
 
     if (dest.managerUri().isEmpty() || dest.managerUri() == myUri) {

tests/auto/qcontactmanagerfiltering/tst_qcontactmanagerfiltering.cpp
@@ -1793,10 +1793,14 @@ void tst_QContactManagerFiltering::relationshipFiltering()
     h2i.setFirst(firstId);
     h2i.setSecond(secondId);
     h2i.setRelationshipType(relationshipType);
-
+    if (relationshipType == "UnknownRelationship")
+        qDebug() << "hi mum";
     // save and check error code
-    if(cm->hasFeature(QContactManager::Relationships)
-        && cm->supportedRelationshipTypes().contains(relationshipType)) {
+    bool succeeded = false;
+    if((cm->hasFeature(QContactManager::Relationships)
+        && cm->supportedRelationshipTypes().contains(relationshipType))
+        || cm->hasFeature(QContactManager::ArbitraryRelationshipTypes)) {
+        succeeded = true;
         QVERIFY(cm->saveRelationship(&h2i));
         QCOMPARE(cm->error(), QContactManager::NoError);
     } else {
@@ -1822,8 +1826,7 @@ void tst_QContactManagerFiltering::relationshipFiltering()
     QString output = convertIds(contacts, ids);
 
     // 5. Remove the created relationship and contacts
-    if(cm->hasFeature(QContactManager::Relationships)
-        && cm->supportedRelationshipTypes().contains(relationshipType)) {
+    if(succeeded) {
         // Check that an existing relationship can be removed
         QVERIFY(cm->removeRelationship(h2i));
         QCOMPARE(cm->error(), QContactManager::NoError);