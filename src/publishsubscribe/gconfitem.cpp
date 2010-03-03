a9f545c0c6f2d9e524ace5dee264fb35fcab4828
Jaakko Vuori <qt-info@nokia.com>
2010-03-03 14:02:26 +0200

    Updated GConfItem wrapper to newer version

--- src/publishsubscribe/gconfitem.cpp
+++ src/publishsubscribe/gconfitem.cpp
@@ -1,43 +1,25 @@
-/****************************************************************************
-**
-** Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies).
-** All rights reserved.
-** Contact: Nokia Corporation (qt-info@nokia.com)
-**
-** This file is part of the Qt Mobility Components.
-**
-** $QT_BEGIN_LICENSE:LGPL$
-** No Commercial Usage
-** This file contains pre-release code and may not be distributed.
-** You may use this file in accordance with the terms and conditions
-** contained in the Technology Preview License Agreement accompanying
-** this package.
-**
-** GNU Lesser General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU Lesser
-** General Public License version 2.1 as published by the Free Software
-** Foundation and appearing in the file LICENSE.LGPL included in the
-** packaging of this file.  Please review the following information to
-** ensure the GNU Lesser General Public License version 2.1 requirements
-** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-** In addition, as a special exception, Nokia gives you certain additional
-** rights.  These rights are described in the Nokia Qt LGPL Exception
-** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
-**
-** If you have questions regarding the use of this file, please contact
-** Nokia at qt-info@nokia.com.
-**
-**
-**
-**
-**
-**
-**
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
+/* * This file is part of libgq *
+ *
+ * Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies).
+ * All rights reserved.
+ *
+ * Contact: Marius Vollmer <marius.vollmer@nokia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
 
 #include <QString>
 #include <QStringList>
@@ -226,16 +208,22 @@ static int convertValue(const QVariant &src, GConfValue **valp)
 
 void GConfItemPrivate::notify_trampoline (GConfClient*,
                                              guint,
-                                             GConfEntry *,
+                                             GConfEntry *entry,
                                              gpointer data)
 {
     GConfItem *item = (GConfItem *)data;
-    item->update_value (true);
+
+
+    item->update_value (true, entry->key, convertValue(entry->value));
 }
 
-void GConfItem::update_value (bool emit_signal)
+void GConfItem::update_value (bool emit_signal, const QString& key, const QVariant& value)
 {
     QVariant new_value;
+
+    if (emit_signal) {
+        emit subtreeChanged(key, value);
+    }
 
     withClient(client) {
         GError *error = NULL;
@@ -350,7 +338,7 @@ GConfItem::GConfItem(const QString &key, QObject *parent)
     priv = new GConfItemPrivate;
     priv->key = key;
     withClient(client) {
-        update_value (false);
+        update_value (false, "", QVariant());
         QByteArray k = convertKey(priv->key);
         gconf_client_add_dir (client, k.data(), GCONF_CLIENT_PRELOAD_ONELEVEL, NULL);
         priv->notify_id = gconf_client_notify_add (client, k.data(),
