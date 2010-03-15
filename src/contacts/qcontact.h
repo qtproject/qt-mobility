465d4f6597f99125f130b05bcda35ead6727aecb
Chris Adams <christopher.adams@nokia.com>
2010-03-15 11:49:28 +1000

    Properly comment out the default argument of the relatedContacts() fn
    The default argument will be added back in after the deprecated
    function is removed (after transition period has elapsed).

--- src/contacts/qcontact.h
+++ src/contacts/qcontact.h
@@ -135,7 +135,7 @@ public:
 
     /* Relationships that this contact was involved in when it was retrieved from the manager */
     QList<QContactRelationship> relationships(const QString& relationshipType = QString()) const;
-    QList<QContactId> relatedContacts(const QString& relationshipType /* = QString() */, QContactRelationship::Role /* role = QContactRelationship::Either */) const;
+    QList<QContactId> relatedContacts(const QString& relationshipType /* = QString() */, QContactRelationship::Role role /* = QContactRelationship::Either */) const;
     QList<QContactId> Q_DECL_DEPRECATED relatedContacts(const QString& relationshipType = QString(), QContactRelationshipFilter::Role role = QContactRelationshipFilter::Either) const;
     void Q_DECL_DEPRECATED setRelationshipOrder(const QList<QContactRelationship>& reordered);
     QList<QContactRelationship> Q_DECL_DEPRECATED relationshipOrder() const;
