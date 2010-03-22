450b7a9e8ca15d4fd09e174ff650f502f29fa5ec
David Laing <david.laing@nokia.com>
2010-03-22 16:33:26 +1000

    Fremantle fixes for errors and the what it means to be a fix.

--- src/location/qgeopositioninfosource_maemo5_p.h
+++ src/location/qgeopositioninfosource_maemo5_p.h
@@ -78,10 +78,12 @@ private:
     QTimer *updateTimer;
     QTimer *requestTimer;
     int timerInterval;
+    bool errorOccurred;
+    bool errorSent;
+
     void activateTimer();
     void startLocationDaemon();
     void stopLocationDaemon();
-    int lowSignalLevelCount;
     
     enum PositionInfoState {
         Undefined = 0,
@@ -91,7 +93,6 @@ private:
         RequestSingleShot = 8,
         PowersaveActive = 16,
         TogglePowersave = 32,
-        SignalLevelLow = 64,        
     };
     int positionInfoState;
     
@@ -106,6 +107,7 @@ public slots:
 
 private slots:
     void requestTimeoutElapsed();
+    void error();
 
 private:
     Q_DISABLE_COPY(QGeoPositionInfoSourceMaemo)
