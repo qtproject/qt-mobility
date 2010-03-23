qtproject/qt-mobility

commit 57d08c7ca2ab95f4fa590f6203e21855350fe289

Refactored samplephonebook to better support symbian SIM backend. (cherry picked from commit 4f4fe56d7ef746879ab082ba1f9ee43a6833e48e)

examples/samplephonebook/contacteditor.cpp
@@ -118,19 +118,17 @@ void ContactEditor::setCurrentContact(QContactManager* manager, QContactLocalId 
     m_manager = manager;
     m_contactId = currentId;
     m_newAvatarPath = QString();
-
-    if (manager == 0 || currentId == 0) {
-        // clear the UI and return.
-        m_nameEdit->setText("");
-        m_phoneEdit->setText("");
-        m_emailEdit->setText("");
-        m_addrEdit->setText("");
-        m_avatarBtn->setText("Add image");
-        m_avatarBtn->setIcon(QIcon());
-
-        if (manager == 0)
-            m_saveBtn->setEnabled(false);
-
+    
+    // Clear UI
+    m_nameEdit->setText("");
+    m_phoneEdit->setText("");
+    m_emailEdit->setText("");
+    m_addrEdit->setText("");
+    m_avatarBtn->setText("Add image");
+    m_avatarBtn->setIcon(QIcon());
+    
+    if (manager == 0) {
+        m_saveBtn->setEnabled(false);
         return;
     }
 
@@ -138,33 +136,65 @@ void ContactEditor::setCurrentContact(QContactManager* manager, QContactLocalId 
     m_saveBtn->setEnabled(true);
 
     // otherwise, build from the contact details.
-    QContact curr = manager->contact(m_contactId);
-    QContactName nm = curr.detail(QContactName::DefinitionName);
+    QContact curr;
+    if (m_contactId != QContactLocalId(0))
+        curr = manager->contact(m_contactId);
+    
+    // Disable fields & buttons according to what the backend supports
+    QMap<QString, QContactDetailDefinition> defs = m_manager->detailDefinitions(QContactType::TypeContact);
+    
+    // name
+    //QContactName nm = curr.detail(QContactName::DefinitionName);
+    if (m_contactId != QContactLocalId(0))
+        m_nameEdit->setText(manager->synthesizedDisplayLabel(curr));
+    
+    // phonenumber
     QContactPhoneNumber phn = curr.detail(QContactPhoneNumber::DefinitionName);
-    QContactEmailAddress em = curr.detail(QContactEmailAddress::DefinitionName);
-    QContactAddress adr = curr.detail(QContactAddress::DefinitionName);
-    QContactAvatar av = curr.detail(QContactAvatar::DefinitionName);
-    QContactThumbnail thumb = curr.detail(QContactThumbnail::DefinitionName);
-
-    m_nameEdit->setText(manager->synthesizedDisplayLabel(curr));
     m_phoneEdit->setText(phn.value(QContactPhoneNumber::FieldNumber));
-    m_emailEdit->setText(em.value(QContactEmailAddress::FieldEmailAddress));
-    m_addrEdit->setText(adr.value(QContactAddress::FieldStreet)); // ugly hack.
     
-    m_avatarBtn->setText(QString());
-    m_avatarBtn->setIcon(QIcon());
-    
-    if (thumb.thumbnail().isNull()) {
-        if (av.imageUrl().isEmpty()) {
-            m_avatarBtn->setText("Add image");
-        } else {
-            m_avatarBtn->setIcon(QIcon(QPixmap(av.imageUrl().toLocalFile())));
-            m_thumbnail = QImage(av.imageUrl().toLocalFile());
-        }
+    // email
+    if (defs.contains(QContactEmailAddress::DefinitionName)) {
+        QContactEmailAddress em = curr.detail(QContactEmailAddress::DefinitionName);
+        m_emailEdit->setText(em.value(QContactEmailAddress::FieldEmailAddress));
+        m_emailEdit->setReadOnly(false);
     } else {
-        m_newAvatarPath = av.imageUrl().toLocalFile();
-        m_thumbnail = thumb.thumbnail();
-        m_avatarBtn->setIcon(QIcon(QPixmap::fromImage(thumb.thumbnail())));
+        m_emailEdit->setText("<not supported>");
+        m_emailEdit->setReadOnly(true);
+    }
+    
+    // address
+    if (defs.contains(QContactAddress::DefinitionName)) {
+        QContactAddress adr = curr.detail(QContactAddress::DefinitionName);
+        m_addrEdit->setText(adr.value(QContactAddress::FieldStreet)); // ugly hack.
+        m_addrEdit->setReadOnly(false);
+    } else {
+        m_addrEdit->setText("<not supported>");
+        m_addrEdit->setReadOnly(true);
+    }    
+    
+    // avatar button
+    if (defs.contains(QContactAvatar::DefinitionName)) {
+        QContactAvatar av = curr.detail(QContactAvatar::DefinitionName);
+        QContactThumbnail thumb = curr.detail(QContactThumbnail::DefinitionName);    
+        m_avatarBtn->setText(QString());
+        m_avatarBtn->setIcon(QIcon());
+        if (thumb.thumbnail().isNull()) {
+            if (av.imageUrl().isEmpty()) {
+                m_avatarBtn->setText("Add image");
+            } else {
+                m_avatarBtn->setIcon(QIcon(QPixmap(av.imageUrl().toLocalFile())));
+                m_thumbnail = QImage(av.imageUrl().toLocalFile());
+            }
+        } else {
+            m_newAvatarPath = av.imageUrl().toLocalFile();
+            m_thumbnail = thumb.thumbnail();
+            m_avatarBtn->setIcon(QIcon(QPixmap::fromImage(thumb.thumbnail())));
+        }
+        m_avatarBtn->setDisabled(false);
+    } else {
+        m_avatarBtn->setIcon(QIcon());
+        m_avatarBtn->setText("<not supported>");
+        m_avatarBtn->setDisabled(true);
     }
 }
 
@@ -207,43 +237,54 @@ void ContactEditor::saveClicked()
         QContact curr;
         if (m_contactId != QContactLocalId(0))
             curr = m_manager->contact(m_contactId);
-        QContactName nm = curr.detail(QContactName::DefinitionName);
-        QContactPhoneNumber phn = curr.detail(QContactPhoneNumber::DefinitionName);
-        QContactEmailAddress em = curr.detail(QContactEmailAddress::DefinitionName);
-        QContactAddress adr = curr.detail(QContactAddress::DefinitionName);
-        QContactAvatar av = curr.detail(QContactAvatar::DefinitionName);
-        QContactThumbnail thumb = curr.detail(QContactThumbnail::DefinitionName);
-
-        QString saveNameField = nameField();
-        if (!saveNameField.isEmpty()) {
-            // if the name has changed (ie, is different to the synthed label) then save it as a custom label.
-            if (m_nameEdit->text() != m_manager->synthesizedDisplayLabel(curr)) {
-                nm.setValue(nameField(), m_nameEdit->text());
-            }
+        
+        if (m_nameEdit->text().isEmpty()) {
+            QMessageBox::information(this, "Failed!", "You must give a name for the contact!");
+            return;
         }
+        
+        if (m_nameEdit->text() != m_manager->synthesizedDisplayLabel(curr)) {
+            // if the name has changed (ie, is different to the synthed label) then save it as a custom label.
+            QString saveNameField = nameField();
+            if (!saveNameField.isEmpty()) {
+                QContactName nm = curr.detail(QContactName::DefinitionName);
+                nm.setValue(saveNameField, m_nameEdit->text());
+                curr.saveDetail(&nm);
+            }
+        }        
+        
+        QContactPhoneNumber phn = curr.detail(QContactPhoneNumber::DefinitionName);
         phn.setNumber(m_phoneEdit->text());
-        em.setEmailAddress(m_emailEdit->text());
-        adr.setStreet(m_addrEdit->text());
-        av.setImageUrl(QUrl::fromLocalFile(m_newAvatarPath));
-
-
-        QImage img(m_thumbnail);
-        thumb.setThumbnail(img);
-
-        curr.saveDetail(&nm);
         curr.saveDetail(&phn);
-        curr.saveDetail(&em);
-        curr.saveDetail(&adr);
-        curr.saveDetail(&av);
-        curr.saveDetail(&thumb);
+        
+        if (!m_emailEdit->isReadOnly()) {
+            QContactEmailAddress em = curr.detail(QContactEmailAddress::DefinitionName);
+            em.setEmailAddress(m_emailEdit->text());
+            curr.saveDetail(&em);
+        }
+        
+        if (!m_addrEdit->isReadOnly()) {
+            QContactAddress adr = curr.detail(QContactAddress::DefinitionName);
+            adr.setStreet(m_addrEdit->text());
+            curr.saveDetail(&adr);
+        }
+        
+        if (m_avatarBtn->isEnabled()) {
+            QContactAvatar av = curr.detail(QContactAvatar::DefinitionName);
+            av.setAvatar(m_newAvatarPath);
+            curr.saveDetail(&av);
+            
+            QContactThumbnail thumb = curr.detail(QContactThumbnail::DefinitionName);
+            QImage img(m_thumbnail);
+            thumb.setThumbnail(img);
+            curr.saveDetail(&thumb);
+        }
 
         bool success = m_manager->saveContact(&curr);
         if (success)
             QMessageBox::information(this, "Success!", "Contact saved successfully!");
-        else {
-            QMessageBox::information(this, "Failed!", "Failed to save contact!");
-            qDebug() << m_manager->error();
-        }
+        else
+            QMessageBox::information(this, "Failed!", QString("Failed to save contact!\n(error code %1)").arg(m_manager->error()));
     }
 
     emit showListPage();

examples/samplephonebook/contactlistpage.cpp
@@ -57,7 +57,39 @@ ContactListPage::ContactListPage(QWidget *parent)
 
     m_backendsCombo = new QComboBox(this);
     QStringList availableManagers = QContactManager::availableManagers();
-    m_backendsCombo->addItems(availableManagers);
+    
+    foreach(QString managerName, availableManagers) {
+
+        QMap<QString, QString> params;
+        QString managerUri = QContactManager::buildUri(managerName, params);
+
+        // Add some parameters to SIM backend so that we can use
+        // all the stores.
+        if (managerName == "symbiansim") {
+            availableManagers.removeAll("symbiansim");
+
+            availableManagers.append("symbiansim:adn");
+            params.insert("store", "ADN");
+            managerUri = QContactManager::buildUri(managerName, params);
+            m_availableManagers.insert(availableManagers.last(), managerUri);
+            
+            availableManagers.append("symbiansim:fdn");
+            params.clear();
+            params.insert("store", "FDN");
+            managerUri = QContactManager::buildUri(managerName, params);
+            m_availableManagers.insert(availableManagers.last(), managerUri);
+
+            availableManagers.append("symbiansim:sdn");
+            params.clear();
+            params.insert("store", "SDN");
+            managerUri = QContactManager::buildUri(managerName, params);
+            m_availableManagers.insert(availableManagers.last(), managerUri);
+        }
+        else {
+            m_availableManagers.insert(managerName, managerUri);
+        }
+    }
+    m_backendsCombo->addItems(m_availableManagers.keys());
     connect(m_backendsCombo, SIGNAL(currentIndexChanged(QString)), this, SLOT(backendSelected()));
     m_filterActiveLabel = new QLabel("Inactive");
     m_filterActiveLabel->setSizePolicy(QSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed));
@@ -117,18 +149,24 @@ ContactListPage::~ContactListPage()
 
 void ContactListPage::backendSelected()
 {
-    QString backend = m_backendsCombo->currentText();
+    QString managerUri = m_availableManagers.value(m_backendsCombo->currentText());
 
     // first, check to see if they reselected the same backend.
-    if (m_manager && m_manager->managerName() == backend)
+    if (m_manager && m_manager->managerUri() == managerUri)
         return;
 
     // the change is real.  update.
-    if (m_initialisedManagers.contains(backend)) {
-        m_manager = m_initialisedManagers.value(backend);
+    if (m_initialisedManagers.contains(managerUri)) {
+        m_manager = m_initialisedManagers.value(managerUri);
     } else {
-        m_manager = new QContactManager(backend);
-        m_initialisedManagers.insert(backend, m_manager);
+        m_manager = QContactManager::fromUri(managerUri);
+        if (m_manager->error()) {
+            QMessageBox::information(this, "Failed!", QString("Failed to open store!\n(error code %1)").arg(m_manager->error()));
+            delete m_manager;
+            m_manager = 0;
+            return;
+        }
+        m_initialisedManagers.insert(managerUri, m_manager);
     }
 
     // signal that the manager has changed.
@@ -163,7 +201,8 @@ void ContactListPage::rebuildList(const QContactFilter& filter)
 
 void ContactListPage::addContactClicked()
 {
-    emit showEditorPage(QContactLocalId(0));
+    if (m_manager)
+        emit showEditorPage(QContactLocalId(0));
 }
 
 void ContactListPage::editClicked()
@@ -175,7 +214,8 @@ void ContactListPage::editClicked()
 
 void ContactListPage::filterClicked()
 {
-    emit showFilterPage(m_currentFilter);
+    if (m_manager)
+        emit showFilterPage(m_currentFilter);
 }
 
 void ContactListPage::deleteClicked()

examples/samplephonebook/contactlistpage.h
@@ -96,6 +96,7 @@ private:
     QMap<QString, QContactManager*> m_initialisedManagers;
 
     QContactFilter m_currentFilter;
+    QMap<QString, QString> m_availableManagers;
 };
 
 #endif // CONTACTLISTPAGE_H

commit d0a5cdc031d89dcb3893a72cb65273b961b0cf7b

Fix compilation of samplephonebook
Was broken in commit 57d08c7

examples/samplephonebook/contacteditor.cpp
@@ -271,7 +271,7 @@ void ContactEditor::saveClicked()
         
         if (m_avatarBtn->isEnabled()) {
             QContactAvatar av = curr.detail(QContactAvatar::DefinitionName);
-            av.setAvatar(m_newAvatarPath);
+            av.setImageUrl(QUrl(m_newAvatarPath));
             curr.saveDetail(&av);
             
             QContactThumbnail thumb = curr.detail(QContactThumbnail::DefinitionName);