qtproject/qt-mobility

commit f3ac550d50bb16be2b8a8072eab94fd2355f9e13

Fixed krazy warnings for contacts, versit and symbian backend
Relates to task MOBILITY-629

plugins/contacts/symbiansim/tsrc/tst_simcm/tst_simcm.cpp
@@ -1032,8 +1032,8 @@ void tst_SimCM::sdnContacts()
     // Verify that contact details have read only flag
     QList<QContact> contacts = cm->contacts();
     QVERIFY(contacts.count());
-    foreach(QContact c, contacts) {
-        foreach(QContactDetail d, c.details()) {
+    foreach(const QContact& c, contacts) {
+        foreach(const QContactDetail& d, c.details()) {
             QVERIFY(d.accessConstraints().testFlag(QContactDetail::ReadOnly));
         }
     }
@@ -1165,7 +1165,7 @@ void tst_SimCM::parseDetails(QContact &contact, QStringList details, QList<QCont
  */
 void tst_SimCM::compareDetails(QContact contact, QList<QContactDetail> expectedDetails)
 {
-    foreach (QContactDetail expectedDetail, expectedDetails) {
+    foreach (const QContactDetail& expectedDetail, expectedDetails) {
         QContactDetail actualDetail = contact.detail(expectedDetail.definitionName());
         QVERIFY(!actualDetail.isEmpty());
 

examples/qmlcontacts/qmlcontact.h
@@ -59,7 +59,7 @@ Q_PROPERTY(QStringList availableActions READ availableActions)
 Q_PROPERTY(QStringList details READ details)
 Q_PROPERTY(QStringList contexts READ contexts)
 public:
-    QmlContact(QContact& contact, QObject *parent = 0);
+    explicit QmlContact(QContact& contact, QObject *parent = 0);
     QmlContact();
     ~QmlContact();
 

examples/qmlcontacts/qmlcontacts.cpp
@@ -113,7 +113,7 @@ QString QMLContactManagerAsync::contactListToQString(const QList<QContactLocalId
     QString list;
 
     for (int i = 0; i < contactIds.count(); i++) {
-        list += QString::number(contactIds.at(i)) +  " ";
+        list += QString::number(contactIds.at(i)) +  ' ';
     }
 
     return list;
@@ -159,8 +159,7 @@ void QMLContactManagerAsync::contactProgress(QContactAbstractRequest::State newS
     }
 
     if(request->contacts().count() > 0) {
-        QContact c;
-        foreach(c, request->contacts()) {
+        foreach(const QContact& c, request->contacts()) {
             QmlContact qmlc(c);
             emit contactsLoaded(&qmlc);
         }

plugins/contacts/symbian/src/cntsymbiandatabase.cpp
@@ -240,7 +240,7 @@ void CntSymbianDatabase::HandleDatabaseEventL(TContactDbObserverEvent aEvent)
 void CntSymbianDatabase::updateGroupMembershipsL()
 {
     CContactIdArray *groupIds = m_contactDatabase->GetGroupIdListL();
-    for (TInt i(0); i < groupIds->Count(); i++) {
+    for (TInt i(0); i < groupIds->Count(); ++i) {
         QContactLocalId id = (*groupIds)[i];
         QSet<QContactLocalId> dummySet;
         updateGroupMembershipsL(id, dummySet, dummySet);

plugins/contacts/symbian/src/filtering/cntdbinfo.cpp
@@ -128,7 +128,7 @@ QString CntDbInfo::getSortQuery( const QList<QContactSortOrder> &sortOrders,
     if(*error == QContactManager::NoError)
         {
         QList<QString> list;
-        foreach( QContactSortOrder s, sortOrders ) 
+        foreach(const QContactSortOrder& s, sortOrders )
             {
             // Find uids for sortings
             // Get the field id for the detail field name

plugins/contacts/symbian/src/transform/cnttransformaddress.cpp
@@ -114,7 +114,7 @@ QContactDetail *CntTransformAddress::transformItemField(const CContactItemField&
 
         // Merge detail with existing detail
         detail = new QContactAddress( existingAddress );
-        foreach(const QString key, address.variantValues().keys() )
+        foreach(const QString& key, address.variantValues().keys() )
             detail->setValue( key, address.variantValue(key) );
         break;
     }

plugins/contacts/symbiansim/src/cntsimstoreprivate.cpp
@@ -81,7 +81,7 @@ void CntSimStorePrivate::ConstructL()
     TBuf<RMobilePhoneBookStore::KMaxPBIDSize> storeName;
     convertStoreNameL(storeName);
     
-    // SDN store is allways read only
+    // SDN store is always read only
     if (m_storeName == KParameterValueSimStoreNameSdn)
         m_readOnlyAccess = true;
 

src/contacts/qcontactdetail.cpp
@@ -254,7 +254,7 @@ QDebug operator<<(QDebug dbg, const QContactDetail& detail)
     dbg.nospace() << "QContactDetail(name=" << detail.definitionName() << ", key=" << detail.key();
     QVariantMap fields = detail.variantValues();
     QVariantMap::const_iterator it;
-    for (it = fields.constBegin(); it != fields.constEnd(); it++) {
+    for (it = fields.constBegin(); it != fields.constEnd(); ++it) {
         dbg.nospace() << ", " << it.key() << '=' << it.value();
     }
     dbg.nospace() << ')';

commit 9c65f2599d3d314fe43052b301c01c0e815e014e

Symbian SIM: Compile fix for for tst_simcm unit test.
It was broken by commit f3ac550d50bb16be2b8a8072eab94fd2355f9e13

(Bug report QTMOBILITY-110)

plugins/contacts/symbiansim/tsrc/tst_simcm/tst_simcm.cpp
@@ -1165,7 +1165,7 @@ void tst_SimCM::parseDetails(QContact &contact, QStringList details, QList<QCont
  */
 void tst_SimCM::compareDetails(QContact contact, QList<QContactDetail> expectedDetails)
 {
-    foreach (const QContactDetail& expectedDetail, expectedDetails) {
+    foreach (QContactDetail expectedDetail, expectedDetails) {
         QContactDetail actualDetail = contact.detail(expectedDetail.definitionName());
         QVERIFY(!actualDetail.isEmpty());
 