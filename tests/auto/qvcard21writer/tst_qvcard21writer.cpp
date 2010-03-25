b1c79d7ca112293e5ef03d681881ae6ac5f36b5e
Michael Goddard <michael.goddard@nokia.com>
2010-03-25 21:17:31 +1000

    Hopefully fix a compile problem on Symbian.
    Q_DECLARE_METATYPE needs to happen earlier - the compiler is stricter.
    Part of the fix for QTMOBILITY-89

--- tests/auto/qvcard21writer/tst_qvcard21writer.cpp
+++ tests/auto/qvcard21writer/tst_qvcard21writer.cpp
@@ -52,6 +52,8 @@ const QString KATAKANA_NOKIA(QString::fromUtf8("\xe3\x83\x8e\xe3\x82\xad\xe3\x82
 
 QTM_USE_NAMESPACE
 
+Q_DECLARE_METATYPE(QVersitProperty)
+
 void tst_QVCard21Writer::init()
 {
     mWriter = new QVCard21Writer;
@@ -78,8 +80,6 @@ void tst_QVCard21Writer::testEncodeVersitProperty()
     mWriter->encodeVersitProperty(property);
     QCOMPARE(encodedProperty, expectedResult);
 }
-
-Q_DECLARE_METATYPE(QVersitProperty)
 
 void tst_QVCard21Writer::testEncodeVersitProperty_data()
 {
