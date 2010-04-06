ccd90e152cb548df9876d97cb3a66bfc31953514
Jaakko Vuori <qt-info@nokia.com>
2010-04-06 11:57:20 +0300

    MOBILITY-643 fix: Added new enum case in TDisplayMode mapping

--- src/systeminfo/qsysteminfo_s60.cpp
+++ src/systeminfo/qsysteminfo_s60.cpp
@@ -595,6 +595,7 @@ int QSystemDisplayInfoPrivate::colorDepth(int screen)
             case EColor4K: depth = 12; break;
             case EColor64K: depth = 16; break;
             case EColor16M:
+            case 13: //value of EColor16MAP (which is missing from gdi.h in S60 v.3.1)
             case EColor16MA: depth = 24; break;
             case EColor16MU: depth = 32; break;
             case ENone:
