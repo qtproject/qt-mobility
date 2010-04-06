qtproject/qt-mobility

commit 4bc854cd3ff57e431ed767837bf6c329431eb41f

Set the manager URI component of a contact Id on retrieve in maemo5
Relates to task QTMOBILITY-161

plugins/contacts/maemo5/qcontactmaemo5backend.cpp
@@ -163,7 +163,13 @@ QContact QContactMaemo5Engine::contact(const QContactLocalId& contactId, const Q
   QContact *contact = d->m_abook->getQContact(contactId, error);
   QContact rtn(*contact);
   delete contact;
-  setContactDisplayLabel(&rtn, synthesizedDisplayLabel(rtn, error));
+  if (error == QContactManager::NoError) {
+    setContactDisplayLabel(&rtn, synthesizedDisplayLabel(rtn, error));
+    QContactId cid;
+    cid.setLocalId(contactId);
+    cid.setManagerUri(managerUri());
+    rtn.setId(cid);
+  }
   return rtn;
 }
 

commit a388ccf39802503a756d86d4e749659184ecbd8a

Fix compile error introduced by 4bc854cd
Relates to task QTMOBILITY-161

plugins/contacts/maemo5/qcontactmaemo5backend.cpp
@@ -163,7 +163,7 @@ QContact QContactMaemo5Engine::contact(const QContactLocalId& contactId, const Q
   QContact *contact = d->m_abook->getQContact(contactId, error);
   QContact rtn(*contact);
   delete contact;
-  if (error == QContactManager::NoError) {
+  if (*error == QContactManager::NoError) {
     setContactDisplayLabel(&rtn, synthesizedDisplayLabel(rtn, error));
     QContactId cid;
     cid.setLocalId(contactId);