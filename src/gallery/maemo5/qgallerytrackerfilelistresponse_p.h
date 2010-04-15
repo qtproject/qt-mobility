a0d1f3b0e4ff8e45cd440790ee7d7300d0b3f28f
Andrew den Exter <andrew.den-exter@nokia.com>
2010-04-15 14:46:11 +1000

    Add private header warnings.

--- src/gallery/maemo5/qgallerytrackerfilelistresponse_p.h
+++ src/gallery/maemo5/qgallerytrackerfilelistresponse_p.h
@@ -42,6 +42,17 @@
 #ifndef QGALLERYTRACKERFILELISTRESPONSE_P_H
 #define QGALLERYTRACKERFILELISTRESPONSE_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API. It exists purely as an
+// implementation detail. This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include "qgallerytrackerlistresponse_p.h"
 
 #include <QtDBus/qdbusinterface.h>
