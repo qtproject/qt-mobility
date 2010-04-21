84794f3817097e120370c3f648399cbe5ff76d7b
Dmytro Poplavskiy <dmytro.poplavskiy@nokia.com>
2010-04-21 12:09:06 +1000

    Player demo: load not only local files but playlists and urls passed as command line arguments.

--- examples/player/player.cpp
+++ examples/player/player.cpp
@@ -227,13 +227,22 @@ Player::Player(QWidget *parent)
 
     metaDataChanged();
 
-    QStringList fileNames = qApp->arguments();
-    fileNames.removeAt(0);
-    foreach (QString const &fileName, fileNames) {
-        if (fileName.startsWith(QLatin1String("http://")))
-            playlist->addMedia(QUrl(fileName));
-        else if (QFileInfo(fileName).exists())
-            playlist->addMedia(QUrl::fromLocalFile(fileName));
+    QStringList arguments = qApp->arguments();
+    arguments.removeAt(0);
+    foreach (QString const &argument, arguments) {
+        QFileInfo fileInfo(argument);
+        if (fileInfo.exists()) {
+            QUrl url = QUrl::fromLocalFile(fileInfo.absoluteFilePath());
+            if (fileInfo.suffix().toLower() == QLatin1String("m3u")) {
+                playlist->load(url);
+            } else
+                playlist->addMedia(url);
+        } else {
+            QUrl url(argument);
+            if (url.isValid()) {
+                playlist->addMedia(url);
+            }
+        }
     }
 }
 
