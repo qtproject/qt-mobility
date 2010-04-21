e35e6466afcd557a28c1c7cb112383205289c690
David Laing <david.laing@nokia.com>
2010-04-21 14:52:02 +1000

    Added warnings to private header files.
    Quite a few were missing.
    
    The license header was also missing from the maps service parser files.

--- src/location/qlandmarkmanagerengine_sqlite_p.h
+++ src/location/qlandmarkmanagerengine_sqlite_p.h
@@ -42,6 +42,17 @@
 #ifndef QLANDMARKMANAGERENGINE_SQLITE_P_H
 #define QLANDMARKMANAGERENGINE_SQLITE_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include "qlandmarkmanagerengine.h"
 
 #include <QSqlDatabase>
