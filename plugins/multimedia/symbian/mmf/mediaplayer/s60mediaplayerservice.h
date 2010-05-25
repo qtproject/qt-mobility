b21920fa33f3167823eafe8b3ab69ab9caa1b89c
Derick Hawcroft <derick.hawcroft@nokia.com>
2010-05-25 14:53:20 +1000

    qvideooutputcontorol.h -> qvideooutputcontrol.h

--- plugins/multimedia/symbian/mmf/mediaplayer/s60mediaplayerservice.h
+++ plugins/multimedia/symbian/mmf/mediaplayer/s60mediaplayerservice.h
@@ -45,7 +45,7 @@
 #include <QtCore/qobject.h>
 
 #include <QMediaService>
-#include <qvideooutputcontorol.h>
+#include <qvideooutputcontrol.h>
 
 #include "s60videooutputcontrol.h"
 #include "ms60mediaplayerresolver.h"
