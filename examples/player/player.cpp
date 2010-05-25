4a769bc6df8f0033d3cbe67381cbec8fd240307f
Dmytro Poplavskiy <dmytro.poplavskiy@nokia.com>
2010-05-25 15:14:52 +1000

    Fixed player example compilation.

--- examples/player/player.cpp
+++ examples/player/player.cpp
@@ -78,9 +78,8 @@ Player::Player(QWidget *parent)
 #endif
 {
     player = new QMediaPlayer(this);
-    // owned by PlaylistModel
-    playlist = new QMediaPlaylist();
-    playlist->setMediaObject(player);
+    playlist = new QMediaPlaylist(this);
+    player->setPlaylist(playlist);
 
     connect(player, SIGNAL(durationChanged(qint64)), SLOT(durationChanged(qint64)));
     connect(player, SIGNAL(positionChanged(qint64)), SLOT(positionChanged(qint64)));
