a05504d2a0c643c6f253527f07bcc0dba8a799b4
juhvu <qt-info@nokia.com>
2010-05-28 15:24:24 +0300

    MOBILITY-800
    Symbian backend: added new call to unsetdefaultif(), which will cause subsequent
    OpenC operations to fail. This is desired, because otherwise the
    network connection will always just bounce back up. Upon QNetworkSession
    deletion the potential unsetdefaultif() is cleared with setdefaultif(0)

--- src/bearer/qnetworksession_s60_p.cpp
+++ src/bearer/qnetworksession_s60_p.cpp
@@ -62,7 +62,12 @@ QNetworkSessionPrivate::QNetworkSessionPrivate()
 #ifdef SNAP_FUNCTIONALITY_AVAILABLE
     iMobility = NULL;
 #endif
-
+    // Try to load "Open C" dll dynamically and
+    // try to attach to unsetdefaultif function dynamically.
+    // This is to avoid build breaks with old OpenC versions.
+    if (iOpenCLibrary.Load(_L("libc")) == KErrNone) {
+        iDynamicUnSetdefaultif = (TOpenCUnSetdefaultifFunction)iOpenCLibrary.Lookup(597);
+    }
     TRAP_IGNORE(iConnectionMonitor.ConnectL());
 }
 
@@ -85,14 +90,15 @@ QNetworkSessionPrivate::~QNetworkSessionPrivate()
         iMobility = NULL;
     }
 #endif
-
     iConnection.Close();
     iSocketServ.Close();
     
     // Close global 'Open C' RConnection
+    // Clears also possible unsetdefaultif() flags.
     setdefaultif(0);
     
     iConnectionMonitor.Close();
+    iOpenCLibrary.Close();
 }
 
 void QNetworkSessionPrivate::configurationStateChanged(TUint32 accessPointId, TUint32 connMonId, QNetworkSession::State newState)
@@ -506,8 +512,15 @@ void QNetworkSessionPrivate::close(bool allowSignals)
     iConnection.Close();
     iSocketServ.Close();
     
-    // Close global 'Open C' RConnection
-    setdefaultif(0);
+    // Close global 'Open C' RConnection. If OpenC supports,
+    // close the defaultif for good to avoid difficult timing
+    // and bouncing issues of network going immediately back up
+    //  because of e.g. select() thread etc.
+    if (iDynamicUnSetdefaultif) {
+        iDynamicUnSetdefaultif();
+    } else {
+        setdefaultif(0);
+    }
 
     if (publicConfig.type() == QNetworkConfiguration::UserChoice) {
         newState(QNetworkSession::Closing);
@@ -589,8 +602,13 @@ void QNetworkSessionPrivate::migrate()
 {
 #ifdef SNAP_FUNCTIONALITY_AVAILABLE
     if (iMobility) {
-        // Close global 'Open C' RConnection
-        setdefaultif(0);
+        // Close global 'Open C' RConnection. If openC supports, use the 'heavy'
+        // version to block all subsequent requests.
+        if (iDynamicUnSetdefaultif) {
+            iDynamicUnSetdefaultif();
+        } else {
+            setdefaultif(0);
+        }
         // Start migrating to new IAP
         iMobility->MigrateToPreferredCarrier();
     }
