qtproject/qt-mobility

commit ee0e4456d56165d23dd92b4a2cad616266589d02

Maemo5: This commits makes the engine faster to retrieve contacts

plugins/contacts/maemo5/qcontactabook.cpp
@@ -869,29 +869,14 @@ OssoABookContact* QContactABook::getAContact(const QContactLocalId& contactId, Q
 
   QCM5_DEBUG << "Getting aContact with id " << m_localIds[contactId] << "local contactId is" << contactId;
 
-  if(QString(m_localIds[contactId]).compare("osso-abook-self") == 0) {
+  if(QString::fromAscii(m_localIds[contactId]).compare("osso-abook-self") == 0) {
     *error = QContactManager::NoError;
     rtn = A_CONTACT(osso_abook_self_contact_get_default());
   } else {
-    EBookQuery* query;
-    GList* contacts;
-
-    query = e_book_query_field_test(E_CONTACT_UID, E_BOOK_QUERY_IS, m_localIds[contactId]);
-    contacts = osso_abook_aggregator_find_contacts(m_abookAgregator, query);
-    if (query)
-        e_book_query_unref(query);
-
-    if (g_list_length(contacts) == 1) {
-      *error = QContactManager::NoError;
-      rtn = A_CONTACT(contacts->data);
-    } else if (g_list_length(contacts) == 0) {
-      *error = QContactManager::DoesNotExistError;
-    } else {
-      // Several contacts have the same UID or the contactId belongs to a roster contact.
-      *error = QContactManager::UnspecifiedError;
-    }
-    if (contacts)
-      g_list_free(contacts);
+    GList* c = osso_abook_aggregator_lookup(m_abookAgregator, m_localIds[contactId]);
+    rtn = A_CONTACT(c->data);
+    *error = rtn ? QContactManager::NoError : QContactManager::DoesNotExistError;
+    return rtn;
   }
 
   return rtn;

commit adb934cfb278ff3ab074a3981500f8da800ee563

Parse ADR details differently in maemo5 backend
We now only parse the first 7 fields of the ADR attribute type, which
are supported by the maemo5 backend.  We do not qFatal() or qCritical()
if the number of fields is more or less than we expected, but instead
attempt to deal with the difference gracefully.
This relates to task QTMOBILITY-380.

Also, fix a crash introduced by ee0e4456d56165d when the user attempts
to retrieve a nonexistent contact.

plugins/contacts/maemo5/qcontactabook.cpp
@@ -874,7 +874,8 @@ OssoABookContact* QContactABook::getAContact(const QContactLocalId& contactId, Q
     rtn = A_CONTACT(osso_abook_self_contact_get_default());
   } else {
     GList* c = osso_abook_aggregator_lookup(m_abookAgregator, m_localIds[contactId]);
-    rtn = A_CONTACT(c->data);
+    if (c)
+      rtn = A_CONTACT(c->data);
     *error = rtn ? QContactManager::NoError : QContactManager::DoesNotExistError;
     return rtn;
   }
@@ -938,14 +939,15 @@ QList<QContactAddress*> QContactABook::getAddressDetail(EContact *eContact) cons
     // Set Address Values
     GList *v = NULL;
     v =e_vcard_attribute_get_values(attr);
-    if (!v)
-      qFatal("ADR attribute data is corrupted"); 
-    if (g_list_length(v) != 7){
-      g_list_free(v);
-      qCritical() << "ADR attribute data is corrupted"; 
+    if (!v) {
+      // ADR attribute data is corrupted.  Skipping.
+      g_list_free(attrList);
+      return rtnList;
     }
+
     int i = 0;
-    while (v){
+    while (v && i < 7) {
+      // we only deal with the first 7 fields: pobox, extension, street, locality, region, postcode, country.
       map[addressFields[i]] = QString::fromUtf8(CONST_CHAR(v->data));
       i++;
       v = v->next;
@@ -958,7 +960,6 @@ QList<QContactAddress*> QContactABook::getAddressDetail(EContact *eContact) cons
   }
   
   g_list_free(attrList);
-  
   return rtnList;
 }
 