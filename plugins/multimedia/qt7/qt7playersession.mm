a12b7316a1aaaae6493903e26328077f2d4d26a5
Justin McPherson <justin.mcpherson@nokia.com>
2010-07-05 16:13:01 +1000

    QT7 backend; use runtime check for natural size change notifications
    Reviewed-by:Dmytro Poplavskiy

--- plugins/multimedia/qt7/qt7playersession.mm
+++ plugins/multimedia/qt7/qt7playersession.mm
@@ -115,15 +115,20 @@ QT_USE_NAMESPACE
                                                      name:QTMovieVolumeDidChangeNotification
                                                    object:m_movie];
 
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(processNaturalSizeChange:)
-                                                     name:
-#if defined(MAC_OS_X_VERSION_10_6) && (MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_6)
-                                                          QTMovieNaturalSizeDidChangeNotification
-#else
-                                                          QTMovieEditedNotification
-#endif
-                                                   object:m_movie];
+        if (QSysInfo::MacintoshVersion >= QSysInfo::MV_10_6) {
+            [[NSNotificationCenter defaultCenter] addObserver:self
+                                                     selector:@selector(processNaturalSizeChange:)
+                                                         name:@"QTMovieNaturalSizeDidChangeNotification"
+                                                       object:m_movie];
+
+        }
+        else {
+            [[NSNotificationCenter defaultCenter] addObserver:self
+                                                     selector:@selector(processNaturalSizeChange:)
+                                                         name:QTMovieEditedNotification
+                                                       object:m_movie];
+        }
+
         [movie retain];
     }
 }
