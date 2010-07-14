bfe6a49eaee5d7fa8d9bc8ce74e0217a580f3256
Chris Adams <christopher.adams@nokia.com>
2010-07-14 10:49:57 +1000

    Add some maemo5 specific spacing to hapticsplayer example
    Due to QGroupBox/QLayouts not being given enough space to render their
    titles on maemo5

--- examples/hapticsplayer/dialog.cpp
+++ examples/hapticsplayer/dialog.cpp
@@ -53,6 +53,18 @@ Dialog::Dialog()
 {
     ui.setupUi(this);
 
+#if defined(Q_WS_MAEMO_5)
+    // maemo5 style problem: title of groupboxes is rendered badly if there isn't enough space
+    // that is, the sizehint of the title area is Preferred rather than Minimum.
+    // to fix that, we manually tweak some spacers.
+    ui.verticalSpacer_2->changeSize(20, 20, QSizePolicy::Expanding, QSizePolicy::Fixed);
+    ui.verticalSpacer_3->changeSize(20, 20, QSizePolicy::Expanding, QSizePolicy::Fixed);
+    ui.verticalSpacer_4->changeSize(20, 30, QSizePolicy::Expanding, QSizePolicy::Fixed);
+    ui.verticalSpacer_5->changeSize(20, 30, QSizePolicy::Expanding, QSizePolicy::Fixed);
+    ui.verticalSpacer_6->changeSize(20, 20, QSizePolicy::Expanding, QSizePolicy::Fixed);
+    ui.verticalSpacer_7->changeSize(20, 30, QSizePolicy::Expanding, QSizePolicy::Fixed);
+#endif
+
     connect(ui.actuators, SIGNAL(currentIndexChanged(int)), SLOT(actuatorChanged()));
     connect(ui.enabled, SIGNAL(toggled(bool)), SLOT(enabledChanged(bool)));
     connect(ui.playPause, SIGNAL(pressed()), SLOT(playPauseClicked()));
