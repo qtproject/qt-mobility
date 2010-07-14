27089c1b1d2120f0d6236c4e9f57af448c3e8127
abcd <qt-info@nokia.com>
2010-07-14 11:24:49 +1000

    Revert "Revert "Search for service framework plugins in correct directory.""
    This reverts commit c2b6d7a2eed9c9641d88b4f1d4c674151465be98.
    
    The original commit is semi correct. Modifications are still
    necessary in a future commit to prevent binary incompatible
    behaviour.

--- src/serviceframework/qservicemanager.cpp
+++ src/serviceframework/qservicemanager.cpp
@@ -67,10 +67,11 @@ static QString qservicemanager_resolveLibraryPath(const QString &libNameOrPath)
     // try to find plug-in via QLibrary
     QStringList paths = QCoreApplication::libraryPaths();
 #ifdef QTM_PLUGIN_PATH
-    paths << QLatin1String(QTM_PLUGIN_PATH)+QLatin1String("/serviceframework");
+    paths << QLatin1String(QTM_PLUGIN_PATH);
 #endif
     for (int i=0; i<paths.count(); i++) {
-        QString libPath = QDir::toNativeSeparators(paths[i]) + QDir::separator() + libNameOrPath;
+        QString libPath = QDir::toNativeSeparators(paths[i] + QLatin1String("/serviceframework")) +
+                          QDir::separator() + libNameOrPath;
         
 #ifdef Q_OS_SYMBIAN
         QFileInfo fi(libPath);
