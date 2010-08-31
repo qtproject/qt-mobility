59d68503d4bb39dae39a72beb366723dbd741675
rajender pasricha <rajender.pasricha@nokia.com>
2010-08-31 15:24:09 +0530

    removed the flag for item type chcking
    As the statement is independent of the flag and would keep onto executing even
    if notes support is not there, so i have removed the flag from this particular
    check.

--- plugins/organizer/symbian/transform/organizeritemrecurrencetransform.cpp
+++ plugins/organizer/symbian/transform/organizeritemrecurrencetransform.cpp
@@ -95,12 +95,10 @@ void OrganizerItemRecurrenceTransform::transformToDetailL(const CCalInstance& in
 
 void OrganizerItemRecurrenceTransform::transformToEntryL(const QOrganizerItem& item, CCalEntry* entry)
 {
-#ifdef AGENDA_EXT_SUPPORT
     if (QOrganizerItemType::TypeNote == item.type()) {
         // Notes do not support the reccurrence rules
         return;
     }
-#endif
     // Clear all repeating properties from this entry.
     // This is needed for removing recurrence rules from an existing entry.
     entry->ClearRepeatingPropertiesL();
