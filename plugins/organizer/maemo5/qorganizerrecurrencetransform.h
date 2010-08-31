ffcb497ea940d3138ae26a5d2a5a95aee99f3465
Marko Männistö <qt-info@nokia.com>
2010-08-31 14:39:56 +0300

    Maemo5: A new workaround for recurrence dates.
    Replaces fix made in 593952fb2fce0751280c740113f1c2b035923d0b

--- plugins/organizer/maemo5/qorganizerrecurrencetransform.h
+++ plugins/organizer/maemo5/qorganizerrecurrencetransform.h
@@ -107,8 +107,6 @@ private:
     QString listOfNumbers(const QList<int> &list) const;
     int qfrequencyToRtype(QOrganizerItemRecurrenceRule::Frequency frequency) const;
 
-    QString qrecurrenceDateToIcalRecurrenceDate(const QDate &date) const;
-
     // crule -> qrule conversions
     QOrganizerItemRecurrenceRule icalRecurrenceRuleToQrecurrenceRule(CRecurrenceRule *rule) const;
     QOrganizerItemRecurrenceRule::Frequency icalFrequencyToQfrequency(FREQUENCY frequency) const;
@@ -117,9 +115,6 @@ private:
 
     // common methods
     QMap<QString, Qt::DayOfWeek> icalRecurrenceWeekDayQdayOfWeekMapping() const;
-    QString createSingleDayRuleFor(const QDate& date) const;
-    QList<QDate> convertRDate(const QString &rdate) const;
-    QDate convertString(const QString &rdate) const;
 
 private:
     // QRecurrence -> CRecurrence
@@ -131,7 +126,7 @@ private:
     // CRecurrence -> QRecurrence
     QList<QOrganizerItemRecurrenceRule> m_lRecurrenceRules;
     QList<QOrganizerItemRecurrenceRule> m_lExceptionRules;
-    mutable QList<QDate> m_lRecurrenceDates;
+    QList<QDate> m_lRecurrenceDates;
     QList<QDate> m_lExceptionDates;
 };
 
