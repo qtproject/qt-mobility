3a8e4242065f5460dff485f69d48f9e59c0a38cd
Dmytro Poplavskiy <dmytro.poplavskiy@nokia.com>
2010-09-02 10:37:32 +1000

    QVideoWidget: pass windowId to QVideoWindowControl early.
    To ensure the winId is know when playback starts.
    
    Reviewed-by:Andrew den Exter

--- src/multimedia/qvideowidget.cpp
+++ src/multimedia/qvideowidget.cpp
@@ -322,7 +322,7 @@ QWindowVideoWidgetBackend::QWindowVideoWidgetBackend(
     connect(control, SIGNAL(fullScreenChanged(bool)), m_widget, SLOT(_q_fullScreenChanged(bool)));
     connect(control, SIGNAL(nativeSizeChanged()), m_widget, SLOT(_q_dimensionsChanged()));
 
-
+    control->setWinId(widget->winId());
 }
 
 QWindowVideoWidgetBackend::~QWindowVideoWidgetBackend()
