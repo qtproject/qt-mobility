b5d2c9f493b31d776e6c371b6b12f3cfb51ad546
Gareth Stockwell <ext-gareth.stockwell@nokia.com>
2010-09-14 15:02:03 +0100

    Implement QAudioInput::suspend() using CMMFDevSound::Stop()
    As with playback mode, lack of support for CMMFDevSound::Pause() in
    DevSound's recording mode causes problems on some devices.
    Specifically, while QAudioInput works fine on the Nokia 5800, this
    bug was observed on the Nokia N8.  This fix means that suspending
    and resuming audio input will work correctly on all devices.
    
    Task-number: QTBUG-13506
    Reviewed-by: Derick Hawcroft

--- src/multimedia/audio/qaudio_symbian_p.h
+++ src/multimedia/audio/qaudio_symbian_p.h
@@ -82,7 +82,10 @@ enum State {
     ,   InitializingState
     ,   ActiveState
     ,   IdleState
-    ,   SuspendedState
+    // QAudio is suspended; DevSound is paused
+    ,   SuspendedPausedState
+    // QAudio is suspended; DevSound is stopped
+    ,   SuspendedStoppedState
 };
 
 /**
@@ -118,7 +121,14 @@ public:
     int samplesProcessed() const;
     bool setFormat(const QAudioFormat &format);
     bool start();
-    void pause();
+
+    // If DevSound implementation supports pause, calls pause and returns true.
+    // Otherwise calls stop and returns false.  In this case, all DevSound buffers
+    // currently held by the backend must be discarded.
+    bool pause();
+
+    void resume();
+
     void stop();
     void bufferProcessed();
 
@@ -141,6 +151,7 @@ signals:
 private:
     void getSupportedCodecs();
     void populateCapabilities();
+    bool isResumeSupported() const;
 
 private:
     const QAudio::Mode              m_mode;
