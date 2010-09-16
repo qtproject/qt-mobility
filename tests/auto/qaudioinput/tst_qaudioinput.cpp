dd619af2efd1136606777a2fa936fb64a54f24a3
Andrew den Exter <andrew.den-exter@nokia.com>
2010-09-16 14:01:38 +1000

    Verify the audio format before trying to open an audio device.
    This was causing a crash on windows because the buffer and period sizes
    were worked out to 0 with an invalid sample size and dividing one by the
    other is division by 0.
    
    Task-number: QTMOBILITY-438
    Reviewed-by: Justin McPherson

--- tests/auto/qaudioinput/tst_qaudioinput.cpp
+++ tests/auto/qaudioinput/tst_qaudioinput.cpp
@@ -55,6 +55,8 @@
 #define SRCDIR ""
 #endif
 
+Q_DECLARE_METATYPE(QAudioFormat)
+
 class tst_QAudioInput : public QObject
 {
     Q_OBJECT
@@ -65,6 +67,7 @@ private slots:
     void initTestCase();
 
     void format();
+    void invalidFormat_data();
     void invalidFormat();
 
     void bufferSize();
@@ -125,6 +128,8 @@ QString tst_QAudioInput::workingDir()
 
 void tst_QAudioInput::initTestCase()
 {
+    qRegisterMetaType<QAudioFormat>();
+
     // Only perform tests if audio output device exists
     const QList<QAudioDeviceInfo> devices =
         QAudioDeviceInfo::availableDevices(QAudio::AudioInput);
@@ -204,10 +209,34 @@ void tst_QAudioInput::format()
             QString("sampleType: requested=%1, actual=%2").arg(requested.sampleType()).arg(actual.sampleType()).toLocal8Bit().constData());
 }
 
+void tst_QAudioInput::invalidFormat_data()
+{
+    QTest::addColumn<QAudioFormat>("invalidFormat");
+
+    QAudioFormat format;
+
+    QTest::newRow("Null Format")
+            << format;
+
+    format = audioDevice.preferredFormat();
+    format.setChannelCount(0);
+    QTest::newRow("Channel count 0")
+            << format;
+
+    format = audioDevice.preferredFormat();
+    format.setSampleRate(0);
+    QTest::newRow("Sample rate 0")
+            << format;
+
+    format = audioDevice.preferredFormat();
+    format.setSampleSize(0);
+    QTest::newRow("Sample size 0")
+            << format;
+}
+
 void tst_QAudioInput::invalidFormat()
 {
-    QAudioFormat invalidFormat;
-    invalidFormat.setFrequency(0);
+    QFETCH(QAudioFormat, invalidFormat);
 
     QVERIFY2(!audioDevice.isFormatSupported(invalidFormat),
             "isFormatSupported() is returning true on an invalid format");
