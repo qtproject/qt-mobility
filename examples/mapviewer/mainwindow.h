4f99edf70c04771b74f6b37be4f52a2e3fc2206f
jounih <qt-info@nokia.com>
2010-09-22 10:40:59 +0300

    MapViever example fixes
    - Split MapWidget to it's own files
    - Fix potential null pointer crashes
    - Reorganize ui creation
    - searchReplyFinished to move viewport based on received places

--- examples/mapviewer/mainwindow.h
+++ examples/mapviewer/mainwindow.h
@@ -46,10 +46,8 @@
 #include <qgeoroutingmanager.h>
 #include <qgeosearchmanager.h>
 #include <qgeosearchreply.h>
-#include <qgraphicsgeomap.h>
 #include <qgeoserviceprovider.h>
 #include <qgeomappixmapobject.h>
-#include <qgeomapcircleobject.h>
 
 #include <QMainWindow>
 #include <QGraphicsView>
@@ -60,67 +58,16 @@
 #include <QTime>
 #include <qnetworksession.h>
 
-
-class QResizeEvent;
-class QShowEvent;
-
 class QSlider;
 class QRadioButton;
 class QLineEdit;
 class QPushButton;
 class QToolButton;
+class QResizeEvent;
+class QShowEvent;
 
 QTM_USE_NAMESPACE
-
-class MapWidget : public QGraphicsGeoMap
-{
-    Q_OBJECT
-public:
-    MapWidget(QGeoMappingManager *manager);
-    ~MapWidget();
-
-public slots:
-    void setMouseClickCoordQuery(bool state);
-
-signals:
-    void coordQueryResult(const QGeoCoordinate &coord);
-
-private slots:
-    void kineticTimerEvent();
-
-protected:
-    void mousePressEvent(QGraphicsSceneMouseEvent* event);
-    void mouseReleaseEvent(QGraphicsSceneMouseEvent* event);
-    void mouseMoveEvent(QGraphicsSceneMouseEvent* event);
-    void mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event);
-    void keyPressEvent(QKeyEvent *event);
-    void keyReleaseEvent(QKeyEvent* event);
-    void wheelEvent(QGraphicsSceneWheelEvent* event);
-
-private:
-    bool coordQueryState;
-    bool panActive;
-    bool panDecellerate;
-
-    // Fractional pan, used by panFloatWrapper
-    QPointF remainingPan;
-
-    // current kinetic panning speed, in pixel/msec
-    QPointF kineticPanSpeed;
-    QPoint panDir;
-    QTimer *kineticTimer;
-    QTime lastMoveTime;
-
-    // An entry in the mouse history. first=speed, second=time
-    typedef QPair<QPointF, QTime> MouseHistoryEntry;
-    // A history of the last (currently 5) mouse move events is stored in order to smooth out movement detection for kinetic panning
-    QList<MouseHistoryEntry> mouseHistory;
-
-    void panFloatWrapper(const QPointF& delta);
-    void applyPan(const Qt::KeyboardModifiers& modifiers);
-public:
-    QGeoMapCircleObject *lastCircle;
-};
+class MapWidget;
 
 class MainWindow : public QMainWindow
 {
