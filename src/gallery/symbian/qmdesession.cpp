qtproject/qt-mobility

commit 807a6ee37e23277f19cf980689bebb8076d23f81

Symbian: MOBILITY-1530
- documentGallaery backend does not report if metadata write was success
- added return for a method that really returns the value
- removed compile warnings for arm compiler
- removed unneeded conversion methods

Reviewed-By: Tero Saarni

src/gallery/symbian/qmdesession.cpp
@@ -77,10 +77,9 @@ void QMdeSession::HandleSessionError(CMdESession& /*aSession*/, TInt /*aError*/)
 
 CMdENamespaceDef& QMdeSession::GetDefaultNamespaceDefL()
 {
-    if (m_cmdeSession) {
+
 #ifdef MDS_25_COMPILATION_ENABLED
-        return m_cmdeSession->GetDefaultNamespaceDefL();
-    }
+    return m_cmdeSession->GetDefaultNamespaceDefL();
 #else
     CMdENamespaceDef *nameSpaceDef = NULL;
         nameSpaceDef = m_cmdeSession->GetDefaultNamespaceDefL();

src/gallery/symbian/qgallerymdsutility.cpp
@@ -1559,41 +1559,6 @@ HBufC* QDocumentGalleryMDSUtility::qStringToS60Desc(const QString& string)
     return str.Alloc();
 }
 
-/*!
- Converts \a desc a Symbian S60 8 bit descriptor (UTF8 string) to a QString.
- Returns the converted QString on success; otherwise returns null QString
- */
-QString QDocumentGalleryMDSUtility::s60Desc8ToQString(const TDesC8& desc)
-{
-    QString qtString;
-    HBufC* s60str;
-    TRAPD(error, s60str = CnvUtfConverter::ConvertToUnicodeFromUtf8L(desc));
-    if (error == KErrNone) {
-        qtString = QString::fromUtf16(s60str->Ptr(), s60str->Length());
-        delete s60str;
-    }
-    return qtString;
-}
-
-/*!
- Converts \a string , a QString, to a Symbian S60 8 bit descriptor (UTF8 string).
-
- Note: Ownership of the returned descriptor (string) is transferred to the caller
-
- Returns a pointer to a Symbian S60 descriptor containing the UTF8 string on success;
- otherwise returns NULL pointer
- */
-HBufC8* QDocumentGalleryMDSUtility::qStringToS60Desc8(const QString& string)
-{
-    TPtrC16 str(reinterpret_cast<const TUint16*>(string.utf16()));
-    HBufC8* s60str;
-    TRAPD(error, s60str = CnvUtfConverter::ConvertFromUnicodeToUtf8L(str));
-    if (error != KErrNone) {
-        return NULL;
-    }
-    return s60str;
-}
-
 QDateTime QDocumentGalleryMDSUtility::symbianTTimetoQDateTime(const TTime& time)
 {
     TDateTime dateTime = time.DateTime();
@@ -2086,9 +2051,9 @@ bool QDocumentGalleryMDSUtility::SetMetaDataFieldL( CMdEObject *item, const QVar
         return false;
     
 #ifdef MDS_25_COMPILATION_ENABLED
-    SetMetaDataFieldForMDS25L( item, value, key );
+    return SetMetaDataFieldForMDS25L( item, value, key );
 #else
-    SetMetaDataFieldForMDS20L( item, value, key );
+    return SetMetaDataFieldForMDS20L( item, value, key );
 #endif
 }
 

src/gallery/symbian/qgallerymdsutility_p.h
@@ -132,8 +132,6 @@ public:
 
     static QString s60DescToQString(const TDesC& desc);
     static HBufC *qStringToS60Desc(const QString& string);
-    static QString s60Desc8ToQString(const TDesC8& desc);
-    static HBufC8 *qStringToS60Desc8(const QString& string);
     static QDateTime symbianTTimetoQDateTime(const TTime& time);
     static TTime QDateTimetosymbianTTime(const QDateTime& time);
 

src/gallery/symbian/qmdegallerytyperesultset.cpp
@@ -108,7 +108,6 @@ bool QMDEGalleryTypeResultSet::fetch(int index)
         m_isValid = true;
         return true;
     }
-    return false;
 }
 
 bool QMDEGalleryTypeResultSet::fetchNext()
@@ -121,7 +120,6 @@ bool QMDEGalleryTypeResultSet::fetchNext()
         m_isValid = true;
         return true;
     }
-    return false;
 }
 
 bool QMDEGalleryTypeResultSet::fetchPrevious()
@@ -134,7 +132,6 @@ bool QMDEGalleryTypeResultSet::fetchPrevious()
         m_isValid = true;
         return true;
     }
-    return false;
 }
 
 bool QMDEGalleryTypeResultSet::fetchFirst()
@@ -147,7 +144,6 @@ bool QMDEGalleryTypeResultSet::fetchFirst()
         m_isValid = true;
         return true;
     }
-    return false;
 }
 
 bool QMDEGalleryTypeResultSet::fetchLast()
@@ -160,7 +156,6 @@ bool QMDEGalleryTypeResultSet::fetchLast()
         m_isValid = true;
         return true;
     }
-    return false;
 }
 
 #include "moc_qmdegallerytyperesultset_p.cpp"

commit fd1cde99470fe62d6abaf3cd003be25a35b48cbc

Symbian: fix build on MDS 2.0 platforms
break was introduced with commit 807a6ee37e23277f19cf980689bebb8076d23f81

src/gallery/symbian/qmdesession.cpp
@@ -77,7 +77,6 @@ void QMdeSession::HandleSessionError(CMdESession& /*aSession*/, TInt /*aError*/)
 
 CMdENamespaceDef& QMdeSession::GetDefaultNamespaceDefL()
 {
-
 #ifdef MDS_25_COMPILATION_ENABLED
     return m_cmdeSession->GetDefaultNamespaceDefL();
 #else
@@ -86,7 +85,6 @@ CMdENamespaceDef& QMdeSession::GetDefaultNamespaceDefL()
     if (!nameSpaceDef)
         User::Leave(KErrBadHandle);
     return *nameSpaceDef;    
-    }
 #endif //MDS_25_COMPILATION_ENABLED
 }
 