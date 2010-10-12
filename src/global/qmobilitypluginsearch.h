1f81270d7c250e7a762c6637f2686aa525bf34dd
Michael Goddard <michael.goddard@nokia.com>
2010-10-12 12:57:58 +1000

    Restore some of the old plugin directory checking code.
    It avoided connecting to the Symbian fileserver multiple times.
    
    Reviewed-by: Kevin Wu Won

--- src/global/qmobilitypluginsearch.h
+++ src/global/qmobilitypluginsearch.h
@@ -52,92 +52,57 @@
 
 QTM_BEGIN_NAMESPACE
 
-//class DirChecker
-//{
-//public:
-//    DirChecker();
-//    ~DirChecker();
-//    bool checkDir(const QDir& dir);
-
-//private:
-//#if defined(Q_OS_SYMBIAN)
-//    RFs rfs;
-//#endif
-//};
-
-//#if defined(Q_OS_SYMBIAN)
-//DirChecker::DirChecker()
-//{
-//    qt_symbian_throwIfError(rfs.Connect());
-//}
-
-//bool DirChecker::checkDir(const QDir& dir)
-//{
-//    bool pathFound = false;
-//    // In Symbian, going cdUp() in a c:/private/<uid3>/ will result in *platsec* error at fileserver (requires AllFiles capability)
-//    // Also, trying to cd() to a nonexistent directory causes *platsec* error. This does not cause functional harm, but should
-//    // nevertheless be changed to use native Symbian methods to avoid unnecessary platsec warnings (as per qpluginloader.cpp).
-//    // Use native Symbian code to check for directory existence, because checking
-//    // for files from under non-existent protected dir like E:/private/<uid> using
-//    // QDir::exists causes platform security violations on most apps.
-//    QString nativePath = QDir::toNativeSeparators(dir.absolutePath());
-//    TPtrC ptr = TPtrC16(static_cast<const TUint16*>(nativePath.utf16()), nativePath.length());
-//    TUint attributes;
-//    TInt err = rfs.Att(ptr, attributes);
-//    if (err == KErrNone) {
-//        // yes, the directory exists.
-//        pathFound = true;
-//    }
-//    return pathFound;
-//}
-
-//DirChecker::~DirChecker()
-//{
-//    rfs.Close();
-//}
-//#else
-//DirChecker::DirChecker()
-//{
-//}
-
-//DirChecker::~DirChecker()
-//{
-//}
-
-//bool DirChecker::checkDir(const QDir &dir)
-//{
-//    return dir.exists();
-//}
-//#endif
-
 #if defined(Q_OS_SYMBIAN)
-    inline bool checkMobilityPluginsDir(const QDir &dir)
-    {
-        RFs rfs;
-        qt_symbian_throwIfError(rfs.Connect());
-        bool pathFound = false;
-        // In Symbian, going cdUp() in a c:/private/<uid3>/ will result in *platsec* error at fileserver (requires AllFiles capability)
-        // Also, trying to cd() to a nonexistent directory causes *platsec* error. This does not cause functional harm, but should
-        // nevertheless be changed to use native Symbian methods to avoid unnecessary platsec warnings (as per qpluginloader.cpp).
-        // Use native Symbian code to check for directory existence, because checking
-        // for files from under non-existent protected dir like E:/private/<uid> using
-        // QDir::exists causes platform security violations on most apps.
-        QString nativePath = QDir::toNativeSeparators(dir.absolutePath());
-        TPtrC ptr = TPtrC16(static_cast<const TUint16*>(nativePath.utf16()), nativePath.length());
-        TUint attributes;
-        TInt err = rfs.Att(ptr, attributes);
-        if (err == KErrNone) {
-            // yes, the directory exists.
-            pathFound = true;
-        }
-        rfs.Close();
-        return pathFound;
+// This is here to avoid connecting to RFS multiple times.
+class DirChecker
+{
+public:
+    DirChecker();
+    ~DirChecker();
+    bool checkDir(const QDir& dir);
+
+private:
+    RFs rfs;
+};
+
+DirChecker::DirChecker()
+{
+    qt_symbian_throwIfError(rfs.Connect());
+}
+
+bool DirChecker::checkDir(const QDir& dir)
+{
+    bool pathFound = false;
+    // In Symbian, going cdUp() in a c:/private/<uid3>/ will result in *platsec* error at fileserver (requires AllFiles capability)
+    // Also, trying to cd() to a nonexistent directory causes *platsec* error. This does not cause functional harm, but should
+    // nevertheless be changed to use native Symbian methods to avoid unnecessary platsec warnings (as per qpluginloader.cpp).
+    // Use native Symbian code to check for directory existence, because checking
+    // for files from under non-existent protected dir like E:/private/<uid> using
+    // QDir::exists causes platform security violations on most apps.
+    QString nativePath = QDir::toNativeSeparators(dir.absolutePath());
+    TPtrC ptr = TPtrC16(static_cast<const TUint16*>(nativePath.utf16()), nativePath.length());
+    TUint attributes;
+    TInt err = rfs.Att(ptr, attributes);
+    if (err == KErrNone) {
+        // yes, the directory exists.
+        pathFound = true;
     }
+    return pathFound;
+}
+
+DirChecker::~DirChecker()
+{
+    rfs.Close();
+}
+
+#define DECL_CHECKDIR() DirChecker dirChecker
+#define CHECKDIR(dir) dirChecker.checkDir(dir)
+
 #else
-    inline bool checkMobilityPluginsDir(const QDir &dir)
-    {
-        return dir.exists();
-    }
+
+#define DECL_CHECKDIR()
+#define CHECKDIR(dir) (dir).exists()
+
 #endif
 
 inline QStringList mobilityPlugins(const QString plugintype)
@@ -155,7 +120,7 @@ inline QStringList mobilityPlugins(const QString plugintype)
         qDebug() << "Plugin paths:" << paths;
 #endif
 
-    //DirChecker dirChecker;
+    DECL_CHECKDIR();
 
     //temp variable to avoid multiple identic path
     QSet<QString> processed;
@@ -169,7 +134,7 @@ inline QStringList mobilityPlugins(const QString plugintype)
             continue;
         processed.insert(paths.at(i));
         QDir pluginsDir(paths.at(i));
-        if (!checkMobilityPluginsDir(pluginsDir))
+        if (!CHECKDIR(pluginsDir))
             continue;
 
 #if defined(Q_OS_WIN)
@@ -189,7 +154,7 @@ inline QStringList mobilityPlugins(const QString plugintype)
             || pluginsDir.path().endsWith(QLatin1String("/plugins/")))
             subdir = plugintype;
 
-        if (checkMobilityPluginsDir(QDir(pluginsDir.path() + QLatin1Char('/') + subdir))) {
+        if (CHECKDIR(QDir(pluginsDir.filePath(subdir)))) {
             pluginsDir.cd(subdir);
             QStringList files = pluginsDir.entryList(QDir::Files);
 
