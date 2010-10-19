3db39fd3d3f13e8a14d59e65bb7b2185f579c21e
Charles Yin <charles.yin@nokia.com>
2010-10-19 13:34:49 +1000

    Add documentation for contacts qml api.
    Relates to task: MOBILITY-1766
    Reviewed By: Michael Goddard

--- plugins/declarative/contacts/qdeclarativecontactfilter.cpp
+++ plugins/declarative/contacts/qdeclarativecontactfilter.cpp
@@ -40,3 +40,318 @@
 ****************************************************************************/
 
 #include "qdeclarativecontactfilter_p.h"
+
+/*!
+   \qmlclass Filter QDeclarativeContactFilter
+   \brief The Filter element is used as a property of ContactModel, to allow
+   selection of contacts which have certain details or properties.
+
+   \ingroup qml-contacts
+
+   This element is part of the \bold{QtMobility.contacts 1.1} module.
+
+   \sa QContactFilter
+ */
+
+
+/*!
+  \qmlproperty enumeration Filter::type
+
+  This property holds the type value of this filter. It can be one of:
+
+  \list
+  \o Filter.DefaultFilter - A filter which matches everything (default).
+  \o Filter.InvalidFilter - An invalid filter which matches nothing.
+  \o Filter.DetailFilter - A filter which matches contacts containing one or more details of a particular definition with a particular value.
+  \o Filter.DetailRangeFilter - A filter which matches contacts containing one or more details of a particular definition whose values are within a particular range.
+  \o Filter.ChangeLogFilter - A filter which matches contacts whose timestamps have been updated since some particular date and time.
+  \o Filter.ActionFilter - A filter which matches contacts for which a particular action is available, or which contain a detail with a particular value for which a particular action is available.
+  \o Filter.RelationshipFilter - A filter which matches contacts which participate in a particular type of relationship, or relationship with a specified contact.
+  \o Filter.IntersectionFilter - A filter which matches all contacts that are matched by all filters it includes.
+  \o Filter.UnionFilter - A filter which matches any contact that is matched by any of the filters it includes.
+  \o Filter.IdFilter - A filter which matches any contact whose local id is contained in a particular list of contact local ids.
+  \endlist
+  */
+
+/*!
+   \qmlclass ActionFilter QDeclarativeContactActionFilter
+   \brief The ActionFilter element provides a filter based around an action availability criterion.
+
+   \ingroup qml-contacts
+
+   This element is part of the \bold{QtMobility.contacts 1.1} module.
+
+   \sa QContactActionFilter
+ */
+
+/*!
+  \qmlproperty string actionName ActionFilter::actionName
+
+  This property holds the action name criterion of the filter.
+  */
+
+/*!
+   \qmlclass ChangeLogFilter QDeclarativeContactChangeLogFilter
+   \brief The ChangeLogFilter element provides a filter based around a contact timestamp criterion.
+
+   \ingroup qml-contacts
+
+   This element is part of the \bold{QtMobility.contacts 1.1} module.
+
+   \sa QContactChangeLogFilter
+ */
+
+/*!
+  \qmlproperty date ChangeLogFilter::since
+
+  This property holds the date and time lower-bound criterion of the filter.
+  */
+
+/*!
+  \qmlproperty enumeration ChangeLogFilter::eventType
+
+  This property holds the type of change that this filter will match against.
+  */
+
+/*!
+   \qmlclass DetailFilter QDeclarativeContactDetailFilter
+   \brief The DetailFilter element provides a filter based around a detail value criterion.
+
+   \ingroup qml-contacts
+
+   This element is part of the \bold{QtMobility.contacts 1.1} module.
+
+   \sa QContactDetailFilter
+ */
+
+/*!
+  \qmlproperty enumeration DetailFilter::detail
+
+  This property holds the detail type of which details will be matched to.
+
+  \sa ContactDetail::type
+  */
+
+/*!
+  \qmlproperty int DetailFilter::field
+
+  This property holds the detail field type of which detail fields will be matched to.
+  Detail field types are enumeration values defined in each detail elements.
+
+  \sa Address
+  \sa Anniversary
+  \sa Avatar
+  \sa Birthday
+  \sa DisplayLabel
+  \sa Email
+  \sa Family
+  \sa Favorite
+  \sa Gender
+  \sa Geolocation
+  \sa GlobalPresence
+  \sa Guid
+  \sa Name
+  \sa NickName
+  \sa Note
+  \sa OnlineAccount
+  \sa Organization
+  \sa PhoneNumber
+  \sa Presence
+  \sa Ringtone
+  \sa SyncTarget
+  \sa Tag
+  \sa Timestamp
+  \sa Url
+  \sa Hobby
+  */
+
+/*!
+  \qmlproperty variant DetailFilter::value
+
+  This property holds the value criterion of the detail filter.
+  */
+
+/*!
+  \qmlproperty enumeration DetailFilter::matchFlags
+
+  This property holds the semantics of the value matching criterion.
+  */
+
+/*!
+   \qmlclass DetailRangeFilter QDeclarativeContactDetailRangeFilter
+   \brief The DetailRangeFilter element provides a filter based around a detail value range criterion.
+
+
+   \ingroup qml-contacts
+
+   This element is part of the \bold{QtMobility.contacts 1.1} module.
+
+   \sa QContactDetailRangeFilter
+ */
+
+/*!
+  \qmlproperty enumeration DetailRangeFilter::detail
+
+  This property holds the detail type of which details will be matched to.
+
+  \sa ContactDetail::type
+  */
+
+/*!
+  \qmlproperty int DetailRangeFilter::field
+
+  This property holds the detail field type of which detail fields will be matched to.
+  Detail field types are enumeration values defined in each detail elements.
+
+  \sa Address
+  \sa Anniversary
+  \sa Avatar
+  \sa Birthday
+  \sa DisplayLabel
+  \sa Email
+  \sa Family
+  \sa Favorite
+  \sa Gender
+  \sa Geolocation
+  \sa GlobalPresence
+  \sa Guid
+  \sa Name
+  \sa NickName
+  \sa Note
+  \sa OnlineAccount
+  \sa Organization
+  \sa PhoneNumber
+  \sa Presence
+  \sa Ringtone
+  \sa SyncTarget
+  \sa Tag
+  \sa Timestamp
+  \sa Url
+  \sa Hobby
+  */
+/*!
+  \qmlproperty variant DetailRangeFilter::min
+
+  This property holds the lower bound of the value range criterion. By default, there is no lower bound.
+  */
+/*!
+  \qmlproperty variant DetailRangeFilter::max
+
+  This property holds the upper bound of the value range criterion. By default, there is no upper bound.
+  */
+
+/*!
+  \qmlproperty enumeration DetailRangeFilter::matchFlags
+
+  This property holds the match flags of the criterion, which define semantics such as case sensitivity, and exact matching.
+  */
+
+/*!
+  \qmlproperty enumeration DetailRangeFilter::rangeFlags
+
+  This property holds a set of flags which defines the boundary condition semantics of the value range criterion.
+  */
+
+/*!
+   \qmlclass IntersectionFilter QDeclarativeContactIntersectionFilter
+   \brief The IntersectionFilter element provides a filter which intersects the results of other filters.
+
+   \ingroup qml-contacts
+
+   This element is part of the \bold{QtMobility.contacts 1.1} module.
+
+   \sa QContactIntersectionFilter
+ */
+
+/*!
+  \qmlproperty QDeclarativeListProperty IntersectionFilter::filters
+
+  This property holds the list of filters which form the intersection filter.
+  */
+
+
+/*!
+   \qmlclass UnionFilter QDeclarativeContactUnionFilter
+   \brief The UnionFilter element provides a filter which unions the results of other filters.
+
+   \ingroup qml-contacts
+
+   This element is part of the \bold{QtMobility.contacts 1.1} module.
+
+   \sa QContactUnionFilter
+ */
+
+/*!
+  \qmlproperty QDeclarativeListProperty UnionFilter::filters
+
+  This property holds the list of filters which form the union filter.
+  */
+
+/*!
+   \qmlclass IdFilter QDeclarativeContactLocalIdFilter
+   \brief The IdFilter element provides a filter based around a list of contact ids.
+
+   \ingroup qml-contacts
+
+   This element is part of the \bold{QtMobility.contacts 1.1} module.
+
+   \sa QContactLocalIdFilter
+ */
+
+/*!
+  \qmlproperty list<string> IdFilter::ids
+
+  This property holds the list of ids of contacts which match this filter.
+  */
+
+
+/*!
+   \qmlclass RelationshipFilter QDeclarativeContactRelationshipFilter
+   \brief The RelationshipFilter element provides a filter based around relationship criteria.
+
+   \ingroup qml-contacts
+
+   This element is part of the \bold{QtMobility.contacts 1.1} module.
+
+   \sa Relationship
+   \sa RelationshipModel
+   \sa QContactRelationshipFilter
+   \sa QContactRelationship
+ */
+
+
+/*!
+  \qmlproperty variant RelationshipFilter::relationshipType
+
+  This property holds the type of relationship that a contact must have in order to match the filter.
+
+  \sa Relationship::type
+ */
+
+/*!
+  \qmlproperty int RelationshipFilter::relatedContactId
+
+  This property holds the id of the contact with whom the tested contact must have a relationship in order for the tested contact to match this filter
+  */
+/*!
+  \qmlproperty enumeration RelationshipFilter::relatedContactRole
+
+  This property holds the role in the relationship with the tested contact that the related contact must play in order for the tested contact to match this filter.
+  The role can be one of:
+  \list
+  \o Relationship.First - The contact is the first contact in the relationship.
+  \o Relationship.Second - The contact is the second contact in the relationship.
+  \o Relationship.Either - The contact is either the first or second contact in the relationship (default).
+  \endlist
+  */
+
+/*!
+   \qmlclass InvalidFilter QDeclarativeContactInvalidFilter
+   \brief the InvalidFilter element provides a filter which will never match any contacts.
+
+   \ingroup qml-contacts
+
+   This element is part of the \bold{QtMobility.contacts 1.1} module.
+
+   \sa QContactInvalidFilter
+ */
