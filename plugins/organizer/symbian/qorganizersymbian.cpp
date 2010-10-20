qtproject/qt-mobility

commit 534a9349c727db5ff555af4c0ba9b9d486f26f75

Fix crash in symbian backend
Id type was not checked by Symbian backend on retrieve or remove.
Relates to task MOBILITY-1332.

Reviewed By: Michael Goddard.

plugins/organizer/symbian/qorganizersymbian.cpp
@@ -682,6 +682,9 @@ QOrganizerItem QOrganizerItemSymbianEngine::itemL(const QOrganizerItemId& itemId
 {
 	Q_UNUSED(fetchHint)
 
+    if (organizeritemId.managerUri() != managerUri()) // XXX TODO: cache managerUri for fast lookup.
+        User::Leave(KErrNotFound);
+
     // Check collection id
     QOrganizerCollectionId collectionLocalId = getCollectionId(itemId);
     if (!m_collections.contains(collectionLocalId))
@@ -1124,6 +1127,9 @@ void QOrganizerItemSymbianEngine::removeItemL(
     const QOrganizerItemId& organizeritemId)
 {
     // TODO: How to remove item instances?
+
+    if (organizeritemId.managerUri() != managerUri()) // XXX TODO: cache managerUri for fast lookup.
+        User::Leave(KErrNotFound);
 
     QOrganizerCollectionId collectionId = getCollectionId(organizeritemId);
     if (!m_collections.contains(collectionId))

commit 4b1028c2560dbf02b770029e8066f12d7e29ff32

Fix compile failure
Introduced by commit 534a9349c727db5ff555af4c0ba9b9d486f26f75
Relates to task MOBILITY-1332

plugins/organizer/symbian/qorganizersymbian.cpp
@@ -682,7 +682,7 @@ QOrganizerItem QOrganizerItemSymbianEngine::itemL(const QOrganizerItemId& itemId
 {
 	Q_UNUSED(fetchHint)
 
-    if (organizeritemId.managerUri() != managerUri()) // XXX TODO: cache managerUri for fast lookup.
+    if (itemId.managerUri() != managerUri()) // XXX TODO: cache managerUri for fast lookup.
         User::Leave(KErrNotFound);
 
     // Check collection id