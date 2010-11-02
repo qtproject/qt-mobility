130247f04382ce2aa52d8d927d270ced7df59b17
Srikanth Parshi <srikanth.parshi@nokia.com>
2010-11-02 15:19:44 +0530

    Fixing qt codingstyle format warnings for MOBILITY-831 and 832

--- src/publishsubscribe/psmapperserver_symbian/pspathmapperserver.cpp
+++ src/publishsubscribe/psmapperserver_symbian/pspathmapperserver.cpp
@@ -1,43 +1,43 @@
 /****************************************************************************
-**
-** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
-** All rights reserved.
-** Contact: Nokia Corporation (qt-info@nokia.com)
-**
-** This file is part of the Qt Mobility Components.
-**
-** $QT_BEGIN_LICENSE:LGPL$
-** No Commercial Usage
-** This file contains pre-release code and may not be distributed.
-** You may use this file in accordance with the terms and conditions
-** contained in the Technology Preview License Agreement accompanying
-** this package.
-**
-** GNU Lesser General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU Lesser
-** General Public License version 2.1 as published by the Free Software
-** Foundation and appearing in the file LICENSE.LGPL included in the
-** packaging of this file.  Please review the following information to
-** ensure the GNU Lesser General Public License version 2.1 requirements
-** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-** In addition, as a special exception, Nokia gives you certain additional
-** rights.  These rights are described in the Nokia Qt LGPL Exception
-** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
-**
-** If you have questions regarding the use of this file, please contact
-** Nokia at qt-info@nokia.com.
-**
-**
-**
-**
-**
-**
-**
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
+ **
+ ** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+ ** All rights reserved.
+ ** Contact: Nokia Corporation (qt-info@nokia.com)
+ **
+ ** This file is part of the Qt Mobility Components.
+ **
+ ** $QT_BEGIN_LICENSE:LGPL$
+ ** No Commercial Usage
+ ** This file contains pre-release code and may not be distributed.
+ ** You may use this file in accordance with the terms and conditions
+ ** contained in the Technology Preview License Agreement accompanying
+ ** this package.
+ **
+ ** GNU Lesser General Public License Usage
+ ** Alternatively, this file may be used under the terms of the GNU Lesser
+ ** General Public License version 2.1 as published by the Free Software
+ ** Foundation and appearing in the file LICENSE.LGPL included in the
+ ** packaging of this file.  Please review the following information to
+ ** ensure the GNU Lesser General Public License version 2.1 requirements
+ ** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+ **
+ ** In addition, as a special exception, Nokia gives you certain additional
+ ** rights.  These rights are described in the Nokia Qt LGPL Exception
+ ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+ **
+ ** If you have questions regarding the use of this file, please contact
+ ** Nokia at qt-info@nokia.com.
+ **
+ **
+ **
+ **
+ **
+ **
+ **
+ **
+ ** $QT_END_LICENSE$
+ **
+ ****************************************************************************/
 #include "pspathmapperserver.h"
 #include "clientservercommon.h"
 #include "pspathmappersession.h"
@@ -51,13 +51,13 @@ static TInt Timeout(TAny *aObject);
 const TInt CPSPathMapperServer::timeoutInterval = 30000000; // 30 seconds
 
 TInt Timeout(TAny *aObject)
-  {
-    ((CPSPathMapperServer *)aObject)->Shutdown();
+{
+    ((CPSPathMapperServer *) aObject)->Shutdown();
     return 1;
-  }
+}
 
-CPSPathMapperServer::CPSPathMapperServer()
-    : CServer2(EPriorityNormal, ESharableSessions), iSessionCount(0)
+CPSPathMapperServer::CPSPathMapperServer() :
+    CServer2(EPriorityNormal, ESharableSessions), iSessionCount(0)
 {
     iPeriodic = CPeriodic::NewL(0);
     iPeriodic->Start(timeoutInterval, timeoutInterval, TCallBack(Timeout, this));
@@ -65,12 +65,13 @@ CPSPathMapperServer::CPSPathMapperServer()
 
 CSession2 *CPSPathMapperServer::NewSessionL(const TVersion &aVersion, const RMessage2 & /*aMessage*/) const
 {
-    if (!User::QueryVersionSupported(TVersion(KServerMajorVersionNumber, 
-        KServerMinorVersionNumber, KServerBuildVersionNumber), aVersion)) {
-        User::Leave(KErrNotSupported);
+    if (!User::QueryVersionSupported(TVersion(KServerMajorVersionNumber, KServerMinorVersionNumber,
+        KServerBuildVersionNumber), aVersion)) {
+        User::Leave( KErrNotSupported);
     }
 
-    return new (ELeave) CPSPathMapperServerSession(iPathMapper, *const_cast<CPSPathMapperServer*>(this));
+    return new (ELeave) CPSPathMapperServerSession(iPathMapper,
+        *const_cast<CPSPathMapperServer*> (this));
 }
 
 void CPSPathMapperServer::PanicServer(TPSPathMapperServerPanic aPanic)
@@ -87,10 +88,9 @@ void CPSPathMapperServer::IncreaseSessions()
 
 void CPSPathMapperServer::DecreaseSessions()
 {
-	if (iSessionCount > 0)
-	    iSessionCount--;
-    if (iSessionCount == 0)
-    {
+    if (iSessionCount > 0)
+        iSessionCount--;
+    if (iSessionCount == 0) {
         iPeriodic->Start(timeoutInterval, timeoutInterval, TCallBack(Timeout, this));
     }
 }
@@ -99,7 +99,6 @@ void CPSPathMapperServer::Shutdown()
 {
     QCoreApplication::exit(0);
 }
-
 
 QTM_END_NAMESPACE
 
