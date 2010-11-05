caaddd67d11c5ee45b15afe0900c6308e42c08d8
Michael Goddard <michael.goddard@nokia.com>
2010-11-05 11:37:28 +1000

    Compile fix for MMK backend.
    Change-Id: I293f53738680f9801af6031081d1c68b93c1922f
    Task-number: QTMOBILITY-687

--- plugins/feedback/mmk/qfeedback.cpp
+++ plugins/feedback/mmk/qfeedback.cpp
@@ -81,7 +81,7 @@ void QFeedbackMMK::setLoaded(QFeedbackFileEffect *effect, bool load)
                 // New sound effect!
                 fi.soundEffect = new QSoundEffect(this);
                 connect(fi.soundEffect, SIGNAL(loadedChanged()), this, SLOT(soundEffectLoaded()));
-                connect(fi.soundEffect, SIGNAL(playingChanged())), this, SLOT(soundEffectPlayingChanged());
+                connect(fi.soundEffect, SIGNAL(playingChanged()), this, SLOT(soundEffectPlayingChanged()));
                 fi.soundEffect->setSource(effect->source());
                 mEffects.insert(effect, fi);
                 mEffectMap.insert(fi.soundEffect, effect);
@@ -171,7 +171,8 @@ void QFeedbackMMK::soundEffectPlayingChanged()
 {
     QSoundEffect* se = qobject_cast<QSoundEffect*>(sender());
     if (se) {
-        FeedbackInfo fi = mEffects.value(se);
+        QFeedbackFileEffect* fileEffect = mEffectMap.value(se);
+        FeedbackInfo fi = mEffects.value(fileEffect);
 
         if (fi.soundEffect == se) {
             fi.playing = se->isPlaying();
