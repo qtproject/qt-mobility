2722dc514525ee8e2b9d437a84aab0c5b0aaf503
Andrew den Exter <andrew.den-exter@nokia.com>
2010-11-11 14:33:31 +1000

    Show the document gallery examples maximised on maemo 6.
    Or full screen in the case of the declarative music browser.

--- examples/documentproperties/main.cpp
+++ examples/documentproperties/main.cpp
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
 
     FileBrowser widget;
 
-#if defined(Q_OS_SYMBIAN) || defined(Q_WS_MAEMO_5)
+#if defined(Q_OS_SYMBIAN) || defined(Q_WS_MAEMO_5) || defined(Q_WS_MAEMO_6)
     widget.showMaximized();
 #else
     widget.show();
