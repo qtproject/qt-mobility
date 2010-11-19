qtproject/qt-mobility

commit 89490ecde47bf258851fe13e1e05bee72ef78f19

Only link against needed Qt libraries.

src/connectivity/bluetooth/bluetooth.pri
@@ -62,7 +62,7 @@ symbian {
         
     LIBS *= -lesock -lbluetooth -lsdpagent -lsdpdatabase -lestlib
 } else:contains(QT_CONFIG, dbus) {
-    QT += dbus
+    QT *= dbus
 
     include(bluez/bluez.pri)
 

src/connectivity/connectivity.pro
@@ -6,6 +6,8 @@ include(../../common.pri)
 
 DEFINES += QT_BUILD_CONNECTIVITY_LIB QT_MAKEDLL
 
+QT = core
+
 include(nfc/nfc.pri)
 !win32:include(bluetooth/bluetooth.pri)
 

commit 6395140233a723a51605185e63019ed4a402d095

Attempt to fix build on Symbian^4.
I believe it was broken by 89490ecde47bf258851fe13e1e05bee72ef78f19.

src/connectivity/bluetooth/bluetooth.pri
@@ -48,6 +48,8 @@ SOURCES += \
     bluetooth/qbluetoothtransferreply.cpp
 
 symbian {
+    QT *= network
+
     PRIVATE_HEADERS += \
         bluetooth/utils_symbian_p.h\
 		bluetooth/qbluetoothdevicediscoveryagent_symbian_p.h        