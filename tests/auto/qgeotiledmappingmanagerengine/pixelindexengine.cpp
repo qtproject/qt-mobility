1887b1ac5b61433bac13d0ec10e9dfe7c0f3878c
Alex Wilson <alex.wilson@nokia.com>
2010-12-01 08:12:56 +1000

    Adding license headers and comments to tests for QGeoTiledMappingManagerEngine

--- tests/auto/qgeotiledmappingmanagerengine/pixelindexengine.cpp
+++ tests/auto/qgeotiledmappingmanagerengine/pixelindexengine.cpp
@@ -1,3 +1,44 @@
+/****************************************************************************
+**
+** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the Qt Mobility Components.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** No Commercial Usage
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the Technology Preview License Agreement accompanying
+** this package.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights.  These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at qt-info@nokia.com.
+**
+**
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
 #include "pixelindexengine.h"
 #include <QBuffer>
 #include <QByteArray>
@@ -19,6 +60,7 @@ PixmapTiledMapReply::PixmapTiledMapReply(QPixmap &pixmap, const QGeoTiledMapRequ
 PixelIndexEngine::PixelIndexEngine(const QMap<QString, QVariant> &parameters, QObject *parent) :
     QGeoTiledMappingManagerEngine(parameters, parent)
 {
+    // might want to parametrise these for future use
     setTileSize(QSize(128,128));
     setMinimumZoomLevel(0.00);
     setMaximumZoomLevel(3.0);
@@ -44,6 +86,7 @@ QGeoTiledMapReply *PixelIndexEngine::getTileImage(const QGeoTiledMapRequest &req
             tpv.py = request.row()*128 + y;
             tpv.px = request.column()*128 + x;
             tpv.zoom = request.zoomLevel();
+
             im.setPixel(x, y, tpv.rgb);
         }
     }
--- tests/auto/qgeotiledmappingmanagerengine/pixelindexengine.h
+++ tests/auto/qgeotiledmappingmanagerengine/pixelindexengine.h
@@ -1,3 +1,44 @@
+/****************************************************************************
+**
+** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the Qt Mobility Components.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** No Commercial Usage
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the Technology Preview License Agreement accompanying
+** this package.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights.  These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at qt-info@nokia.com.
+**
+**
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
 #ifndef PIXELINDEXENGINE_H
 #define PIXELINDEXENGINE_H
 
@@ -24,26 +65,34 @@ typedef union {
     };
 } TilePixelValue;
 
+/*!
+  A tiled map reply subclass that is constructed already finished, with
+  a given QPixmap as its content (saved in PNG format).
+  */
 class PixmapTiledMapReply : public QGeoTiledMapReply
 {
     Q_OBJECT
 public:
-    PixmapTiledMapReply(QPixmap &pixmap, const QGeoTiledMapRequest &request, QObject *parent=0);
+    PixmapTiledMapReply(QPixmap &pixmap, const QGeoTiledMapRequest &request,
+                        QObject *parent=0);
 
 private:
     QPixmap m_pixmap;
 };
 
+/*!
+  Tiled map engine that returns tiles made up of pixels with their color set
+  to a TilePixelValue for that pixel's location and zoom level. Used for testing
+  the map rendering code.
+  */
 class PixelIndexEngine : public QGeoTiledMappingManagerEngine
 {
     Q_OBJECT
 public:
-    PixelIndexEngine(const QMap<QString, QVariant> &parameters, QObject *parent=0);
+    PixelIndexEngine(const QMap<QString, QVariant> &parameters,
+                     QObject *parent=0);
+
     QGeoTiledMapReply *getTileImage(const QGeoTiledMapRequest &request);
-
-signals:
-
-public slots:
 
 };
 
