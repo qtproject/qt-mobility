3289d7bb11500494efb02b16313f8d7bb7ee4f62
Alex Wilson <alex.wilson@nokia.com>
2010-12-01 08:12:56 +1000

    Adding license headers and comments to tests for QGeoTiledMappingManagerEngine

--- tests/auto/qgeotiledmappingmanagerengine/tst_qgeotiledmappingmanagerengine.cpp
+++ tests/auto/qgeotiledmappingmanagerengine/tst_qgeotiledmappingmanagerengine.cpp
@@ -1,3 +1,46 @@
+/****************************************************************************
+**
+** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the Qt Mobility Components.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** No Commercial Usage
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the Technology Preview License Agreement accompanying
+** this package.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights.  These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at qt-info@nokia.com.
+**
+**
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+//TESTED_COMPONENT=src/location
+
 #include "../qlocationtestutils_p.h"
 
 #include <qgeocoordinate.h>
@@ -63,6 +106,10 @@ void tst_QGeoTiledMappingManagerEngine::centering_data()
     QTest::newRow("Just before the date line") << 3.0 << -85.0 << 179.99;
 }
 
+/*!
+  Tests that the pixel rendered at the center of the view lines up with what
+  coordinateToWorldReferencePosition() says it should be.
+  */
 void tst_QGeoTiledMappingManagerEngine::centering()
 {
     QFETCH(qreal, zoom);
@@ -94,6 +141,10 @@ void tst_QGeoTiledMappingManagerEngine::centering()
     QCOMPARE(tpv.py, static_cast<uint>(centerPixel.y()));
 }
 
+/*!
+  Tests tile boundaries near the center of the view to see that they form nice
+  continuous lines of pixels
+  */
 void tst_QGeoTiledMappingManagerEngine::stitching()
 {
     QGeoCoordinate center(0.0, -135.0);
@@ -140,6 +191,10 @@ void tst_QGeoTiledMappingManagerEngine::zoomLevels_data()
     QTest::newRow("zoom level 3") << 3.0 << 512u << 512u;
 }
 
+/*!
+  Tests that the center pixel has the correct coordinates at different
+  zoom levels.
+  */
 void tst_QGeoTiledMappingManagerEngine::zoomLevels()
 {
     QFETCH(qreal, zoom);
@@ -197,6 +252,10 @@ void tst_QGeoTiledMappingManagerEngine::sizes_data()
                                                  << QPoint(512+407, 512-52);
 }
 
+/*!
+  Tests that the center and edges of the viewport are rendering the correct
+  pixels at different viewport sizes.
+  */
 void tst_QGeoTiledMappingManagerEngine::sizes()
 {
     QFETCH(int, width);
@@ -236,6 +295,10 @@ void tst_QGeoTiledMappingManagerEngine::sizes()
     QCOMPARE(tpv.py, static_cast<uint>(topRight.y()));
 }
 
+/*!
+  Tests that the paint() function does not draw outside its assigned
+  boundaries.
+  */
 void tst_QGeoTiledMappingManagerEngine::paintLimits()
 {
     gmd->setWindowSize(QSizeF(200, 200));
