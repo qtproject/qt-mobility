2c13ea2a6eff687ddb744e65739469ca17056288
Lorn Potter <lorn.potter@nokia.com>
2010-12-07 17:24:25 +1000

    use something better to highlight the arguments in the docs.

--- src/systeminfo/qsystemstorageinfo.cpp
+++ src/systeminfo/qsystemstorageinfo.cpp
@@ -92,8 +92,8 @@ QSystemStorageInfoPrivate *getSystemStorageInfoPrivate() { return storageInfoPri
 /*!
      \fn void QSystemStorageInfo::storageStateChanged(const QString &vol, QSystemStorageInfo::StorageState state)
 
-     This signal gets emitted when a volume has changed from one StorageState to another, \vol
-     being the volume name, and \state being the new state..
+     This signal gets emitted when a volume has changed from one StorageState to another, \a vol
+     being the volume name, and \a state being the new state..
 */
 
 
