543df77cbe63f903030f2ef60ef079cbfe8d08f7
Leela Prasanna Kumar Chintagunta <leelaprasannakumar.chintagunta@nokia.com>
2010-12-09 16:05:58 +0530

    Fixing flowcontrol and tab style issues QMobility-1876 and QMobility-1381

--- src/systeminfo/qsysteminfo_s60_p.h
+++ src/systeminfo/qsysteminfo_s60_p.h
@@ -281,8 +281,7 @@ private:    //data
 
     CRepository *m_bluetoothRepository;
     CCenRepNotifyHandler *m_bluetoothNotifyHandler;
-
-	QSystemDeviceInfo::BatteryStatus m_previousBatteryStatus;
+    QSystemDeviceInfo::BatteryStatus m_previousBatteryStatus;
 };
 
 //////// QSystemScreenSaver
