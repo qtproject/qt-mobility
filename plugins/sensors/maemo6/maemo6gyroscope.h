4a3205790f51781e716257ca4d7299804cf49e57
p2niemel <pia.s.niemela@nokia.com>
2010-12-13 11:50:29 +0200

    gyro added

---
+++ plugins/sensors/maemo6/maemo6gyroscope.h
@@ -0,0 +1,30 @@
+#ifndef MAEMO6GYROSCOPE_H
+#define MAEMO6GYROSCOPE_H
+
+#include "maemo6sensorbase.h"
+#include <qgyroscope.h>
+#include <gyroscopesensor_i.h>
+#include <datatypes/xyz.h>
+
+
+QTM_USE_NAMESPACE
+
+class maemo6gyroscope : public maemo6sensorbase
+{
+    Q_OBJECT
+
+public:
+    static char const * const id;
+    maemo6gyroscope(QSensor *sensor);
+
+private:
+    QGyroscopeReading m_reading;
+    static bool m_initDone;
+    static qreal const MILLI;
+                          
+private slots:
+    void slotDataAvailable(const XYZ& data);
+};
+
+
+#endif // MAEMO6GYROSCOPE_H
