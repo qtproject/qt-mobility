38b9446340218eaaf4a6072e0fe96a18ced23945
Aaron McCarthy <aaron.mccarthy@nokia.com>
2010-12-14 17:36:56 +1000

    Split emulator backend code into multiple source files.

---
+++ src/connectivity/nfc/qnearfieldtarget_emulator_p.h
@@ -0,0 +1,96 @@
+/****************************************************************************
+**
+** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the Qt Mobility Components.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** No Commercial Usage
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the Technology Preview License Agreement accompanying
+** this package.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights.  These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at qt-info@nokia.com.
+**
+**
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#ifndef QNEARFIELDTARGET_EMULATOR_P_H
+#define QNEARFIELDTARGET_EMULATOR_P_H
+
+#include "qnearfieldtagtype1.h"
+#include "targetemulator_p.h"
+
+#include <QtCore/QMap>
+
+QTM_USE_NAMESPACE
+
+class TagType1 : public QNearFieldTagType1
+{
+    Q_OBJECT
+
+public:
+    TagType1(TagBase *tag, QObject *parent);
+    ~TagType1();
+
+    QByteArray uid() const;
+
+    AccessMethods accessMethods() const;
+
+    RequestId sendCommand(const QByteArray &command);
+    bool waitForRequestCompleted(const RequestId &id, int msecs = 5000);
+
+private:
+    TagBase *m_tag;
+};
+
+class TagActivator : public QObject
+{
+    Q_OBJECT
+
+public:
+    TagActivator();
+    ~TagActivator();
+
+    void initialize();
+    void reset();
+
+    static TagActivator *instance();
+
+protected:
+    void timerEvent(QTimerEvent *e);
+
+signals:
+    void tagActivated(TagBase *tag);
+    void tagDeactivated(TagBase *tag);
+
+private:
+    QMap<TagBase *, bool>::Iterator m_current;
+    int timerId;
+};
+
+#endif // QNEARFIELDTARGET_EMULATOR_P_H
