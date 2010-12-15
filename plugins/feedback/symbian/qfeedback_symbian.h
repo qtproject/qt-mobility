b90c81afb2bd90becc6b6b76e3b4bde8b3f57066
Chris Adams <christopher.adams@nokia.com>
2010-12-15 16:31:18 +1000

    Use S^3 Haptics API for S^3 Feedback plugin
    This commit changes the symbian feedback plugin to use the
    CHWRMHaptics API on S^3.
    
    Task-number: MOBILITY-1631
    Reviewed-by: Michael Goddard

--- plugins/feedback/symbian/qfeedback_symbian.h
+++ plugins/feedback/symbian/qfeedback_symbian.h
@@ -51,28 +51,52 @@
 #include <QtCore/QTime>
 #endif
 
-
 #include <qmobilityglobal.h>
-
 #include <qfeedbackplugininterfaces.h>
-
 #include <e32base.h>
-#include <hwrmvibra.h>
 
+#ifdef USE_CHWRMVIBRA_PLZ
+#include <hwrmvibra.h>
+#define VIBRA_DEVICE 0
+#endif //USE_CHWRMVIBRA_PLZ
+
+#ifdef HAS_THEME_EFFECTS
+class MTouchFeedback;
+#ifdef HAS_ADVANCED_TACTILE_SUPPORT
+#define TOUCH_DEVICE 1
+#endif //HAS_ADVANCED_TACTILE_SUPPORT
+#endif //HAS_THEME_EFFECTS
+
+#ifdef USE_CHWRMHAPTICS_PLZ
+#include <hwrmhaptics.h>
+#endif //USE_CHWRMHAPTICS_PLZ
 
 QT_BEGIN_HEADER
 QTM_USE_NAMESPACE
 
+// We have theme effects on 5.0 onwards
+// and file effects if CHWRMHaptics is found (s^3 onwards?)
+
+#ifdef HAS_CUSTOM_EFFECTS
 class QFeedbackSymbian : public QObject, public QFeedbackHapticsInterface
-#ifndef NO_TACTILE_SUPPORT
+#ifdef HAS_THEME_EFFECTS
 , public QFeedbackThemeInterface
-#endif //NO_TACTILE_SUPPORT
+#endif
+#ifdef HAS_FILE_EFFECTS
+, public QFeedbackFileInterface
+#endif
+#ifdef USE_CHWRMHAPTICS_PLZ
+, public MHWRMHapticsObserver
+#endif
 {
     Q_OBJECT
     Q_INTERFACES(QTM_NAMESPACE::QFeedbackHapticsInterface)
-#ifndef NO_TACTILE_SUPPORT
+#ifdef HAS_THEME_EFFECTS
     Q_INTERFACES(QTM_NAMESPACE::QFeedbackThemeInterface)
-#endif //NO_TACTILE_SUPPORT
+#endif
+#ifdef HAS_FILE_EFFECTS
+    Q_INTERFACES(QTM_NAMESPACE::QFeedbackFileInterface)
+#endif
 public:
     QFeedbackSymbian();
     virtual ~QFeedbackSymbian();
@@ -90,15 +114,28 @@ public:
     virtual void setEffectState(const QFeedbackHapticsEffect *, QFeedbackEffect::State);
     virtual QFeedbackEffect::State effectState(const QFeedbackHapticsEffect *);
 
-#ifndef NO_TACTILE_SUPPORT
+#ifdef HAS_THEME_EFFECTS
     virtual bool play(QFeedbackEffect::ThemeEffect);
-#endif // NO_TACTILE_SUPPORT
+#endif
+
+#ifdef HAS_FILE_EFFECTS
+    // File interface functions
+    virtual void setLoaded(QFeedbackFileEffect*, bool);
+    virtual void setEffectState(QFeedbackFileEffect *, QFeedbackEffect::State);
+    virtual QFeedbackEffect::State effectState(const QFeedbackFileEffect *);
+    virtual int effectDuration(const QFeedbackFileEffect*);
+    virtual QStringList supportedMimeTypes();
+#endif
+
+#ifdef USE_CHWRMHAPTICS_PLZ
+    // MHWRMHapticsObserver functions
+    void EffectCompletedL(TInt error, TInt handle);
+    void HapticsStatusChangedL(THWRMHapticsStatus aStatus);
+#endif
 
 private:
+#ifdef USE_CHWRMVIBRA_PLZ
     CHWRMVibra *vibra();
-
-    QList<QFeedbackActuator*> m_actuators;
-    CCoeControl *defaultWidget();
 
     CHWRMVibra *m_vibra;
     bool m_vibraActive;
@@ -112,7 +149,7 @@ private:
            m_elapsedTimer.invalidate();
 #else
             m_elapsedTimer = QTime();
-#endif
+#endif //QT_VERSION
         }
 
         ~PausableElapsedTimer()
@@ -145,7 +182,7 @@ private:
             m_elapsedTimer.invalidate();
 #else
             m_elapsedTimer = QTime();
-#endif
+#endif //QT_VERSION
             delete mTimer;
             mTimer = 0;
         }
@@ -165,14 +202,44 @@ private:
         QElapsedTimer m_elapsedTimer;
 #else
         QTime m_elapsedTimer;
-#endif
+#endif //QT_VERSION
         int pausedTime;
         QTimer* mTimer;
 
     };
 
     QHash<const QFeedbackHapticsEffect*, PausableElapsedTimer> m_elapsed;
+
+#endif //USE_CHWRMVIBRA_PLZ
+#ifdef USE_CHWRMHAPTICS_PLZ
+    CHWRMHaptics *haptics(int actuator);
+    CHWRMHaptics *fileHaptics();
+
+    QHash<THWRMLogicalActuators, CHWRMHaptics*> m_haptics;
+
+    struct FileContent {
+        FileContent() : fileHandle(0), refCount(0) { }
+        FileContent(const QByteArray &arr) : fileHandle(0), ba(arr), refCount(1) { }
+        TPtrC8 constData() const {return TPtrC8(reinterpret_cast<const unsigned char *>(ba.constData()), ba.length());}
+        TInt fileHandle;
+
+        QByteArray ba;
+        int refCount;
+    };
+    QHash<QString, FileContent> fileData;
+    QHash<const QFeedbackEffect*, TInt> effectHandles;
+    QHash<TInt, const QFeedbackEffect*> handleEffects;
+#endif //USE_CHWRMHAPTICS_PLZ
+
+#ifdef HAS_THEME_EFFECTS
+    static MTouchFeedback *touchInstance();
+#endif //HAS_THEME_EFFECTS
+
+    void initActuators();
+    QList<QFeedbackActuator*> m_actuators;
+    CCoeControl *defaultWidget();
 };
+#endif //HAS_CUSTOM_EFFECTS
 
 
 QT_END_HEADER
--- plugins/feedback/symbian/symbian.pro
+++ plugins/feedback/symbian/symbian.pro
@@ -5,25 +5,45 @@ PLUGIN_TYPE=feedback
 
 include(../../../common.pri)
 
-HEADERS += qfeedback.h
-SOURCES += qfeedback.cpp
+HEADERS += qfeedback_symbian.h
+SOURCES += qfeedback_symbian.cpp
 
 INCLUDEPATH += $$SOURCE_DIR/src/feedback
 
-LIBS += -lhwrmvibraclient
 CONFIG += mobility
 MOBILITY = feedback
 
 
 symbian {
     contains(S60_VERSION, 3.1)|contains(S60_VERSION, 3.2) {
-        DEFINES += NO_TACTILE_SUPPORT
+        # S60 3rd edition does not support theme effects, nor does it support
+        # envelope modifiers to custom effects (attack and fade).
     } else {
+        # S60 5th edition supports theme effects, but it does not support
+        # envelope modifiers to custom effects (attack and fade).
+        DEFINES += HAS_THEME_EFFECTS
+        SOURCES += qfeedback_themed.cpp
         LIBS += -ltouchfeedback
     }
 
     contains(advancedtouchfeedback_enabled, yes) {
-        DEFINES += ADVANCED_TACTILE_SUPPORT
+        # this uses the same lib and headers as touchfeedback
+        # but on some platforms (S^3) includes more theme effects.
+        DEFINES += HAS_ADVANCED_TACTILE_SUPPORT
+    }
+
+    contains(chwrmhaptics_enabled, yes) {
+        DEFINES += HAS_FILE_EFFECTS
+        DEFINES += HAS_CUSTOM_EFFECTS
+        DEFINES += HAS_ENVELOPE_SUPPORT
+        DEFINES += USE_CHWRMHAPTICS_PLZ
+        SOURCES += qfeedback_chwrmhaptics.cpp
+        LIBS += -lhwrmhapticsclient
+    } else {
+        DEFINES += HAS_CUSTOM_EFFECTS
+        DEFINES += USE_CHWRMVIBRA_PLZ
+        SOURCES += qfeedback_chwrmvibra.cpp
+        LIBS += -lhwrmvibraclient
     }
 
     TARGET.EPOCALLOWDLLDATA=1
