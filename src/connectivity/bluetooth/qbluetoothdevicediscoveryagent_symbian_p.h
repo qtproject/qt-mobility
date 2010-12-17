21edcace02f40bc67f1a15c4f3006297d064d040
Antti Luoma <antti.luoma@digia.com>
2010-12-17 16:42:37 +0200

    Symbian:Bluetooth: add a disclaimer about private headers

--- src/connectivity/bluetooth/qbluetoothdevicediscoveryagent_symbian_p.h
+++ src/connectivity/bluetooth/qbluetoothdevicediscoveryagent_symbian_p.h
@@ -42,6 +42,17 @@
 #ifndef QBLUETOOTHDEVICEDISCOVERYAGENT_SYMBIAN_P_H
 #define QBLUETOOTHDEVICEDISCOVERYAGENT_SYMBIAN_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API. It exists purely as an
+// implementation detail. This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include "qbluetoothdevicediscoveryagent_p.h"
 #include "symbian/bluetoothlinkmanagerdevicediscoverer.h"
 
--- src/connectivity/bluetooth/qbluetoothtransferreply_symbian_p.h
+++ src/connectivity/bluetooth/qbluetoothtransferreply_symbian_p.h
@@ -42,6 +42,17 @@
 #ifndef QBLUETOOTHTRANSFERREPLYSYMBIAN_H
 #define QBLUETOOTHTRANSFERREPLYSYMBIAN_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API. It exists purely as an
+// implementation detail. This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include "qbluetoothtransferreply.h"
 #include <e32base.h>
 #include <obex.h>
