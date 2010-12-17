8838746f9b72401847b739c82ceacb829f77b3aa
p2niemel <pia.s.niemela@nokia.com>
2010-12-17 15:17:44 +0200

    sensor error when no backend

--- plugins/sensors/maemo6/maemo6sensorbase.h
+++ plugins/sensors/maemo6/maemo6sensorbase.h
@@ -62,6 +62,7 @@ protected:
 
     static const float GRAVITY_EARTH;
     static const float GRAVITY_EARTH_THOUSANDTH;    //for speed
+    static const int KErrNotFound;
 
     void setRanges(qreal correctionFactor=1);
 
@@ -71,7 +72,7 @@ protected:
 
         if (!initDone) {
             if (!m_remoteSensorManager->loadPlugin(sensorName)){
-
+                sensorError(KErrNotFound);
                 return;
             }
             m_remoteSensorManager->registerSensorInterface<T>(sensorName);
@@ -81,6 +82,7 @@ protected:
             m_sensorInterface = const_cast<T*>(T::listenInterface(sensorName));
         }
         if (!m_sensorInterface) {
+            sensorError(KErrNotFound);
             return;
         }
 
