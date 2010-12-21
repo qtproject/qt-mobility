qtproject/qt-mobility

commit 9959cf2a80600a8333ac2def5a48e8588dd7bea6

Fix location of def files on linux-armcc builds.

common.pri
@@ -92,7 +92,7 @@ contains(build_unit_tests, yes) {
     !plugin {
         contains(TEMPLATE,.*lib) {
             DESTDIR = $$OUTPUT_DIR/lib
-            symbian:defFilePath=../s60installs
+            symbian:DEF_FILE=../s60installs
             VERSION = 1.2.0
         } else {
             DESTDIR = $$OUTPUT_DIR/bin

commit 16d0b6851439f32c7ac1995b409f8e9cd6876a57

Fix possible breakage caused by 9959cf2a.

common.pri
@@ -92,6 +92,7 @@ contains(build_unit_tests, yes) {
     !plugin {
         contains(TEMPLATE,.*lib) {
             DESTDIR = $$OUTPUT_DIR/lib
+            symbian:defFilePath=../s60installs
             symbian:DEF_FILE=../s60installs
             VERSION = 1.2.0
         } else {