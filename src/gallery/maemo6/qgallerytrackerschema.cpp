26d036975c51606217fca072e76c5d239bfc76a2
Andrew den Exter <andrew.den-exter@nokia.com>
2011-01-13 17:15:19 +1000

    Fix maemo6 document gallery schema test failures.
    Return an error if a filter value cannot be converted to a string, or
    if the value is a QRegExp then use the pattern for RegExp and Equals
    comparisons.

--- src/gallery/maemo6/qgallerytrackerschema.cpp
+++ src/gallery/maemo6/qgallerytrackerschema.cpp
@@ -400,26 +400,62 @@ static bool qt_writeCondition(
     return qt_writeConditionHelper( error, query, filter.filters(), properties, composites, "||" );
 }
 
-static void qt_write_comparison(
-        const QLatin1String &field, const QString &value, const char *op, QString *query)
+static bool qt_write_comparison(
+        QDocumentGallery::Error *error,
+        const QLatin1String &field,
+        const QVariant &value,
+        const char *op,
+        QString *query)
 {
-    *query += QLatin1String("(")
-            + field
-            + QLatin1String(op)
-            + QLatin1String("'")
-            + value
-            + QLatin1String("')");
+    if (value.canConvert(QVariant::String)) {
+        *query += QLatin1String("(")
+                + field
+                + QLatin1String(op)
+                + QLatin1String("'")
+                + value.toString()
+                + QLatin1String("')");
+        return true;
+    } else {
+        *error = QDocumentGallery::FilterError;
+        return false;
+    }
 }
 
-static void qt_write_function(
-        const char *function, const QString &field, const QString &value, QString *query)
+static bool qt_write_function(
+        QDocumentGallery::Error *,
+        const char *function,
+        const QString &field,
+        const QRegExp &regExp,
+        QString *query)
 {
     *query += QLatin1String(function)
             + QLatin1String("(")
             + field
             + QLatin1String(",\"")
-            + value
+            + regExp.pattern()
             + QLatin1String("\")");
+    return true;
+}
+
+static bool qt_write_function(
+        QDocumentGallery::Error *error,
+        const char *function,
+        const QString &field,
+        const QVariant &value,
+        QString *query)
+{
+    if (value.canConvert(QVariant::String)) {
+        *query += QLatin1String(function)
+                + QLatin1String("(")
+                + field
+                + QLatin1String(",\"")
+                + value.toString()
+                + QLatin1String("\")");
+        return true;
+    } else {
+        *error = QDocumentGallery::FilterError;
+        return false;
+    }
 }
 
 static bool qt_writeCondition(
@@ -437,41 +473,33 @@ static bool qt_writeCondition(
     int index;
 
     if ((index = properties.indexOfProperty(propertyName)) != -1) {
+        const QVariant value = filter.value();
+
         switch (filter.comparator()) {
         case QGalleryFilter::Equals:
-            qt_write_comparison(properties[index].field, filter.value().toString(), "=", query);
-            break;
+            return value.type() != QVariant::RegExp
+                    ? qt_write_comparison(error, properties[index].field, value, "=", query)
+                    : qt_write_function(error, "REGEX", properties[index].field, value.toRegExp(), query);
         case QGalleryFilter::LessThan:
-            qt_write_comparison(properties[index].field, filter.value().toString(), "<", query);
-            break;
+            return qt_write_comparison(error, properties[index].field, value, "<", query);
         case QGalleryFilter::GreaterThan:
-            qt_write_comparison(properties[index].field, filter.value().toString(), ">", query);
-            break;
+            return qt_write_comparison(error, properties[index].field, value, ">", query);
         case QGalleryFilter::LessThanEquals:
-            qt_write_comparison(properties[index].field, filter.value().toString(), "<=", query);
-            break;
+            return qt_write_comparison(error, properties[index].field, value, "<=", query);
         case QGalleryFilter::GreaterThanEquals:
-            qt_write_comparison(properties[index].field, filter.value().toString(), ">=", query);
-            break;
+            return qt_write_comparison(error, properties[index].field, value, ">=", query);
         case QGalleryFilter::Contains:
-            qt_write_function(
-                    "fn:contains", properties[index].field, filter.value().toString(), query);
-            break;
+            return  qt_write_function(error, "fn:contains", properties[index].field, value, query);
         case QGalleryFilter::StartsWith:
-            qt_write_function(
-                    "fn:starts-with", properties[index].field, filter.value().toString(), query);
-            break;
+            return qt_write_function(error, "fn:starts-with", properties[index].field, value, query);
         case QGalleryFilter::EndsWith:
-            qt_write_function(
-                    "fn:ends-with", properties[index].field, filter.value().toString(), query);
-            break;
+            return qt_write_function(error, "fn:ends-with", properties[index].field, value, query);
         case QGalleryFilter::Wildcard:
-            qt_write_function(
-                    "fn:contains", properties[index].field, "*", query);
-            break;
+            return qt_write_function(error, "fn:contains", properties[index].field, value, query);
         case QGalleryFilter::RegExp:
-            qt_write_function("REGEX", properties[index].field, filter.value().toString(), query);
-            break;
+            return value.type() != QVariant::RegExp
+                    ? qt_write_function(error, "REGEX", properties[index].field, value, query)
+                    : qt_write_function(error, "REGEX", properties[index].field, value.toRegExp(), query);
         default:
             *error = QDocumentGallery::FilterError;
 
