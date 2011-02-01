a1370297a36b57b459c289fde9ff9053837138a5
Aaron McCarthy <aaron.mccarthy@nokia.com>
2011-02-01 11:53:14 +1000

    Add target type for NFC Forum device.

--- src/connectivity/nfc/qnearfieldtarget.h
+++ src/connectivity/nfc/qnearfieldtarget.h
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
 ** All rights reserved.
 ** Contact: Nokia Corporation (qt-info@nokia.com)
 **
@@ -74,7 +74,8 @@ public:
         NfcTagType2,
         NfcTagType3,
         NfcTagType4,
-        MifareTag
+        MifareTag,
+        NfcForumDevice
     };
 
     enum AccessMethod {
