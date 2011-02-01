6ddd1e111da233419c836e43518b8b8429a3f6ea
Aaron McCarthy <aaron.mccarthy@nokia.com>
2011-02-01 16:27:03 +1000

    Provide tag read/write feedback.

--- examples/connectivity/ndefeditor/main.cpp
+++ examples/connectivity/ndefeditor/main.cpp
@@ -38,7 +38,6 @@
 **
 ****************************************************************************/
 
-
 #include <QtGui/QApplication>
 #include "mainwindow.h"
 
@@ -46,7 +45,11 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+#if defined(Q_WS_S60)
+    w.showMaximized();
+#else
     w.show();
+#endif
 
     return a.exec();
 }
