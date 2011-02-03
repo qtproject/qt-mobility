cf2fd5bff7a698944e71ef6dbcb4bcc79dc26f4f
Aaron McCarthy <aaron.mccarthy@nokia.com>
2011-02-04 09:35:09 +1000

    Fix typo.

--- src/connectivity/bluetooth/qbluetoothtransferreply.cpp
+++ src/connectivity/bluetooth/qbluetoothtransferreply.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
 ** All rights reserved.
 ** Contact: Nokia Corporation (qt-info@nokia.com)
 **
@@ -73,7 +73,7 @@ QTM_BEGIN_NAMESPACE
 /*!
     \enum QBluetoothTransferReply::TransferError
 
-    This enum describes the type of error that occured
+    This enum describes the type of error that occurred
 
     \value NoError          No error.
     \value UnknownError     Unknown error, no better enum available
