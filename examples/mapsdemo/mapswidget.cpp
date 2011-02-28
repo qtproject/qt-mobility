bba0721115bec34b4566a4136d21690cceef1797
juhvu <qt-info@nokia.com>
2011-02-28 16:16:49 +1000

    Mapsdemo zoom '+' to fit zoombox in common screen sizes.

--- examples/mapsdemo/mapswidget.cpp
+++ examples/mapsdemo/mapswidget.cpp
@@ -251,7 +251,7 @@ void ZoomButtonItem::setRect(qreal x, qreal y, qreal w, qreal h)
 
     QFont f;
     f.setFixedPitch(true);
-    f.setPixelSize(h/3.0);
+    f.setPixelSize(h/5.0);
     d->plusText->setFont(f);
     d->minusText->setFont(f);
 
