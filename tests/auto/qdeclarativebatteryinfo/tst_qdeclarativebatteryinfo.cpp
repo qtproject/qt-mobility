44cb23d410b08dc003d7154ede2f04fae2dc2700
Lorn Potter <lorn.potter@nokia.com>
2011-03-02 09:44:48 +1000

    currentFLow needs to report -1 if unknown.

--- tests/auto/qdeclarativebatteryinfo/tst_qdeclarativebatteryinfo.cpp
+++ tests/auto/qdeclarativebatteryinfo/tst_qdeclarativebatteryinfo.cpp
@@ -289,7 +289,7 @@ void tst_QDeclarativeBatteryInfo::tst_currentFlow()
     QDeclarativeBatteryInfo bi;
     int flow = bi.currentFlow();
     if (bi.batteryStatus() == QSystemBatteryInfo::BatteryUnknown) {
-        QVERIFY(flow == 0);
+        QVERIFY(flow == -1);
     } else {
         QVERIFY(flow != -1);
     }
