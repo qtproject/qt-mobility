6776e1cc7a5789f4f2a6da60bafc53384ad1c91b
prashanth grandhi <prashanth.2.grandhi@nokia.com>
2011-04-05 16:16:57 +0300

    Added cntversitprefplugin

---
+++ plugins/versit/symbian/cntversitplugins/cntversitprefplugin/inc/cntversitprefplugin.h
@@ -0,0 +1,49 @@
+/*
+* Copyright (c) 2002 Nokia Corporation and/or its subsidiary(-ies).
+* All rights reserved.
+* This component and the accompanying materials are made available
+* under the terms of "Eclipse Public License v1.0"
+* which accompanies this distribution, and is available
+* at the URL "http://www.eclipse.org/legal/epl-v10.html".
+*
+* Initial Contributors:
+* Nokia Corporation - initial contribution.
+*
+* Contributors:
+*
+* Description: 
+*     QtMobility Versit plugin for VCard PREF handling 
+*
+*/
+#ifndef CNTVERSITPREFPLUGIN_H
+#define CNTVERSITPREFPLUGIN_H
+
+#include <qversitcontacthandler.h>
+
+QTM_USE_NAMESPACE
+
+class CntVersitPrefPlugin : public QVersitContactHandler
+{
+public:
+    CntVersitPrefPlugin();
+    void propertyProcessed(const QVersitDocument& document,
+        const QVersitProperty& property,
+        const QContact& contact,
+        bool* alreadyProcessed,
+        QList<QContactDetail>* updatedDetails);
+    void documentProcessed(const QVersitDocument& document, QContact* contact);
+    void detailProcessed(const QContact& contact,
+        const QContactDetail& detail,
+        const QVersitDocument& document,
+        QSet<QString>* processedFields,
+        QList<QVersitProperty>* toBeRemoved,
+        QList<QVersitProperty>* toBeAdded);
+    void contactProcessed(const QContact& contact, QVersitDocument* document);
+private:
+    friend class TestCntVersitPrefPlugin;
+    QMap<QString,QString> mDetailMappings;
+    QList<QContactDetail> mPrefDetailList;
+};
+
+#endif
+
---
+++ plugins/versit/symbian/cntversitplugins/cntversitprefplugin/tsrc/ut_cntversitprefplugin/ut_cntversitprefplugin.pro
@@ -0,0 +1,43 @@
+# Copyright (c) 2009 Nokia Corporation and/or its subsidiary(-ies).
+# All rights reserved.
+# This component and the accompanying materials are made available
+# under the terms of "Eclipse Public License v1.0"
+# which accompanies this distribution, and is available
+# at the URL "http://www.eclipse.org/legal/epl-v10.html".
+# Initial Contributors:
+# Nokia Corporation - initial contribution.
+# Contributors:
+# Description:
+TEMPLATE = app
+TARGET = 
+QT += testlib \
+    sql \
+    xml
+CONFIG += qtestlib
+CONFIG += mobility
+MOBILITY = versit \
+    contacts
+DEPENDPATH += .
+DEPENDPATH += ../..
+INCLUDEPATH += $$APP_LAYER_SYSTEMINCLUDE
+INCLUDEPATH += .
+INCLUDEPATH += ../../../../../inc
+
+symbian:
+ { 
+    MMP_RULES += "USERINCLUDE ../.."
+    
+    # Input
+    HEADERS += ut_cntversitprefplugin.loc
+    
+    HEADERS += ut_cntversitprefplugin.h
+    HEADERS += ../../inc/cntversitprefpluginfactory.h
+    HEADERS += ../../inc/cntversitprefplugin.h
+    SOURCES += ut_cntversitprefplugin.cpp
+    SOURCES += ../../src/cntversitprefpluginfactory.cpp
+    SOURCES += ../../src/cntversitprefplugin.cpp
+    TARGET.CAPABILITY = ALL \
+        -TCB
+}
+
+
