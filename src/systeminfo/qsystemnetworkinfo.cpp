qtproject/qt-mobility

commit 70cf441f6884791f8824f920a0e22d90331892db

Fix the connection & disconnection of signals.
Connect the private object only for the first time.
Disconnect only when there isn't any left.

src/systeminfo/qsystemnetworkinfo.cpp
@@ -304,6 +304,10 @@ QSystemNetworkInfo::NetworkMode QSystemNetworkInfo::currentMode()
 */
 void QSystemNetworkInfo::connectNotify(const char *signal)
 {
+    // connect only once
+    if (receivers(signal) > 1)
+        return;
+
     //check for networkSignalStrengthChanged() signal connect notification
     //This is not required on all platforms
 #if defined(Q_WS_MAEMO_5) || defined(Q_WS_MAEMO_6)
@@ -343,6 +347,10 @@ void QSystemNetworkInfo::connectNotify(const char *signal)
 */
 void QSystemNetworkInfo::disconnectNotify(const char *signal)
 {
+    // disconnect only when there's no connections
+    if (receivers(signal) == 0)
+        return;
+
     //check for networkSignalStrengthChanged() signal disconnect notification
     //This is not required on all platforms
 #if defined(Q_WS_MAEMO_5) || defined(Q_WS_MAEMO_6)

src/systeminfo/qsysteminfo_maemo_p.h
@@ -113,7 +113,6 @@ public:
     QSystemNetworkInfoPrivate(QSystemNetworkInfoLinuxCommonPrivate *parent = 0);
     virtual ~QSystemNetworkInfoPrivate();
 
-
     int cellId();
     QSystemNetworkInfo::CellDataTechnology cellDataTechnology();
 

commit 4a6bed6885c415b4bf1e54068449856d81f4674c

Disconnect only when no other signal connected.
Fix the "typo" introduced by 70cf441f.

src/systeminfo/qsystemnetworkinfo.cpp
@@ -345,7 +345,7 @@ void QSystemNetworkInfo::connectNotify(const char *signal)
 void QSystemNetworkInfo::disconnectNotify(const char *signal)
 {
     // disconnect only when there's no connections
-    if (receivers(signal) == 0)
+    if (receivers(signal) > 0)
         return;
 
     //check for networkSignalStrengthChanged() signal disconnect notification