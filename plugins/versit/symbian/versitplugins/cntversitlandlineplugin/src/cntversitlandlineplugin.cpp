13bad9afd1ed18295b62c073121d9a5ab4a8af72
prashanth grandhi <prashanth.2.grandhi@nokia.com>
2011-04-12 13:08:07 +0300

    Fix license header and spelling errors

--- plugins/versit/symbian/versitplugins/cntversitlandlineplugin/src/cntversitlandlineplugin.cpp
+++ plugins/versit/symbian/versitplugins/cntversitlandlineplugin/src/cntversitlandlineplugin.cpp
@@ -1,22 +1,43 @@
-/*
-* Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
-* All rights reserved.
-* This component and the accompanying materials are made available
-* under the terms of "Eclipse Public License v1.0"
-* which accompanies this distribution, and is available
-* at the URL "http://www.eclipse.org/legal/epl-v10.html".
-*
-* Initial Contributors:
-* Nokia Corporation - initial contribution.
-*
-* Contributors:
-*
-* Description: 
-*     Provides the contact handler for the QVersit plugin implementation 
-*     class CntVersitLandlinePluginFactory
-*     
-*
-*/
+/****************************************************************************
+**
+** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the Qt Mobility Components.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** No Commercial Usage
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the Technology Preview License Agreement accompanying
+** this package.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights.  These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at qt-info@nokia.com.
+**
+**
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
 
 #include "qtcontacts.h"
 #include "qversitproperty.h"
@@ -164,7 +185,7 @@ void CntVersitLandlinePlugin::exportLandLineToVoice(
     while (i.hasNext() && !foundmatch) {
         QVersitProperty property = i.next();
         
-        // when the matching number is found, append the type paramater "VOICE"
+        // when the matching number is found, append the type parameter "VOICE"
         if (property.value() == number 
                 && property.name() == QLatin1String("TEL") ) { 
                  
--- plugins/versit/symbian/versitplugins/cntversitprefplugin/cntversitprefplugin.pro
+++ plugins/versit/symbian/versitplugins/cntversitprefplugin/cntversitprefplugin.pro
@@ -1,21 +1,3 @@
-#
-# Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
-#
-# All rights reserved.
-# This component and the accompanying materials are made available
-# under the terms of "Eclipse Public License v1.0"
-# which accompanies this distribution, and is available
-# at the URL "http://www.eclipse.org/legal/epl-v10.html".
-#
-# Initial Contributors:
-# Nokia Corporation - initial contribution.
-#
-# Contributors:
-#
-# Description:
-#    QtMobility Versit plugin for VCard PREF handling
-#
-
 TEMPLATE = lib
 CONFIG += plugin
 TARGET = $$qtLibraryTarget(cntversitprefplugin)
