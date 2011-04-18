qtproject/qt-mobility

commit dfc91645084f8f8f6f4eeba2b68b1defc3567f41

fix QTMOBILITY-1497
UnionFilter & IntersectionFilter don't emit filterChanged signals when
sub filters changed, add _append, _at, _clear _count static functions
to connect these signals when declarative list property changes it content.

Task-number: QTMOBILITY-1497
Reviewed-by:Chris Adams

Change-Id: Ia4bdd9c5b68dc0f051a36f1e42b7fc6ce2b549e9

plugins/declarative/organizer/qdeclarativeorganizeritemfilter.cpp
@@ -364,3 +364,40 @@ QOrganizerItemFilter QDeclarativeOrganizerItemCollectionFilter::filter() const
     f.setCollectionIds(ids);
     return f;
 }
+
+QDeclarativeListProperty<QDeclarativeOrganizerItemFilter> QDeclarativeOrganizerItemCompoundFilter::filters()
+{
+    return QDeclarativeListProperty<QDeclarativeOrganizerItemFilter>(this,
+                                                          0, // opaque data parameter
+                                                          filters_append,
+                                                          filters_count,
+                                                          filters_at,
+                                                          filters_clear);
+}
+
+void QDeclarativeOrganizerItemCompoundFilter::filters_append(QDeclarativeListProperty<QDeclarativeOrganizerItemFilter>* prop, QDeclarativeOrganizerItemFilter* filter)
+{
+    QDeclarativeOrganizerItemCompoundFilter* compoundFilter = static_cast<QDeclarativeOrganizerItemCompoundFilter*>(prop->object);
+    compoundFilter->m_filters.append(filter);
+    QObject::connect(filter, SIGNAL(filterChanged()), compoundFilter, SIGNAL(filterChanged()));
+    emit compoundFilter->filterChanged();
+}
+
+int QDeclarativeOrganizerItemCompoundFilter::filters_count(QDeclarativeListProperty<QDeclarativeOrganizerItemFilter>* prop)
+{
+    // The 'prop' is in a sense 'this' for this static function (as given in filters() function)
+    return static_cast<QDeclarativeOrganizerItemCompoundFilter*>(prop->object)->m_filters.count();
+}
+
+QDeclarativeOrganizerItemFilter* QDeclarativeOrganizerItemCompoundFilter::filters_at(QDeclarativeListProperty<QDeclarativeOrganizerItemFilter>* prop, int index)
+{
+    return static_cast<QDeclarativeOrganizerItemCompoundFilter*>(prop->object)->m_filters.at(index);
+}
+
+void QDeclarativeOrganizerItemCompoundFilter::filters_clear(QDeclarativeListProperty<QDeclarativeOrganizerItemFilter>* prop)
+{
+    QDeclarativeOrganizerItemCompoundFilter* filter = static_cast<QDeclarativeOrganizerItemCompoundFilter*>(prop->object);
+    qDeleteAll(filter->m_filters);
+    filter->m_filters.clear();
+    emit filterChanged();
+}

plugins/declarative/organizer/plugin.cpp
@@ -108,6 +108,7 @@ public:
         qmlRegisterType<QDeclarativeOrganizerItemIntersectionFilter>(uri, 1, 1, "IntersectionFilter");
         qmlRegisterType<QDeclarativeOrganizerItemUnionFilter>(uri, 1, 1, "UnionFilter");
         qmlRegisterType<QDeclarativeOrganizerItemInvalidFilter>(uri, 1, 1, "InvalidFilter");
+        qmlRegisterType<QDeclarativeOrganizerItemCompoundFilter>();
     }
 };
 

plugins/declarative/organizer/qdeclarativeorganizeritemfilter_p.h
@@ -111,6 +111,28 @@ signals:
 
 QML_DECLARE_TYPE(QDeclarativeOrganizerItemFilter)
 
+class QDeclarativeOrganizerItemCompoundFilter : public QDeclarativeOrganizerItemFilter
+{
+    Q_OBJECT
+    Q_PROPERTY(QDeclarativeListProperty<QDeclarativeOrganizerItemFilter> filters READ filters)
+    Q_CLASSINFO("DefaultProperty", "filters")
+
+public:
+    explicit QDeclarativeOrganizerItemCompoundFilter(QObject* parent = 0) : QDeclarativeOrganizerItemFilter(parent){}
+    virtual ~QDeclarativeOrganizerItemCompoundFilter() {}
+    // 'READ' accessor for the filters, basically this is also a 'WRITE' accessor
+    // as per QDeclarativeListProperty's design.
+    QDeclarativeListProperty<QDeclarativeOrganizerItemFilter> filters();
+
+    static void filters_append(QDeclarativeListProperty<QDeclarativeOrganizerItemFilter>* prop, QDeclarativeOrganizerItemFilter* filter);
+    static int filters_count(QDeclarativeListProperty<QDeclarativeOrganizerItemFilter>* prop);
+    static QDeclarativeOrganizerItemFilter* filters_at(QDeclarativeListProperty<QDeclarativeOrganizerItemFilter>* prop, int index);
+    static void filters_clear(QDeclarativeListProperty<QDeclarativeOrganizerItemFilter>* prop);
+
+protected:
+    QList<QDeclarativeOrganizerItemFilter*> m_filters;
+};
+QML_DECLARE_TYPE(QDeclarativeOrganizerItemCompoundFilter)
 
 //changelog filter
 class QDeclarativeOrganizerItemChangeLogFilter : public QDeclarativeOrganizerItemFilter
@@ -521,17 +543,13 @@ QML_DECLARE_TYPE(QDeclarativeOrganizerItemIdFilter)
 
 
 //intersection filter
-class QDeclarativeOrganizerItemIntersectionFilter : public QDeclarativeOrganizerItemFilter
+class QDeclarativeOrganizerItemIntersectionFilter : public QDeclarativeOrganizerItemCompoundFilter
 {
     Q_OBJECT
-    Q_PROPERTY(QDeclarativeListProperty<QDeclarativeOrganizerItemFilter> filters READ filters NOTIFY valueChanged)
-    Q_CLASSINFO("DefaultProperty", "filters")
-
 public:
     QDeclarativeOrganizerItemIntersectionFilter(QObject *parent = 0)
-        :QDeclarativeOrganizerItemFilter(parent)
+        :QDeclarativeOrganizerItemCompoundFilter(parent)
     {
-        connect(this, SIGNAL(valueChanged()), SIGNAL(filterChanged()));
     }
 
     QOrganizerItemFilter filter() const
@@ -544,33 +562,18 @@ public:
         f.setFilters(filters);
         return f;
     }
-
-    QDeclarativeListProperty<QDeclarativeOrganizerItemFilter> filters()
-    {
-        return QDeclarativeListProperty<QDeclarativeOrganizerItemFilter>(this, m_filters);
-    }
-
-signals:
-    void valueChanged();
-
-private:
-    QList<QDeclarativeOrganizerItemFilter*> m_filters;
 };
 QML_DECLARE_TYPE(QDeclarativeOrganizerItemIntersectionFilter)
 
 
 //union filter
-class QDeclarativeOrganizerItemUnionFilter : public QDeclarativeOrganizerItemFilter
+class QDeclarativeOrganizerItemUnionFilter : public QDeclarativeOrganizerItemCompoundFilter
 {
     Q_OBJECT
-    Q_PROPERTY(QDeclarativeListProperty<QDeclarativeOrganizerItemFilter> filters READ filters NOTIFY valueChanged)
-    Q_CLASSINFO("DefaultProperty", "filters")
-
 public:
     QDeclarativeOrganizerItemUnionFilter(QObject *parent = 0)
-        :QDeclarativeOrganizerItemFilter(parent)
+        :QDeclarativeOrganizerItemCompoundFilter(parent)
     {
-        connect(this, SIGNAL(valueChanged()), SIGNAL(filterChanged()));
     }
 
     QOrganizerItemFilter filter() const
@@ -583,17 +586,6 @@ public:
         f.setFilters(filters);
         return f;
     }
-
-    QDeclarativeListProperty<QDeclarativeOrganizerItemFilter> filters()
-    {
-        return QDeclarativeListProperty<QDeclarativeOrganizerItemFilter>(this, m_filters);
-    }
-
-signals:
-    void valueChanged();
-
-private:
-    QList<QDeclarativeOrganizerItemFilter*> m_filters;
 };
 QML_DECLARE_TYPE(QDeclarativeOrganizerItemUnionFilter)
 

plugins/declarative/organizer/qdeclarativeorganizermodel.cpp
@@ -854,7 +854,7 @@ void QDeclarativeOrganizerModel::removeItem(const QString& id)
 }
 
 /*!
-  \qmlmethod OrganizerModel::removeItem(list<string> itemId)
+  \qmlmethod OrganizerModel::removeItems(list<string> itemId)
   Removes the organizer items with the given \a ids from the backend.
 
   \since organizer 1.1.1

tests/qmlauto/organizer/testcases/tst_organizeritems.qml
@@ -65,6 +65,11 @@ TestCase {
         field:DisplayLabel.Label
     }
 
+    UnionFilter {
+      id:unionFilter
+      DetailFilter {id:detailFilter1; detail:Detail.DisplayLabel; field:DisplayLabel.Label}
+      DetailFilter {id:detailFilter2; detail:Detail.DisplayLabel; field:DisplayLabel.Label}
+    }
     Event {
         id:newEvent1
         displayLabel: "NewEvent1"
@@ -164,6 +169,19 @@ TestCase {
         model.update();
         wait(100);
         verify(model.itemCount === 2); //both events matched now
+
+        //union filter
+        detailFilter1.value = "Edited";
+        detailFilter1.matchFlags = Filter.MatchStartsWith;
+        detailFilter2.matchFlags = Filter.MatchStartsWith;
+        detailFilter2.value = "New";
+        model.autoUpdate = true;
+        model.filter = unionFilter;
+        wait(100);
+        verify(model.itemCount === 2); //both matched
+        detailFilter1.value = "New";
+        wait(100);
+        verify(model.itemCount === 1); //only matches 1
 
     }
 

commit 1ad232c81febdab0b579b7504400435aee9bf8b3

fix build error introduced by dfc91645084f8f8f6f4eeba2b68b1defc3567f41
Change-Id: I9c5d850e6d92ab6bae1297d5bd61686ea1205504

plugins/declarative/organizer/qdeclarativeorganizeritemfilter.cpp
@@ -399,5 +399,5 @@ void QDeclarativeOrganizerItemCompoundFilter::filters_clear(QDeclarativeListProp
     QDeclarativeOrganizerItemCompoundFilter* filter = static_cast<QDeclarativeOrganizerItemCompoundFilter*>(prop->object);
     qDeleteAll(filter->m_filters);
     filter->m_filters.clear();
-    emit filterChanged();
+    emit filter->filterChanged();
 }