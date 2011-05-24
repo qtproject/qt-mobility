fe96de807977901b5d4d857e6fff92d83c74331a
Tuomas Viitanen <tuomas.viitanen@nokia.com>
2011-05-24 18:13:08 +0300

    Symbian CameraBE: Take clip rect into account
    Clip the viewfinder size based on the clip rect to be able to always start the viewfinder.
    
    Reviewed-by: Dmytro Poplavskiy

--- plugins/multimedia/symbian/ecam/s60cameraengine.cpp
+++ plugins/multimedia/symbian/ecam/s60cameraengine.cpp
@@ -202,7 +202,6 @@ void CCameraEngine::StartDirectViewFinderL(RWsSession& aSession,
                             TRect& aScreenRect,
                             TRect& aClipRect)
 {
-    Q_UNUSED(aClipRect)
     if (iEngineState < EEngineIdle)
         User::Leave(KErrNotReady);
 
@@ -210,6 +209,14 @@ void CCameraEngine::StartDirectViewFinderL(RWsSession& aSession,
         User::Leave(KErrNotSupported);
 
     if (!iCamera->ViewFinderActive()) {
+        // Viewfinder extent needs to be clipped according to the clip rect.
+        // This is because the native camera framework does not support
+        // clipping and starting viewfinder with bigger than the display(S60
+        // 5.0 and older)/window(Symbian^3 and later) would cause viewfinder
+        // starting to fail entirely. This causes shrinking effect in some
+        // cases, but is better than not having the viewfinder at all.
+        if (aScreenRect.Intersects(aClipRect))
+            aScreenRect.Intersection(aClipRect);
 
         if (iCameraIndex != 0)
             iCamera->SetViewFinderMirrorL(true);
