42b934d8f432b7c750bb1882297b272f1b7c6acd
Shane Bradley <shane.bradley@nokia.com>
2011-06-21 16:05:25 +1000

    Fix up the tested components tags in the gallery autotests so they show up in the metrics.

--- tests/auto/qgallerytrackerschema_maemo5/tst_qgallerytrackerschema.cpp
+++ tests/auto/qgallerytrackerschema_maemo5/tst_qgallerytrackerschema.cpp
@@ -39,7 +39,7 @@
 **
 ****************************************************************************/
 
-//TESTED_COMPONENT=src/documentgallery
+//TESTED_COMPONENT=src/gallery
 
 #include <maemo5/qgallerytrackerschema_p.h>
 
