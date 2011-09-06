qtproject/qt-mobility

commit c552f1a781665f253c14c642fddead42f8dab106

SymbianCameraBE: Write separate method for getting scaled preview size
Makes the code cleaner as same piece of code is not in 2 places.

Reviewed-by: Dmytro Poplavskiy
Change-Id: I5df4359efdb910f9cebad52abe334d2a6a7fde2b

plugins/multimedia/symbian/ecam/s60imagecapturesession.cpp
@@ -675,6 +675,25 @@ void S60ImageCaptureSession::MceoFocusComplete()
     emit focusStatusChanged(QCamera::Locked, QCamera::LockAcquired);
 }
 
+TSize S60ImageCaptureSession::getScaledPreviewSize(const QSize originalSize) const
+{
+    // Set proper Preview Size
+    TSize scaledSize((originalSize.width() / KSnapshotDownScaleFactor),
+                     (originalSize.height() / KSnapshotDownScaleFactor));
+
+    if (scaledSize.iWidth < KSnapshotMinWidth || scaledSize.iHeight < KSnapshotMinHeight)
+        scaledSize.SetSize((originalSize.width() / (KSnapshotDownScaleFactor/2)),
+                           (originalSize.height() / (KSnapshotDownScaleFactor/2)));
+    if (scaledSize.iWidth < KSnapshotMinWidth || scaledSize.iHeight < KSnapshotMinHeight)
+        scaledSize.SetSize((originalSize.width() / (KSnapshotDownScaleFactor/4)),
+                           (originalSize.height() / (KSnapshotDownScaleFactor/4)));
+    if (scaledSize.iWidth < KSnapshotMinWidth || scaledSize.iHeight < KSnapshotMinHeight)
+        scaledSize.SetSize(originalSize.width(),
+                           originalSize.height());
+
+    return scaledSize;
+}
+
 void S60ImageCaptureSession::MceoCapturedDataReady(TDesC8* aData)
 {
     emit imageExposed(m_currentImageId);
@@ -855,15 +874,7 @@ void S60ImageCaptureSession::saveImageL(TDesC8 *aData, TFileName &aPath)
         S60ImageCaptureDecoder *imageDecoder = S60ImageCaptureDecoder::DataNewL(this, fileSystemAccess, aData);
         CleanupStack::PushL(imageDecoder);
 
-        // Set proper Preview Size
-        TSize scaledSize((m_captureSize.width() / KSnapshotDownScaleFactor), (m_captureSize.height() / KSnapshotDownScaleFactor));
-        if (scaledSize.iWidth < KSnapshotMinWidth || scaledSize.iHeight < KSnapshotMinHeight)
-            scaledSize.SetSize((m_captureSize.width() / (KSnapshotDownScaleFactor/2)), (m_captureSize.height() / (KSnapshotDownScaleFactor/2)));
-        if (scaledSize.iWidth < KSnapshotMinWidth || scaledSize.iHeight < KSnapshotMinHeight)
-            scaledSize.SetSize((m_captureSize.width() / (KSnapshotDownScaleFactor/4)), (m_captureSize.height() / (KSnapshotDownScaleFactor/4)));
-        if (scaledSize.iWidth < KSnapshotMinWidth || scaledSize.iHeight < KSnapshotMinHeight)
-            scaledSize.SetSize(m_captureSize.width(), m_captureSize.height());
-
+        TSize scaledSize = getScaledPreviewSize(m_imageSettings->imageResolution());
         TFrameInfo *info = imageDecoder->frameInfo();
         if (!info) {
             setError(KErrGeneral, tr("Preview image creation failed."));
@@ -1909,15 +1920,7 @@ void S60ImageCaptureSession::handleImageEncoded(int error)
         return;
     }
 
-    // Set proper Preview Size
-    TSize scaledSize((m_captureSize.width() / KSnapshotDownScaleFactor), (m_captureSize.height() / KSnapshotDownScaleFactor));
-    if (scaledSize.iWidth < KSnapshotMinWidth || scaledSize.iHeight < KSnapshotMinHeight)
-        scaledSize.SetSize((m_captureSize.width() / (KSnapshotDownScaleFactor/2)), (m_captureSize.height() / (KSnapshotDownScaleFactor/2)));
-    if (scaledSize.iWidth < KSnapshotMinWidth || scaledSize.iHeight < KSnapshotMinHeight)
-        scaledSize.SetSize((m_captureSize.width() / (KSnapshotDownScaleFactor/4)), (m_captureSize.height() / (KSnapshotDownScaleFactor/4)));
-    if (scaledSize.iWidth < KSnapshotMinWidth || scaledSize.iHeight < KSnapshotMinHeight)
-        scaledSize.SetSize(m_captureSize.width(), m_captureSize.height());
-
+    TSize scaledSize = getScaledPreviewSize(m_imageSettings->imageResolution());
     TFrameInfo *info = m_imageDecoder->frameInfo();
     if (!info) {
         setError(KErrGeneral, tr("Preview image creation failed."));

plugins/multimedia/symbian/ecam/s60imagecapturesession.h
@@ -314,6 +314,8 @@ private: // Internal
 
     QVideoFrame generateImageBuffer(TDesC8 *aData);
 
+    TSize getScaledPreviewSize(const QSize originalSize) const;
+
 signals: // Notifications
 
     void stateChanged(QCamera::State);

commit a8d1d3dd1d5bb3dc71973b215d8769a51240ac70

SymbianCameraBE: Fix build issue
Fix build issue caused by commit:
c552f1a781665f253c14c642fddead42f8dab106

The change was cherry picked from incompatible source.
Now using the right parameter.

Reviewed-by: TrustMe

plugins/multimedia/symbian/ecam/s60imagecapturesession.cpp
@@ -875,7 +875,7 @@ void S60ImageCaptureSession::saveImageL(TDesC8 *aData, TFileName &aPath)
         S60ImageCaptureDecoder *imageDecoder = S60ImageCaptureDecoder::DataNewL(this, fileSystemAccess, aData);
         CleanupStack::PushL(imageDecoder);
 
-        TSize scaledSize = getScaledPreviewSize(m_imageSettings->imageResolution());
+        TSize scaledSize = getScaledPreviewSize(m_captureSize);
         TFrameInfo *info = imageDecoder->frameInfo();
         if (!info) {
             setError(KErrGeneral, tr("Preview image creation failed."));
@@ -1882,7 +1882,7 @@ void S60ImageCaptureSession::handleImageEncoded(int error)
         return;
     }
 
-    TSize scaledSize = getScaledPreviewSize(m_imageSettings->imageResolution());
+    TSize scaledSize = getScaledPreviewSize(m_captureSize);
     TFrameInfo *info = m_imageDecoder->frameInfo();
     if (!info) {
         setError(KErrGeneral, tr("Preview image creation failed."));