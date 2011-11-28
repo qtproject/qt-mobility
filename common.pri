qtproject/qt-mobility

commit a9d8bf0d92a9ad171461fdcce0922e4de3bea08b

Enabled deployment of infixed binaries on Symbian
Task-number: QTMOBILITY-1810
Reviewed-by: Miikka Heikkinen

common.pri
@@ -50,18 +50,6 @@ win32:contains(CONFIG_WIN32,build_all) {
     }
 }
 
-# Helper function.  This should move to a .prf file
-defineReplace(mobilityDeployFilename) {
-   unset(MOBILITY_DEPLOY_NAME)
-   MOBILITY_DEPLOY_NAME = $$1
-   CONFIG(debug, debug|release): {
-      mac:RET = $$member(MOBILITY_DEPLOY_NAME, 0)_debug
-      else:win32:RET = $$member(MOBILITY_DEPLOY_NAME, 0)d
-   }
-   isEmpty(RET):RET = $$MOBILITY_DEPLOY_NAME
-   return($$RET)
-}
-
 # Make sure this goes everywhere we need it
 symbian: load(data_caging_paths)
 

features/utils.pri
@@ -1,0 +1,27 @@
+# Helper functions
+
+defineReplace(mobilityDeployFilename) {
+   unset(MOBILITY_DEPLOY_NAME)
+   MOBILITY_DEPLOY_NAME = $$1
+   CONFIG(debug, debug|release): {
+      mac:RET = $$member(MOBILITY_DEPLOY_NAME, 0)_debug
+      else:win32:RET = $$member(MOBILITY_DEPLOY_NAME, 0)d
+   }
+   isEmpty(RET):RET = $$MOBILITY_DEPLOY_NAME
+   return($$RET)
+}
+
+symbian {
+    defineReplace(mobilityUID) {
+        ret = $$1
+        !isEmpty(QT_LIBINFIX): ret = $$replace(ret, 0x2, 0xE)
+        return($$ret)
+    }
+}
+
+defineReplace(mobilityPluginTarget) {
+    target = $$1
+    symbian: target = $$target$${QT_LIBINFIX}
+    return($$qtLibraryTarget($$target))
+}
+

plugins/contacts/serviceactionmanager/serviceactionmanager.pro
@@ -1,6 +1,8 @@
+include(../../../features/utils.pri)
+
 TEMPLATE = lib
 CONFIG += plugin
-TARGET = $$qtLibraryTarget(qtcontacts_serviceactionmanager)
+TARGET = $$mobilityPluginTarget(qtcontacts_serviceactionmanager)
 PLUGIN_TYPE=contacts
 
 include(../../../common.pri)
@@ -19,7 +21,7 @@ MOBILITY = contacts serviceframework
 symbian: {
     TARGET.CAPABILITY = ALL -TCB
     TARGET.EPOCALLOWDLLDATA = 1
-    TARGET.UID3=0x200315FD
+    TARGET.UID3 = $$mobilityUID(0x200315FD)
 
     target.path = /sys/bin
     INSTALLS += target

plugins/contacts/symbian/plugin/deploy.pri
@@ -1,3 +1,0 @@
-PUBLIC_HEADERS += inc/cntbackendsdefs.h
-CONFIG += middleware
-include(../../../../features/deploy.pri)

plugins/contacts/symbian/plugin/plugin.pro
@@ -1,16 +1,19 @@
+include(../../../../features/utils.pri)
+
 TEMPLATE = lib
 CONFIG += plugin
-TARGET = $$qtLibraryTarget(qtcontacts_symbian)
+TARGET = $$mobilityPluginTarget(qtcontacts_symbian)
 PLUGIN_TYPE=contacts
 
 include(symbian_defines.pri)
-include(deploy.pri)
+PUBLIC_HEADERS += inc/cntbackendsdefs.h
+CONFIG += middleware
 symbian: { 
     load(data_caging_paths)
 
     TARGET.CAPABILITY = ALL -TCB
     TARGET.EPOCALLOWDLLDATA = 1
-    TARGET.UID3 = 0x2002AC7B
+    TARGET.UID3 = $$mobilityUID(0x2002AC7B)
   
     INCLUDEPATH += $$APP_LAYER_SYSTEMINCLUDE
 	

plugins/contacts/symbiansim/symbiansim.pro
@@ -4,9 +4,11 @@
 #
 ######################################################################
 
+include(../../../features/utils.pri)
+
 TEMPLATE = lib
 CONFIG += plugin
-TARGET = $$qtLibraryTarget(qtcontacts_symbiansim)
+TARGET = $$mobilityPluginTarget(qtcontacts_symbiansim)
 PLUGIN_TYPE=contacts
 
 include(../../../common.pri)
@@ -17,7 +19,7 @@ symbian: {
     
     TARGET.CAPABILITY = ALL -TCB
     TARGET.EPOCALLOWDLLDATA = 1
-    TARGET.UID3 = 0x2002AC85
+    TARGET.UID3 = $$mobilityUID(0x2002AC85)
 
     INCLUDEPATH += $$APP_LAYER_SYSTEMINCLUDE
 

plugins/declarative/connectivity/connectivity.pro
@@ -1,3 +1,5 @@
+include(../../../features/utils.pri)
+
 INCLUDEPATH += . \
                ../../../src/global \
                ../../../include \
@@ -7,7 +9,7 @@ INCLUDEPATH += . \
 
 DEPENDPATH += ../../../src/connectivity
 
-TARGET = $$qtLibraryTarget(declarative_connectivity)
+TARGET = $$mobilityPluginTarget(declarative_connectivity)
 TARGETPATH = QtMobility/connectivity
 
 TEMPLATE = lib
@@ -64,7 +66,7 @@ symbian {
     # Allow writable DLL data
     TARGET.EPOCALLOWDLLDATA = 1
     # Target UID, makes every Symbian app unique
-#    TARGET.UID3 = 0x20021325
+#    TARGET.UID3 = $$mobilityUID(0x20021325)
     # Specifies what files shall be deployed: the plugin itself and the qmldir file.
     importFiles.sources = $$DESTDIR/declarative_connectivity$${QT_LIBINFIX}.dll qmldir 
     importFiles.path = $$QT_IMPORTS_BASE_DIR/$$TARGETPATH

plugins/declarative/contacts/contacts.pro
@@ -1,3 +1,5 @@
+include(../../../features/utils.pri)
+
 INCLUDEPATH += . \
                ../../../src/global \
                ../../../include \
@@ -10,7 +12,7 @@ DEPENDPATH += ../../../src/contacts
 
 TEMPLATE = lib
 CONFIG += plugin
-TARGET = $$qtLibraryTarget(declarative_contacts)
+TARGET = $$mobilityPluginTarget(declarative_contacts)
 TARGETPATH = QtMobility/contacts
 DEFINES += QT_MAKEDLL
 PLUGIN_TYPE = declarative
@@ -67,7 +69,7 @@ symbian {
     # Allow writable DLL data
     TARGET.EPOCALLOWDLLDATA = 1
     # Target UID, makes every Symbian app unique
-    TARGET.UID3 = 0x20021325
+    TARGET.UID3 = $$mobilityUID(0x20021325)
     # Specifies what files shall be deployed: the plugin itself and the qmldir file.
     importFiles.sources = $$DESTDIR/declarative_contacts$${QT_LIBINFIX}.dll qmldir 
     importFiles.path = $$QT_IMPORTS_BASE_DIR/$$TARGETPATH

commit 7fee6c1af20d38de50c41b610b229eee3caa6eba

Fix build break caused by a9d8bf0d
Task-number: QTMOBILITY-1810
Reviewed-by: TrustMe

common.pri
@@ -4,6 +4,7 @@
 #
 ######################################################################
 
+include($$PWD/features/utils.pri)
 
 CONFIG(debug, debug|release) {
     WAS_IN_DEBUG=debug