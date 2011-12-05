qtproject/qt-mobility

commit f7c1dda00aabb3931cebd6375741f96306ff65f3

Add EGL video rendering path on Symbian
This change adds support for rendering video and camera viewfinder
frames via EGL, as an alternative to the existing rendering path in
which frames are sent directly to the compositing window manager.

Rendering via EGL rather than directly to the compositor has a number
of advantages, including:
* Elimination of artefacts.  The direct rendering path requires content
  on the video and UI surfaces to be synchronized; in the EGL rendering
  path, this synchronization is deferred to the OpenGLES/OpenVG
  implementation.
* Full support for QGraphicsView and QML APIs.  The direct rendering
  path does not support operations such as free rotation and shear
  transformations; these are supported by both OpenGLES and OpenVG.
* Ability to transform video content using OpenGLES.  Making video
  frames available to EGL means that they can be transformed using
  shader effects, or rendered into 3D scenes.

It also has some disadvantages:
* Greater memory consumption
* Lower-quality scaling

Due to these drawbacks, it is desirable to use the direct rendering
path for 'standard' full-screen video playback / viewfinder display.
The QGraphicsVideoItem implementation by defaut switches
automatically between the two rendering paths; see comments in
qgraphicsvideoitem_symbian.cpp for details.

The EGL rendering path is implemented using the EGL_NOK_image_endpoint2
extension.  If this is not available, QGraphicsVideoItem falls back to
using the direct rendering path.

If creation of the EGL endpoint fails, QGraphicsVideoItem falls back to
using the direct rendering path.

Task-number: QTMOBILITY-1818
Task-number: MOBILITY-3084
Reviewed-by: mread

plugins/multimedia/symbian/videooutput/s60videobuffer.cpp
@@ -1,0 +1,251 @@
+/****************************************************************************
+**
+** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the Qt Mobility Components.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this
+** file. Please review the following information to ensure the GNU Lesser
+** General Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights. These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU General
+** Public License version 3.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of this
+** file. Please review the following information to ensure the GNU General
+** Public License version 3.0 requirements will be met:
+** http://www.gnu.org/copyleft/gpl.html.
+**
+** Other Usage
+** Alternatively, this file may be used in accordance with the terms and
+** conditions contained in a signed written agreement between you and Nokia.
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "s60eglextensions.h"
+#include "s60mmtrace.h"
+#include "s60videoeglrenderercontrol.h"
+#include "s60videobuffer.h"
+#include <QtCore/QElapsedTimer>
+#include <QtCore/QVariant>
+
+#ifndef QT_NO_OPENGL
+#include <GLES/glext.h>
+#endif
+
+// TODO: get rid of these magic numbers
+const QAbstractVideoBuffer::HandleType EGLImageTextureHandle =
+      QAbstractVideoBuffer::HandleType(QAbstractVideoBuffer::UserHandle + 128);
+const QAbstractVideoBuffer::HandleType VGImageTextureHandle =
+      QAbstractVideoBuffer::HandleType(QAbstractVideoBuffer::UserHandle + 129);
+
+// Dummy pointer returned by map() - see comments below
+static uchar* const DummyMapReturnValue = reinterpret_cast<uchar *>(1);
+
+//-----------------------------------------------------------------------------
+// S60EglImageVideoBuffer
+//-----------------------------------------------------------------------------
+
+S60EglImageVideoBuffer::S60EglImageVideoBuffer(S60VideoEglRendererControl *control,
+                                               S60EglEndpoint *endpoint)
+    : QAbstractVideoBuffer(EGLImageTextureHandle)
+    , m_timer(new QElapsedTimer)
+    , m_control(control)
+    , m_endpoint(endpoint)
+    , m_delay(0)
+{
+    VERBOSE_TRACE("S60EglImageVideoBuffer::S60EglImageVideoBuffer" << qtThisPtr());
+}
+
+S60EglImageVideoBuffer::S60EglImageVideoBuffer(S60VideoEglRendererControl *control,
+                                               S60EglEndpoint *endpoint,
+                                               QAbstractVideoBuffer::HandleType type)
+    : QAbstractVideoBuffer(type)
+    , m_timer(new QElapsedTimer)
+    , m_control(control)
+    , m_endpoint(endpoint)
+    , m_delay(0)
+{
+
+}
+
+S60EglImageVideoBuffer::~S60EglImageVideoBuffer()
+{
+    VERBOSE_TRACE("S60EglImageVideoBuffer::~S60EglImageVideoBuffer" << qtThisPtr());
+    unmap();
+}
+
+QAbstractVideoBuffer::MapMode S60EglImageVideoBuffer::mapMode() const
+{
+    return NotMapped;
+}
+
+// Note: the use of map/unmap to indicate when the frame has been displayed
+// by QGraphicsVideoItem is something of a hack, but in QtMobility 1.2,
+// there is no suitable QVideoFrame API, so we must do it this way.
+
+uchar *S60EglImageVideoBuffer::map(MapMode mode, int *numBytes, int *bytesPerLine)
+{
+    Q_UNUSED(mode);
+    Q_UNUSED(numBytes);
+    Q_UNUSED(bytesPerLine);
+    VERBOSE_TRACE("S60EglImageVideoBuffer::map" << qtThisPtr());
+    acquireEglImage();
+    return DummyMapReturnValue;
+}
+
+void S60EglImageVideoBuffer::unmap()
+{
+    VERBOSE_TRACE("S60EglImageVideoBuffer::unmap" << qtThisPtr());
+    releaseEglImage();
+}
+
+QVariant S60EglImageVideoBuffer::handle() const
+{
+    return m_eglImage;
+}
+
+void S60EglImageVideoBuffer::acquireEglImage()
+{
+    Q_ASSERT(EGL_NO_IMAGE_KHR == m_eglImage);
+    m_timer->start();
+    m_eglImage = m_control->acquireEglImage(m_endpoint);
+}
+
+void S60EglImageVideoBuffer::releaseEglImage()
+{
+    if (EGL_NO_IMAGE_KHR != m_eglImage) {
+        VERBOSE_TRACE("S60EglImageVideoBuffer::releaseEglImage" << qtThisPtr());
+        // Here we need to calculate the time interval between acquiring an image
+        // from the endpoint, and that image being rendered.  This is passed back
+        // to the producer, which may use it to correct AV sync.
+        // We assume that unmap() was called by the client shortly after rendering
+        // the image.
+        m_delay = 1000 * m_timer->elapsed();
+        m_control->releaseEglImage(m_endpoint, m_delay);
+        m_eglImage = EGL_NO_IMAGE_KHR;
+    }
+}
+
+
+//-----------------------------------------------------------------------------
+// S60GlTextureVideoBuffer
+//-----------------------------------------------------------------------------
+
+#ifndef QT_NO_OPENGL
+
+S60GlTextureVideoBuffer::S60GlTextureVideoBuffer(S60VideoEglRendererControl *control,
+                                                 S60EglEndpoint *endpoint,
+                                                 S60EglExtensions *extensions)
+    : S60EglImageVideoBuffer(control, endpoint, QAbstractVideoBuffer::GLTextureHandle)
+    , m_extensions(extensions)
+    , m_texture(0)
+{
+
+}
+
+S60GlTextureVideoBuffer::~S60GlTextureVideoBuffer()
+{
+    VERBOSE_TRACE("S60GlTextureVideoBuffer::~S60GlTextureVideoBuffer" << qtThisPtr());
+    unmap();
+}
+
+uchar *S60GlTextureVideoBuffer::map(MapMode mode, int *numBytes, int *bytesPerLine)
+{
+    Q_UNUSED(mode);
+    Q_UNUSED(numBytes);
+    Q_UNUSED(bytesPerLine);
+    VERBOSE_TRACE("S60GlTextureVideoBuffer::map" << qtThisPtr());
+    acquireEglImage();
+    glGenTextures(1, &m_texture);
+    glBindTexture(GL_TEXTURE_2D, m_texture);
+    const GLeglImageOES glImage = reinterpret_cast<GLeglImageOES>(m_eglImage);
+    m_extensions->glEglImageTargetTexture(GL_TEXTURE_2D, glImage);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    return DummyMapReturnValue;
+}
+
+void S60GlTextureVideoBuffer::unmap()
+{
+    VERBOSE_TRACE("S60GlTextureVideoBuffer::unmap" << qtThisPtr());
+    if (m_texture)
+        glDeleteTextures(1, &m_texture);
+    releaseEglImage();
+}
+
+QVariant S60GlTextureVideoBuffer::handle() const
+{
+    return m_texture;
+}
+
+#endif // !QT_NO_OPENGL
+
+
+//-----------------------------------------------------------------------------
+// S60VGImageVideoBuffer
+//-----------------------------------------------------------------------------
+
+#ifndef QT_NO_OPENGL
+
+S60VgImageVideoBuffer::S60VgImageVideoBuffer(S60VideoEglRendererControl *control,
+                                             S60EglEndpoint *endpoint,
+                                             S60EglExtensions *extensions)
+    : S60EglImageVideoBuffer(control, endpoint, VGImageTextureHandle)
+    , m_extensions(extensions)
+{
+    VERBOSE_TRACE("S60VgImageVideoBuffer::S60VgImageVideoBuffer" << qtThisPtr());
+}
+
+S60VgImageVideoBuffer::~S60VgImageVideoBuffer()
+{
+    VERBOSE_TRACE("S60VgImageVideoBuffer::~S60VgImageVideoBuffer" << qtThisPtr());
+    unmap();
+}
+
+uchar *S60VgImageVideoBuffer::map(MapMode mode, int *numBytes, int *bytesPerLine)
+{
+    Q_UNUSED(mode);
+    Q_UNUSED(numBytes);
+    Q_UNUSED(bytesPerLine);
+    VERBOSE_TRACE("S60GlTextureVideoBuffer::map" << qtThisPtr());
+    acquireEglImage();
+    m_vgImage = m_extensions->vgCreateEglImageTarget(m_eglImage);
+    return DummyMapReturnValue;
+}
+
+void S60VgImageVideoBuffer::unmap()
+{
+    VERBOSE_TRACE("S60VgImageVideoBuffer::unmap" << qtThisPtr());
+    if (VG_INVALID_HANDLE != m_vgImage)
+        vgDestroyImage(m_vgImage);
+    releaseEglImage();
+}
+
+QVariant S60VgImageVideoBuffer::handle() const
+{
+    return static_cast<quint32>(m_vgImage);
+}
+
+#endif // !QT_NO_OPENGL
+

plugins/multimedia/symbian/videooutput/s60videoeglrenderercontrol.cpp
@@ -1,0 +1,219 @@
+/****************************************************************************
+**
+** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the Qt Mobility Components.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this
+** file. Please review the following information to ensure the GNU Lesser
+** General Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights. These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU General
+** Public License version 3.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of this
+** file. Please review the following information to ensure the GNU General
+** Public License version 3.0 requirements will be met:
+** http://www.gnu.org/copyleft/gpl.html.
+**
+** Other Usage
+** Alternatively, this file may be used in accordance with the terms and
+** conditions contained in a signed written agreement between you and Nokia.
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "s60eglendpoint.h"
+#include "s60mmtrace.h"
+#include "s60videobuffer.h"
+#include "s60videoeglrenderercontrol.h"
+#include <EGL/egl.h>
+#include <QtMultimediaKit/QAbstractVideoSurface>
+#include <QtMultimediaKit/QVideoFrame>
+#include <QtMultimediaKit/QVideoSurfaceFormat>
+
+#ifdef VIDEOOUTPUT_MEASURE_FRAMERATE
+#include "s60videoframerate.h"
+#endif
+
+S60VideoEglRendererControl::S60VideoEglRendererControl(S60EglExtensions *extensions, QObject *parent)
+    : QVideoRendererControl(parent)
+    , m_nativeSurface(TSurfaceId::CreateNullId())
+    , m_doesProduceEglImages(false)
+    , m_surface(0)
+    , m_eglExtensions(extensions)
+    , m_eglEndpoint(0)
+    , m_buffer(0)
+    , m_frameRate(0)
+{
+    TRACE("S60VideoEglRendererControl::S60VideoEglRendererControl" << qtThisPtr());
+    Q_ASSERT(m_eglExtensions);
+#ifdef VIDEOOUTPUT_MEASURE_FRAMERATE
+    m_frameRate = new S60VideoFrameRate(this);
+#endif
+}
+
+S60VideoEglRendererControl::~S60VideoEglRendererControl()
+{
+    TRACE("S60VideoEglRendererControl::~S60VideoEglRendererControl" << qtThisPtr());
+}
+
+QAbstractVideoSurface *S60VideoEglRendererControl::surface() const
+{
+    return m_surface;
+}
+
+void S60VideoEglRendererControl::setSurface(QAbstractVideoSurface *surface)
+{
+    if (surface != m_surface) {
+        TRACE("S60VideoEglRendererControl::setSurface" << qtThisPtr()
+              << "surface" << surface);
+        m_surface = surface;
+        if (!m_surface)
+            destroyEndpoint();
+        else if (!m_nativeSurface.IsNull())
+            createEndpoint();
+        emit surfaceChanged();
+    }
+}
+
+void S60VideoEglRendererControl::setNativeSurface(TSurfaceId surface)
+{
+    if (surface != m_nativeSurface) {
+        TRACE("S60VideoEglRendererControl::setNativeSurface" << qtThisPtr()
+              << "surface" << (void*)surface.iInternal[3]);
+        m_nativeSurface = surface;
+        if (m_nativeSurface.IsNull())
+            destroyEndpoint();
+        else if (m_surface)
+            createEndpoint();
+    }
+}
+
+const QSize &S60VideoEglRendererControl::nativeSize() const
+{
+    return m_nativeSize;
+}
+
+bool S60VideoEglRendererControl::doesProduceEglImages() const
+{
+    return m_doesProduceEglImages;
+}
+
+void S60VideoEglRendererControl::setNativeSize(QSize size)
+{
+    if (size != m_nativeSize) {
+        TRACE("S60VideoEglRendererControl::setNativeSize" << qtThisPtr()
+              << "size" << size);
+        m_nativeSize = size;
+        emit nativeSizeChanged();
+    }
+}
+
+void S60VideoEglRendererControl::setDoesProduceEglImages(bool enabled)
+{
+    if (enabled != m_doesProduceEglImages) {
+        TRACE("S60VideoEglRendererControl::setDoesProduceEglImages" << qtThisPtr()
+              << "enabled" << enabled);
+        m_doesProduceEglImages = enabled;
+    }
+}
+
+// Helper function
+EGLenum currentEglApi()
+{
+    EGLenum api = 0;
+    if (EGL_NO_CONTEXT != eglGetCurrentContext())
+        api = eglQueryAPI();
+    return api;
+}
+
+void S60VideoEglRendererControl::imageAvailable()
+{
+    const EGLenum api = currentEglApi();
+    VERBOSE_TRACE("S60VideoEglRendererControl::imageAvailable" << qtThisPtr()
+                  << "api" << api);
+    Q_ASSERT(!m_buffer);
+    if (m_doesProduceEglImages) {
+        m_buffer = new S60EglImageVideoBuffer(this, m_eglEndpoint);
+    } else {
+        switch (api) {
+        case EGL_OPENGL_ES_API:
+            m_buffer = new S60GlTextureVideoBuffer(this, m_eglEndpoint, m_eglExtensions);
+            break;
+        case EGL_OPENVG_API:
+            m_buffer = new S60VgImageVideoBuffer(this, m_eglEndpoint, m_eglExtensions);
+            break;
+        }
+    }
+    Q_ASSERT(m_buffer);
+    QVideoSurfaceFormat format(m_nativeSize,
+                               QVideoFrame::Format_RGB32,
+                               m_buffer->handleType());
+    format.setScanLineDirection(QVideoSurfaceFormat::BottomToTop);
+    if (!m_surface->isActive())
+        m_surface->start(format);
+    const QVideoFrame frame(m_buffer, format.frameSize(), format.pixelFormat());
+    m_surface->present(frame);
+#ifdef VIDEOOUTPUT_MEASURE_FRAMERATE
+    m_frameRate->notify();
+#endif
+}
+
+EGLImageKHR S60VideoEglRendererControl::acquireEglImage(S60EglEndpoint *endpoint) const
+{
+    VERBOSE_TRACE("S60VideoEglRendererControl::acquireEglImage" << qtThisPtr());
+    EGLImageKHR image = EGL_NO_IMAGE_KHR;
+    if (m_eglEndpoint == endpoint)
+        image = m_eglEndpoint->acquireImage();
+    return image;
+}
+
+void S60VideoEglRendererControl::releaseEglImage(S60EglEndpoint *endpoint, qint64 delay)
+{
+    VERBOSE_TRACE("S60VideoEglRendererControl::releaseEglImage" << qtThisPtr()
+                  << "delay" << delay);
+    if (m_eglEndpoint == endpoint) {
+        if (delay)
+            m_eglEndpoint->setDelay(delay);
+        m_eglEndpoint->releaseImage();
+    }
+    m_buffer = 0;
+}
+
+void S60VideoEglRendererControl::destroyEndpoint()
+{
+    if (m_eglEndpoint) {
+        delete m_eglEndpoint;
+        m_eglEndpoint = 0;
+    }
+}
+
+void S60VideoEglRendererControl::createEndpoint()
+{
+    Q_ASSERT(m_surface);
+    Q_ASSERT(!m_nativeSurface.IsNull());
+    Q_ASSERT(!m_eglEndpoint);
+    m_eglEndpoint = new S60EglEndpoint(m_nativeSurface, m_eglExtensions, this);
+    if (m_eglEndpoint->isValid())
+        connect(m_eglEndpoint, SIGNAL(imageAvailable()),
+                this, SLOT(imageAvailable()));
+    else
+        emit error();
+}

src/multimedia/multimedia.pro
@@ -10,7 +10,7 @@ INCLUDEPATH+= .
 
 QT += network
 
-contains(QT_CONFIG, opengl) | contains(QT_CONFIG, opengles2): !symbian {
+contains(QT_CONFIG, opengl) | contains(QT_CONFIG, opengles2) {
    QT += opengl
 } else {
    DEFINES += QT_NO_OPENGL
@@ -191,9 +191,16 @@ maemo6 {
 
 symbian {
     contains(surfaces_s60_enabled, yes) {
-        SOURCES += qgraphicsvideoitem_symbian.cpp
+        SOURCES += qeglimagevideosurface_symbian.cpp \
+                   qgraphicsvideoitem_symbian.cpp
+        HEADERS += qeglimagevideosurface_symbian_p.h
     } else {
         SOURCES += qgraphicsvideoitem_overlay.cpp
+    }
+    contains(QT_CONFIG, openvg) {
+        LIBS += -llibopenvg
+    } else {
+        DEFINES += QT_NO_OPENVG
     }
 }
 

tests/auto/multimedia.pro
@@ -50,6 +50,7 @@ SUBDIRS += \
 # doesn't believe it's an untested directory
 # qmultimedia_common
 
+symbian: SUBDIRS += qgraphicsvideoitem_symbian
 
 contains (QT_CONFIG, declarative) {
     SUBDIRS += \

plugins/multimedia/symbian/ecam/camera_s60.pri
@@ -131,7 +131,7 @@ HEADERS += \
     $$PWD/s60cameraengine.h \
     $$PWD/s60cameraviewfinderengine.h \
     $$PWD/s60cameraengineobserver.h \
-    $$PWD/s60videorenderercontrol.h \
+    $$PWD/s60bitmapviewfinderrenderercontrol.h \
     $$PWD/s60cameracapturedestinationcontrol.h \
     $$PWD/s60cameracapturebufferformatcontrol.h \
     $$PWD/s60filenamegenerator.h
@@ -158,7 +158,7 @@ SOURCES += \
     $$PWD/s60cameraadvsettings.cpp \
     $$PWD/s60cameraengine.cpp \
     $$PWD/s60cameraviewfinderengine.cpp \
-    $$PWD/s60videorenderercontrol.cpp \
+    $$PWD/s60bitmapviewfinderrenderercontrol.cpp \
     $$PWD/s60cameracapturedestinationcontrol.cpp \
     $$PWD/s60cameracapturebufferformatcontrol.cpp \
     $$PWD/s60filenamegenerator.cpp

plugins/multimedia/symbian/ecam/s60bitmapviewfinderrenderercontrol.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
 ** All rights reserved.
 ** Contact: Nokia Corporation (qt-info@nokia.com)
 **
@@ -39,24 +39,39 @@
 **
 ****************************************************************************/
 
-#ifndef S60VIDEOOUTPUTINTERFACE_H
-#define S60VIDEOOUTPUTINTERFACE_H
+#include <qabstractvideosurface.h>
 
-#include <QtCore/qglobal.h>
-#include <QtGui/qwindowdefs.h>
-#include <coecntrl.h>
+#include "s60bitmapviewfinderrenderercontrol.h"
 
-class S60VideoOutputInterface
+S60BitmapViewFinderRendererControl::S60BitmapViewFinderRendererControl(QObject *parent) :
+    QVideoRendererControl(parent),
+    m_surface(0)
 {
-public:
-	RWindow *videoWindowHandle() const { return videoWinId() ? static_cast<RWindow *>(videoWinId()->DrawableWindow()) : 0 ; }
-    virtual WId videoWinId() const = 0;
-    // If VIDEOOUTPUT_GRAPHICS_SURFACES is defined, the return value is the video
-    // rectangle relative to the video window.  If not, the return value is the
-    // absolute screen rectangle.
-    virtual QRect videoDisplayRect() const = 0;
-    virtual Qt::AspectRatioMode videoAspectRatio() const = 0;
-};
+}
 
-#endif // S60VIDEOOUTPUTINTERFACE_H
+S60BitmapViewFinderRendererControl::~S60BitmapViewFinderRendererControl()
+{
+    // Stop surface if still active
+    if (m_surface && m_surface->isActive())
+        m_surface->stop();
+}
 
+QAbstractVideoSurface *S60BitmapViewFinderRendererControl::surface() const
+{
+    return m_surface;
+}
+
+void S60BitmapViewFinderRendererControl::setSurface(QAbstractVideoSurface *surface)
+{
+    if (m_surface != surface) {
+        if (surface == 0) {
+            // Stop current surface if needed
+            if (m_surface && m_surface->isActive())
+                m_surface->stop();
+        }
+        m_surface = surface;
+        emit surfaceChanged();
+    }
+}
+
+// End of file

plugins/multimedia/symbian/ecam/s60bitmapviewfinderrenderercontrol.h
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
 ** All rights reserved.
 ** Contact: Nokia Corporation (qt-info@nokia.com)
 **
@@ -39,59 +39,32 @@
 **
 ****************************************************************************/
 
-#ifndef S60VIDEOPLAYERSERVICE_H
-#define S60VIDEOPLAYERSERVICE_H
+#ifndef S60BITMAPVIEWFINDERRENDERERCONTROL_H
+#define S60BITMAPVIEWFINDERRENDERERCONTROL_H
 
-#include <QtCore/qobject.h>
-#include <qmediaservice.h>
+#include <qvideorenderercontrol.h>
 
-#include "s60mediaplayeraudioendpointselector.h"
-
-QT_BEGIN_NAMESPACE
-class QMediaMetaData;
-class QMediaPlayerControl;
-class QMediaPlaylist;
-QT_END_NAMESPACE
-
-QT_USE_NAMESPACE
-
-class S60VideoPlayerSession;
-class S60AudioPlayerSession;
-class S60MediaPlayerControl;
-class S60MediaMetaDataProvider;
-class S60MediaStreamControl;
-class S60MediaRecognizer;
-class S60VideoOutputFactory;
-
-class QMediaContent;
-class QMediaPlaylistNavigator;
-class S60MediaNetworkAccessControl;
-
-class S60MediaPlayerService : public QMediaService
+/*
+ * Control for QGraphicsVideoItem. Viewfinder frames are streamed to a surface
+ * which is drawn to the display by the Qt Graphics View Framework.
+ * VideoRendererControl uses only Bitmap Viewfinder.
+ */
+class S60BitmapViewFinderRendererControl : public QVideoRendererControl
 {
     Q_OBJECT
-
 public:
+    S60BitmapViewFinderRendererControl(QObject *parent = 0);
+    virtual ~S60BitmapViewFinderRendererControl();
 
-    S60MediaPlayerService(QObject *parent = 0);
-    ~S60MediaPlayerService();
+    // S60VideoRendererControl
+    QAbstractVideoSurface *surface() const;
+    void setSurface(QAbstractVideoSurface *surface);
 
-    QMediaControl *requestControl(const char *name);
-    void releaseControl(QMediaControl *control);
-
-    S60MediaPlayerSession* createPlayerSession(const QMediaContent &content);
-
-private:
-    S60MediaPlayerSession *createAudioPlayerSession();
-    S60MediaPlayerSession *createVideoPlayerSession();
+signals:
+    void surfaceChanged();
 
 private:
-    S60MediaPlayerControl *m_control;
-    S60MediaMetaDataProvider *m_metaData;
-    S60MediaPlayerAudioEndpointSelector *m_audioEndpointSelector;
-    S60MediaStreamControl *m_streamControl;
-    S60MediaNetworkAccessControl *m_networkAccessControl;
-    S60VideoOutputFactory *m_videoOutputFactory;
+    QAbstractVideoSurface   *m_surface;
 };
 
-#endif
+#endif // S60VIEWFINDERRENDERERCONTROL_H

commit 5c1aaddf9f5d8f335ca1f18bce0d534d016da5b2

Fix build break caused by f7c1dda0
Allow code to build when QT_NO_OPENGL is defined.

Task-number: QTMOBILITY-1818
Task-number: MOBILITY-3084
Reviewed-by: TrustMe

plugins/multimedia/symbian/videooutput/s60videobuffer.cpp
@@ -53,8 +53,10 @@
 // TODO: get rid of these magic numbers
 const QAbstractVideoBuffer::HandleType EGLImageTextureHandle =
       QAbstractVideoBuffer::HandleType(QAbstractVideoBuffer::UserHandle + 128);
+#ifndef QT_NO_OPENVG
 const QAbstractVideoBuffer::HandleType VGImageTextureHandle =
       QAbstractVideoBuffer::HandleType(QAbstractVideoBuffer::UserHandle + 129);
+#endif // !QT_NO_OPENVG
 
 // Dummy pointer returned by map() - see comments below
 static uchar* const DummyMapReturnValue = reinterpret_cast<uchar *>(1);
@@ -206,7 +208,7 @@ QVariant S60GlTextureVideoBuffer::handle() const
 // S60VGImageVideoBuffer
 //-----------------------------------------------------------------------------
 
-#ifndef QT_NO_OPENGL
+#ifndef QT_NO_OPENVG
 
 S60VgImageVideoBuffer::S60VgImageVideoBuffer(S60VideoEglRendererControl *control,
                                              S60EglEndpoint *endpoint,
@@ -247,5 +249,5 @@ QVariant S60VgImageVideoBuffer::handle() const
     return static_cast<quint32>(m_vgImage);
 }
 
-#endif // !QT_NO_OPENGL
+#endif // !QT_NO_OPENVG
 

plugins/multimedia/symbian/videooutput/s60videoeglrenderercontrol.cpp
@@ -154,12 +154,16 @@ void S60VideoEglRendererControl::imageAvailable()
         m_buffer = new S60EglImageVideoBuffer(this, m_eglEndpoint);
     } else {
         switch (api) {
+#ifndef QT_NO_OPENGL
         case EGL_OPENGL_ES_API:
             m_buffer = new S60GlTextureVideoBuffer(this, m_eglEndpoint, m_eglExtensions);
             break;
+#endif
+#ifndef QT_NO_OPENVG
         case EGL_OPENVG_API:
             m_buffer = new S60VgImageVideoBuffer(this, m_eglEndpoint, m_eglExtensions);
             break;
+#endif
         }
     }
     Q_ASSERT(m_buffer);

src/multimedia/multimedia.pro
@@ -197,6 +197,7 @@ symbian {
     } else {
         SOURCES += qgraphicsvideoitem_overlay.cpp
     }
+    contains(QT_CONFIG, egl): LIBS *= -llibegl
     contains(QT_CONFIG, openvg) {
         LIBS += -llibopenvg
     } else {