qtproject/qt-mobility

commit c76719330dad910caf5b66c20fa08d2be2f494b5

Added Function to handle HW/Media Key Events to Test Player Application

demos/player/playercontrols.cpp
@@ -46,7 +46,7 @@
 #include <QtGui/qtoolbutton.h>
 #include <QtGui/qcombobox.h>
 
-PlayerControls::PlayerControls(QWidget *parent)
+PlayerControls::PlayerControls(QWidget *parent, QMediaPlayer *player)
     : QWidget(parent)
     , playerState(QMediaPlayer::StoppedState)
     , playerMuted(false)
@@ -58,6 +58,9 @@ PlayerControls::PlayerControls(QWidget *parent)
     , volumeSlider(0)
     , rateBox(0)
 {
+    initRemCon();
+    iplayer = player;
+
     playButton = new QToolButton(this);
     playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
 
@@ -113,6 +116,26 @@ PlayerControls::PlayerControls(QWidget *parent)
     if (rateBox)
         layout->addWidget(rateBox);
     setLayout(layout);
+}
+
+PlayerControls::~PlayerControls()
+{
+    delete interfaceSelector;
+}
+
+void PlayerControls::initRemCon()
+{
+    try{
+        QT_TRAP_THROWING(
+        interfaceSelector = CRemConInterfaceSelector::NewL();
+        coreTarget = CRemConCoreApiTarget::NewL(*interfaceSelector, *this);
+        interfaceSelector->OpenTargetL());
+   }  catch (const std::exception &e) {
+       delete interfaceSelector;
+       interfaceSelector = 0;
+       coreTarget = 0;
+   }
+
 }
 
 QMediaPlayer::State PlayerControls::state() const
@@ -208,4 +231,41 @@ void PlayerControls::setPlaybackRate(float rate)
 void PlayerControls::updateRate()
 {
     emit changeRate(playbackRate());
+}
+
+void PlayerControls::MrccatoCommand(TRemConCoreApiOperationId aOperationId, TRemConCoreApiButtonAction aButtonAct)
+{
+    if (!coreTarget)
+        return;
+
+    switch (aOperationId){
+        case ERemConCoreApiVolumeUp:
+            if ((aButtonAct == ERemConCoreApiButtonClick) ||
+                (aButtonAct == ERemConCoreApiButtonPress) ||
+                (aButtonAct == ERemConCoreApiButtonRelease))
+             emit changeVolume((iplayer->volume()) + 10);
+             break;
+        case ERemConCoreApiVolumeDown:
+            if ((aButtonAct == ERemConCoreApiButtonClick) ||
+                (aButtonAct == ERemConCoreApiButtonPress) ||
+                (aButtonAct == ERemConCoreApiButtonRelease))
+             emit changeVolume((iplayer->volume()) - 10);
+             break;
+        case ERemConCoreApiPausePlayFunction:
+            if (aButtonAct == ERemConCoreApiButtonClick)
+             playClicked();
+             break;
+        case ERemConCoreApiStop:
+            if (aButtonAct == ERemConCoreApiButtonClick)
+             emit stop();
+             break;
+        case ERemConCoreApiBackward:
+            if (aButtonAct == ERemConCoreApiButtonClick)
+             emit previous();
+             break;
+        case ERemConCoreApiForward:
+            if (aButtonAct == ERemConCoreApiButtonClick)
+             emit next();
+             break;
+    }
 }

demos/player/playercontrols.h
@@ -41,9 +41,17 @@
 #ifndef PLAYERCONTROLS_H
 #define PLAYERCONTROLS_H
 
-#include <qmediaplayer.h>
+#include <QtCore/qglobal.h>
+
+#ifdef Q_OS_SYMBIAN
 
 #include <QtGui/qwidget.h>
+#include <qmediaplayer.h>
+
+#include <remconcoreapitarget.h>
+#include <remconcoreapitargetobserver.h>
+#include <remconinterfaceselector.h>
+
 
 QT_BEGIN_NAMESPACE
 class QAbstractButton;
@@ -53,17 +61,25 @@ QT_END_NAMESPACE
 
 QT_USE_NAMESPACE
 
-class PlayerControls : public QWidget
+class PlayerControls : public QWidget, public MRemConCoreApiTargetObserver
 {
     Q_OBJECT
 public:
-    PlayerControls(QWidget *parent = 0);
+
+    PlayerControls(QWidget *parent, QMediaPlayer *player);
+    ~PlayerControls();
 
     QMediaPlayer::State state() const;
 
     int volume() const;
     bool isMuted() const;
     qreal playbackRate() const;
+
+protected:
+    void MrccatoCommand(TRemConCoreApiOperationId aOperationId, TRemConCoreApiButtonAction aButtonAct);
+
+private:
+    void initRemCon();
 
 public slots:
     void setState(QMediaPlayer::State state);
@@ -96,6 +112,11 @@ private:
     QAbstractButton *muteButton;
     QAbstractSlider *volumeSlider;
     QComboBox *rateBox;
+
+    CRemConInterfaceSelector *interfaceSelector;
+    CRemConCoreApiTarget *coreTarget;
+    QMediaPlayer *iplayer;
 };
 
-#endif
+#endif // Q_OS_SYMBIAN
+#endif // PLAYERCONTROLS_H

demos/player/player.cpp
@@ -100,7 +100,7 @@ Player::Player(QWidget *parent)
 
     connect(openButton, SIGNAL(clicked()), this, SLOT(open()));
 
-    PlayerControls *controls = new PlayerControls(this);
+    PlayerControls *controls = new PlayerControls(this, player);
     controls->setState(player->state());
     controls->setVolume(player->volume());
     controls->setMuted(controls->isMuted());

demos/player/player.pro
@@ -25,6 +25,10 @@ maemo* {
 }
 
 symbian {
-    LIBS += -lavkon -lcone -leikcore
+    LIBS += -lavkon \
+        -lcone \
+        -leikcore \
+        -lremconinterfacebase \
+        -lremconcoreapi
     TARGET.CAPABILITY = ReadUserData
 }

commit 9a17a2cfbfe76f3ee12a4d6754d58cd6323092bf

Fix build break caused by c7671933
Symbian-specific change was not enclosed with '#ifdef Q_OS_SYMBIAN'

Reviewed-by: Vinod Maralad

demos/player/playercontrols.cpp
@@ -58,7 +58,9 @@ PlayerControls::PlayerControls(QWidget *parent, QMediaPlayer *player)
     , volumeSlider(0)
     , rateBox(0)
 {
+#ifdef Q_OS_SYMBIAN
     initRemCon();
+#endif // Q_OS_SYMBIAN
     iplayer = player;
 
     playButton = new QToolButton(this);
@@ -120,22 +122,9 @@ PlayerControls::PlayerControls(QWidget *parent, QMediaPlayer *player)
 
 PlayerControls::~PlayerControls()
 {
+#ifdef Q_OS_SYMBIAN
     delete interfaceSelector;
-}
-
-void PlayerControls::initRemCon()
-{
-    try{
-        QT_TRAP_THROWING(
-        interfaceSelector = CRemConInterfaceSelector::NewL();
-        coreTarget = CRemConCoreApiTarget::NewL(*interfaceSelector, *this);
-        interfaceSelector->OpenTargetL());
-   }  catch (const std::exception &e) {
-       delete interfaceSelector;
-       interfaceSelector = 0;
-       coreTarget = 0;
-   }
-
+#endif
 }
 
 QMediaPlayer::State PlayerControls::state() const
@@ -233,6 +222,7 @@ void PlayerControls::updateRate()
     emit changeRate(playbackRate());
 }
 
+#ifdef Q_OS_SYMBIAN
 void PlayerControls::MrccatoCommand(TRemConCoreApiOperationId aOperationId, TRemConCoreApiButtonAction aButtonAct)
 {
     if (!coreTarget)
@@ -269,3 +259,18 @@ void PlayerControls::MrccatoCommand(TRemConCoreApiOperationId aOperationId, TRem
              break;
     }
 }
+
+void PlayerControls::initRemCon()
+{
+    try {
+        QT_TRAP_THROWING(
+            interfaceSelector = CRemConInterfaceSelector::NewL();
+            coreTarget = CRemConCoreApiTarget::NewL(*interfaceSelector, *this);
+            interfaceSelector->OpenTargetL());
+    } catch (const std::exception &e) {
+        delete interfaceSelector;
+        interfaceSelector = 0;
+        coreTarget = 0;
+    }
+}
+#endif // Q_OS_SYMBIAN

demos/player/playercontrols.h
@@ -42,16 +42,14 @@
 #define PLAYERCONTROLS_H
 
 #include <QtCore/qglobal.h>
-
-#ifdef Q_OS_SYMBIAN
-
 #include <QtGui/qwidget.h>
 #include <qmediaplayer.h>
 
+#ifdef Q_OS_SYMBIAN
 #include <remconcoreapitarget.h>
 #include <remconcoreapitargetobserver.h>
 #include <remconinterfaceselector.h>
-
+#endif // Q_OS_SYMBIAN
 
 QT_BEGIN_NAMESPACE
 class QAbstractButton;
@@ -61,7 +59,10 @@ QT_END_NAMESPACE
 
 QT_USE_NAMESPACE
 
-class PlayerControls : public QWidget, public MRemConCoreApiTargetObserver
+class PlayerControls : public QWidget
+#ifdef Q_OS_SYMBIAN
+                     , public MRemConCoreApiTargetObserver
+#endif // Q_OS_SYMBIAN
 {
     Q_OBJECT
 public:
@@ -75,11 +76,12 @@ public:
     bool isMuted() const;
     qreal playbackRate() const;
 
+#ifdef Q_OS_SYMBIAN
 protected:
     void MrccatoCommand(TRemConCoreApiOperationId aOperationId, TRemConCoreApiButtonAction aButtonAct);
-
 private:
     void initRemCon();
+#endif // Q_OS_SYMBIAN
 
 public slots:
     void setState(QMediaPlayer::State state);
@@ -112,11 +114,11 @@ private:
     QAbstractButton *muteButton;
     QAbstractSlider *volumeSlider;
     QComboBox *rateBox;
-
+#ifdef Q_OS_SYMBIAN
     CRemConInterfaceSelector *interfaceSelector;
     CRemConCoreApiTarget *coreTarget;
+#endif // Q_OS_SYMBIAN
     QMediaPlayer *iplayer;
 };
 
-#endif // Q_OS_SYMBIAN
 #endif // PLAYERCONTROLS_H